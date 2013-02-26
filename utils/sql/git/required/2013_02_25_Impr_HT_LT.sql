50c50eb6cb52185a10ef6d141508723ee8743a3d
af4t <af4t@comcast.net>
2013-02-26 08:52:51 -0500

    SK Touch of the Wicked should reduce timers for Improved Harm Touch & Leech Touch, as well as regular HT.

---
+++ utils/sql/git/required/2013_02_25_Impr_HT_LT.sql
@@ -0,0 +1,3 @@
+/* Touch of the Wicked should affect Improved Harm Touch and Leech Touch also */
+update aa_actions set redux_aa=596, redux_rate=17 where aaid=207;
+update aa_actions set redux_aa=596, redux_rate=17 where aaid=208;
