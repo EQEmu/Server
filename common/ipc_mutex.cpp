d8628170b5d22bd6baa030c2bec35d37b9ce045d
Arthur Ice <addtheice@gmail.com>
2013-03-03 15:26:40 -0800

    Fixed missing include that blocked compile on linux.

--- common/ipc_mutex.cpp
+++ common/ipc_mutex.cpp
@@ -4,13 +4,13 @@
   This program is free software; you can redistribute it and/or modify
   it under the terms of the GNU General Public License as published by
   the Free Software Foundation; version 2 of the License.
-  
+
     This program is distributed in the hope that it will be useful,
     but WITHOUT ANY WARRANTY except by those people which sell it, which
 	are required to give you total support for your newly bought product;
 	without even the implied warranty of MERCHANTABILITY or FITNESS FOR
 	A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
-	
+
 	  You should have received a copy of the GNU General Public License
 	  along with this program; if not, write to the Free Software
 	  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
@@ -23,6 +23,7 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
+#include <unistd.h>
 #endif
 #include "types.h"
 #include "eqemu_exception.h"
@@ -53,8 +54,8 @@ namespace EQEmu {
 #else
         std::string final_name = name;
         final_name += ".lock";
-        
-        imp_->fd_ = open(final_name.c_str(), 
+
+        imp_->fd_ = open(final_name.c_str(),
             O_RDWR | O_CREAT | O_CLOEXEC,
             S_IRUSR | S_IWUSR);
 
