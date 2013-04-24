b15cb08f549172f4f996be65af911888a5ce5c95
Derision <DerisionEQ@gmail.com>
2013-04-24 19:31:13 +0100

    EQExtractor2 update.

---
+++ utils/EQExtractor2/EQExtractor2/PatchApril15-2013.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+using System.Diagnostics;
+using System.Collections.Generic;
+using EQExtractor2.InternalTypes;
+using EQExtractor2.OpCodes;
+using EQPacket;
+using MyUtils;
+
+namespace EQExtractor2.Patches
+{
+    class PatchApril152013Decoder : PatchMarch132013Decoder
+    {
+        public PatchApril152013Decoder()
+        {
+            Version = "EQ Client Build Date April 15 2013.";
+                                   
+            PatchConfFileName = "patch_April15-2013.conf";            
+
+            SupportsSQLGeneration = false;
+        }       
+    }
+}
