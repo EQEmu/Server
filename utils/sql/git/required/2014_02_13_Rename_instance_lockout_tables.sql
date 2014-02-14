5f02de1c951433bca2c89703b514d48dcfa7a653
JJ <jeremiah.oligario@gmail.com>
2014-02-13 21:14:05 -0500

    Remove db name from sql.

--- utils/sql/git/required/2014_02_13_Rename_instance_lockout_tables.sql
+++ utils/sql/git/required/2014_02_13_Rename_instance_lockout_tables.sql
@@ -1,3 +1,3 @@
 -- rename the instance_lockout tables to instance_list. They have nothing to do with lockouts.
-ALTER TABLE `peq`.`instance_lockout` RENAME TO  `peq`.`instance_list` ;
-ALTER TABLE `peq`.`instance_lockout_player` RENAME TO  `peq`.`instance_list_player` ;
+ALTER TABLE `instance_lockout` RENAME TO  `instance_list` ;
+ALTER TABLE `instance_lockout_player` RENAME TO  `instance_list_player` ;
