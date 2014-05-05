6effe033d640636532dbe4884c72755cd6a5667c
Arthur Dene Ice <addtheice@gmail.com>
2014-05-05 14:04:22 -0700

    storing return value of write, may want to use in the future. this gets rid of warning.

--- common/ProcLauncher.cpp
+++ common/ProcLauncher.cpp
@@ -244,13 +244,13 @@ ProcLauncher::ProcRef ProcLauncher::Launch(Spec *&to_launch) {
 				if(dup2(outfd, STDOUT_FILENO) == -1) {
 					fprintf(stderr, "Unable to duplicate FD %d to %d. Log file will be empty: %s\n", outfd, STDOUT_FILENO, strerror(errno));
 					const char *err = "Unable to redirect stdout into this file. That sucks.";
-					write(outfd, err, strlen(err));
+					auto writeSize = write(outfd, err, strlen(err));
 				}
 				close(STDERR_FILENO);
 				if(dup2(outfd, STDERR_FILENO) == -1) {
 					//can no longer print to screen..
 					const char *err = "Unable to redirect stderr into this file. You might miss some error info in this log.";
-					write(outfd, err, strlen(err));
+					auto writeSize = write(outfd, err, strlen(err));
 				}
 				close(STDIN_FILENO);
 
