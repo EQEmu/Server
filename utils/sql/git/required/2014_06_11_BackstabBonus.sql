b036f493bdb85481c8ef65f0d0ff11a31d221919
Kinglykrab <kinglykrab@gmail.com>
2014-06-11 08:31:34 -0400

    meele to melee in NPCs. Fixed insert queries for rule_values, added ruleset_id.

--- utils/sql/git/required/2014_06_11_BackstabBonus.sql
+++ utils/sql/git/required/2014_06_11_BackstabBonus.sql
@@ -1,5 +1,7 @@
-INSERT INTO `rule_values` VALUES ('Combat:BackstabBonus', 0, 'Modify Backstab skill damage by percent.');
-INSERT INTO `rule_values` VALUES ('Combat:AvoidanceCap', 1000, 'Avoidance Cap.');
+INSERT INTO `rule_values` VALUES (1, 'Combat:BackstabBonus', 0, 'Modify Backstab skill damage by percent.');
+INSERT INTO `rule_values` VALUES (1, 'Combat:AvoidanceCap', 1000, 'Avoidance Cap.');
+ALTER TABLE `npc_types` CHANGE d_meele_texture1 d_melee_texture1 int(10);
+ALTER TABLE `npc_types` CHANGE d_meele_texture2 d_melee_texture2 int(10);
 ALTER TABLE `player_corpses` CHANGE IsBurried isBuried tinyint(3);
 ALTER TABLE `player_corpses` CHANGE rezzed isResurrected tinyint(3);
 ALTER TABLE `player_corpses_backup` CHANGE IsBurried isBuried tinyint(3);
