7f9eb569f31df3cb406dd103f400ce3c22e9a5cf
Kinglykrab <kinglykrab@gmail.com>
2014-06-11 08:03:40 -0400

    Avoidance Cap.

--- utils/sql/git/required/2014_06_11_BackstabBonus.sql
+++ utils/sql/git/required/2014_06_11_BackstabBonus.sql
@@ -1,4 +1,5 @@
 INSERT INTO `rule_values` VALUES ('Combat:BackstabBonus', 0, 'Modify Backstab skill damage by percent.');
+INSERT INTO `rule_values` VALUES ('Combat:AvoidanceCap', 1000, 'Avoidance Cap.');
 ALTER TABLE `player_corpses` CHANGE IsBurried isBuried tinyint(3);
 ALTER TABLE `player_corpses` CHANGE rezzed isResurrected tinyint(3);
 ALTER TABLE `player_corpses_backup` CHANGE IsBurried isBuried tinyint(3);
