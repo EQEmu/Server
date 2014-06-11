a164ab6fec0b80dc232ed414eb041750750e5eaa
Kinglykrab <kinglykrab@gmail.com>
2014-06-11 04:38:27 -0400

    Modified column names in player_corpses and player_corpses_backups to reflect source changes.

--- utils/sql/git/required/2014_06_11_BackstabBonus.sql
+++ utils/sql/git/required/2014_06_11_BackstabBonus.sql
@@ -1,1 +1,5 @@
-INSERT INTO rule_values VALUES ('Combat:BackstabBonus', 0, 'Modify Backstab skill damage by percent.');
+INSERT INTO `rule_values` VALUES ('Combat:BackstabBonus', 0, 'Modify Backstab skill damage by percent.');
+ALTER TABLE `player_corpses` CHANGE IsBurried isBuried tinyint(3);
+ALTER TABLE `player_corpses` CHANGE rezzed isResurrected tinyint(3);
+ALTER TABLE `player_corpses_backup` CHANGE IsBurried isBuried tinyint(3);
+ALTER TABLE `player_corpses_backup` CHANGE rezzed isResurrected tinyint(3);
