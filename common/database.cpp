77a5d82de46029f3a238615cd9c3392cb6541d2b
Arthur Ice <addtheice@gmail.com>
2014-07-10 22:07:23 -0700

    GetAdventureStats converted to StringFormat

--- common/database.cpp
+++ common/database.cpp
@@ -2480,12 +2480,10 @@ void Database::UpdateAdventureStatsEntry(uint32 char_id, uint8 theme, bool win)
 bool Database::GetAdventureStats(uint32 char_id, uint32 &guk_w, uint32 &mir_w, uint32 &mmc_w, uint32 &ruj_w,
 								uint32 &tak_w, uint32 &guk_l, uint32 &mir_l, uint32 &mmc_l, uint32 &ruj_l, uint32 &tak_l)
 {
-	char *query = nullptr;
 
-	auto results = QueryDatabase(query, MakeAnyLenString(&query, "SELECT `guk_wins`, `mir_wins`, `mmc_wins`, `ruj_wins`, `tak_wins`, "
-		"`guk_losses`, `mir_losses`, `mmc_losses`, `ruj_losses`, `tak_losses` FROM `adventure_stats` WHERE player_id=%u",
-		char_id));
-	safe_delete_array(query);
+	std::string query = StringFormat("SELECT `guk_wins`, `mir_wins`, `mmc_wins`, `ruj_wins`, `tak_wins`, `guk_losses`, "
+		"`mir_losses`, `mmc_losses`, `ruj_losses`, `tak_losses` FROM `adventure_stats` WHERE player_id=%u", char_id);
+	auto results = QueryDatabase(query);
 
 	if (!results.Success())
 		return false;
