dbf86dbda2196ebdb7f86f8b9299f999ec1b7369
Kinglykrab <kinglykrab@gmail.com>
2014-08-18 17:29:43 -0400

    Fix OutofCombat and InCombat naming in source.

--- utils/sql/git/required/2014_08_18_spells_new_update.sql
+++ utils/sql/git/required/2014_08_18_spells_new_update.sql
@@ -1,3 +1,2 @@
 ALTER TABLE `spells_new` CHANGE `field191` `viral_targets` INT(11);
 ALTER TABLE `spells_new` CHANGE `field191` `viral_timer` INT(11);
-ALTER TABLE `spells_new` CHANGE `field218` `aemaxtargets` INT(11);
