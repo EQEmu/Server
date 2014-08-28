19271f90a448d3e45418e486f8804fd11f10c9b4
KimLS <KLS@peqtgc.com>
2014-08-28 03:17:47 -0700

    Crash fixes

--- common/database.cpp
+++ common/database.cpp
@@ -161,6 +161,9 @@ uint32 Database::CheckLogin(const char* name, const char* password, int16* oStat
 		return 0;
 	}
 
+	if(results.RowCount() < 1)
+		return 0;
+
 	auto row = results.begin();
 
 	uint32 id = atoi(row[0]);
