0a27273d6414e6d97b622ff59cc38d27d3db8188
RicardoCampos <ricardocampos23@googlemail.com>
2014-10-21 22:48:42 +0100

    Bugfix: Corpse stays locked when looting fails
    If looting fails either because of cooldown or there are items on your
    cursor, the client is sent an end loot packet- the client releases the
    corpse but no one else can loot. This simply resets BeingLootedBy in
    this instance.

--- zone/corpse.cpp
+++ zone/corpse.cpp
@@ -1069,6 +1069,10 @@ void Corpse::LootItem(Client* client, const EQApplicationPacket* app)
 	if(!loot_cooldown_timer.Check())
 	{
 		SendEndLootErrorPacket(client);
+		//unlock corpse for others
+		if (this->BeingLootedBy = client->GetID()) {
+			BeingLootedBy = 0xFFFFFFFF;
+		}
 		return;
 	}
 
@@ -1077,6 +1081,10 @@ void Corpse::LootItem(Client* client, const EQApplicationPacket* app)
 	{
 		client->Message(13, "You may not loot an item while you have an item on your cursor.");
 		SendEndLootErrorPacket(client);
+		//unlock corpse for others
+		if (this->BeingLootedBy = client->GetID()) {
+			BeingLootedBy = 0xFFFFFFFF;
+		}
 		return;
 	}
 
