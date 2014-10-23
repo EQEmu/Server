a062c27e1a845fe8276d507096f711b018888e01
Michael Cook (mackal) <mcook@mackal.net>
2014-10-22 22:27:51 -0400

    Take into account endurance bonuses from AAs

--- zone/client_mods.cpp
+++ zone/client_mods.cpp
@@ -1907,7 +1907,7 @@ uint16 Mob::GetInstrumentMod(uint16 spell_id) const
 
 void Client::CalcMaxEndurance()
 {
-	max_end = CalcBaseEndurance() + spellbonuses.Endurance + itembonuses.Endurance;
+	max_end = CalcBaseEndurance() + spellbonuses.Endurance + itembonuses.Endurance + aabonuses.Endurance;
 
 	if (max_end < 0) {
 		max_end = 0;
