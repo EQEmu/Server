481492cb599113cfc636c065f3812286fdfd2b5d
KimLS <KLS@peqtgc.com>
2014-10-30 19:38:13 -0700

    Fixed SQL to be current date, also it will no longer rely on a field that we removed from the db [skip ci]

---
+++ utils/sql/git/required/2014_10_30_special_abilities_null.sql
@@ -0,0 +1,4 @@
+ALTER TABLE `merc_stats` MODIFY COLUMN `special_abilities`  text CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL;
+
+ALTER TABLE `npc_types` MODIFY COLUMN `special_abilities`  text CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL;
+
