57f8f610943d254d25497030778fecf2165f4835
KayenEQ <kayen85@gmail.com>
2014-10-30 21:22:53 -0400

    code optimization

--- utils/sql/git/required/2014_10_16_special_abilities_null.sql
+++ utils/sql/git/required/2014_10_16_special_abilities_null.sql
@@ -1,6 +1,4 @@
-ALTER TABLE `merc_stats`
-MODIFY COLUMN `special_abilities`  text CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL AFTER `attack_speed`;
+ALTER TABLE `merc_stats` MODIFY COLUMN `special_abilities`  text CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL AFTER `attack_speed`;
 
-ALTER TABLE `npc_types`
-MODIFY COLUMN `special_abilities`  text CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL AFTER `npcspecialattks`;
+ALTER TABLE `npc_types` MODIFY COLUMN `special_abilities`  text CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL AFTER `npcspecialattks`;
 
