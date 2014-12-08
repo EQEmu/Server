910ee66fce942e5c1dcac1f9381aba76320a77ab
Akkadius <akkadius1@gmail.com>
2014-12-08 05:39:54 -0600

    Add "gender" to Zone.SetEntityAttribute

--- zone/remote_call.cpp
+++ zone/remote_call.cpp
@@ -328,5 +328,7 @@ void handle_rc_set_entity_attribute(const std::string &method, const std::string
 		if (params[1] == "appearance_effect"){ ent->SendAppearanceEffect(atoi(params[2].c_str()), 0, 0, 0, 0); }
 		if (params[1] == "size"){ ent->ChangeSize(atoi(params[2].c_str())); }
 		if (params[1] == "texture"){ ent->SendIllusionPacket(0, 0xFF, atoi(params[2].c_str()), 0, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0); }
+		if (params[1] == "gender"){ ent->SendIllusionPacket(0, atoi(params[2].c_str()), 0xFF, 0, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0); }
+
 	}
 }
