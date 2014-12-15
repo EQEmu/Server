1fcb8235c529bf103220f8bc66999b6f227fefb6
Akkadius <akkadius1@gmail.com>
2014-12-15 17:28:04 -0600

    guild_mgr.cpp header cleanup

--- zone/guild_mgr.cpp
+++ zone/guild_mgr.cpp
@@ -15,14 +15,15 @@
 	along with this program; if not, write to the Free Software
 	Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
 */
-#include "../common/debug.h"
-#include "guild_mgr.h"
-#include "zonedb.h"
-#include "worldserver.h"
+
 #include "../common/servertalk.h"
 #include "../common/string_util.h"
+
 #include "client.h"
 #include "entity.h"
+#include "guild_mgr.h"
+#include "worldserver.h"
+#include "zonedb.h"
 
 ZoneGuildManager guild_mgr;
 GuildBankManager *GuildBanks;
