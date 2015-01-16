edd871353d9242a9cbd51ed68ad3b3076dde54fc
Akkadius <akkadius1@gmail.com>
2015-01-16 02:08:04 -0600

    Removed attack.cpp #EQDEBUG

--- zone/attack.cpp
+++ zone/attack.cpp
@@ -16,10 +16,6 @@
 	Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
 */
 
-#if EQDEBUG >= 5
-//#define ATTACK_DEBUG 20
-#endif
-
 #include "../common/debug.h"
 #include "../common/eq_constants.h"
 #include "../common/eq_packet_structs.h"
