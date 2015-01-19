e5dd2634f9032397b30cdfa2596d12c6c50d9bd5
Akkadius <akkadius1@gmail.com>
2015-01-19 05:26:02 -0600

    Remove Duplicative MySQL Error: Error setting pathgrid '%s': '%s'

--- zone/waypoints.cpp
+++ zone/waypoints.cpp
@@ -1238,7 +1238,6 @@ uint32 ZoneDatabase::AddWPForSpawn(Client *client, uint32 spawn2id, float xpos, 
 	auto results = QueryDatabase(query);
 	if (!results.Success()) {
 		// Query error
-		Log.Out(Logs::General, Logs::Error, "Error setting pathgrid '%s': '%s'", query.c_str(), results.ErrorMessage().c_str());
 		return 0;
 	}
 
