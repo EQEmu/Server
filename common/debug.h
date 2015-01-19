0b70706a6447532fb3f2c69be72deb3c9156f5a9
Akkadius <akkadius1@gmail.com>
2015-01-19 04:09:02 -0600

    Stuff

--- common/debug.h
+++ common/debug.h
@@ -1,5 +1,5 @@
 /*	EQEMu: Everquest Server Emulator
-	Copyright (C) 2001-2002 EQEMu Development Team (http://eqemu.org)
+	Copyright (C) 2001-2015 EQEMu Development Team (http://eqemu.org)
 
 	This program is free software; you can redistribute it and/or modify
 	it under the terms of the GNU General Public License as published by
@@ -37,10 +37,5 @@
 	#include <windows.h>
 	#include <winsock2.h>
 #endif
-
-
-
-
-
 
 #endif
