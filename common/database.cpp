5a8e45faab73e6caf65ba582f50e94891fb53ae4
Akkadius <akkadius1@gmail.com>
2015-01-20 02:09:23 -0600

    Remove Duplicative MySQL Error: Unable to clear groups:

--- common/database.cpp
+++ common/database.cpp
@@ -3164,8 +3164,8 @@ void Database::SetGroupID(const char* name, uint32 id, uint32 charid, uint32 ism
 
 void Database::ClearAllGroups(void)
 {
-	std::string query("delete from group_id");
-	auto results = QueryDatabase(query);
+	std::string query("DELETE FROM `group_id`");
+	QueryDatabase(query);
 
 	if (!results.Success())
 		std::cout << "Unable to clear groups: " << results.ErrorMessage() << std::endl;
