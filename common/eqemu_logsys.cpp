e4797d04f094897a310db97b8bf9130600907eec
Akkadius <akkadius1@gmail.com>
2015-01-20 05:18:21 -0600

    Change some defaults for logs

--- common/eqemu_logsys.cpp
+++ common/eqemu_logsys.cpp
@@ -94,6 +94,8 @@ void EQEmuLogSys::LoadLogSettingsDefaults()
 	log_settings[Logs::Zone_Server].log_to_console = 1;
 	log_settings[Logs::QS_Server].log_to_console = 1;
 	log_settings[Logs::UCS_Server].log_to_console = 1;
+	log_settings[Logs::Crash].log_to_console = 1;
+	log_settings[Logs::MySQLError].log_to_console = 1;
 
 	log_settings_loaded = true;
 }
