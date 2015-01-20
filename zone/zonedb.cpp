ffed0514d201d6c15624d00f8d9afde65e5296b2
Akkadius <akkadius1@gmail.com>
2015-01-20 01:42:06 -0600

    Remove Duplicative MySQL Error: Error in DeleteMerchantTemp query '

--- zone/zonedb.cpp
+++ zone/zonedb.cpp
@@ -2502,7 +2502,6 @@ void ZoneDatabase::DeleteMerchantTemp(uint32 npcid, uint32 slot){
 	std::string query = StringFormat("DELETE FROM merchantlist_temp WHERE npcid=%d AND slot=%d", npcid, slot);
 	auto results = QueryDatabase(query);
 	if (!results.Success())
-		std::cerr << "Error in DeleteMerchantTemp query '" << query << "' " << results.ErrorMessage() << std::endl;
 
 }
 
