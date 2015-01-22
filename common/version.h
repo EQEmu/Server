ec121e5ec89b56d459dc1264c0f74c9b4e7bb947
Akkadius <akkadius1@gmail.com>
2015-01-22 15:53:30 -0600

    Re-Fix db_update_manifest.txt and Binary Database Version

--- common/version.h
+++ common/version.h
@@ -30,7 +30,7 @@
 	Manifest: https://github.com/EQEmu/Server/blob/master/utils/sql/db_update_manifest.txt	
 */
 
-#define CURRENT_BINARY_DATABASE_VERSION 9067
+#define CURRENT_BINARY_DATABASE_VERSION 9068
 #define COMPILE_DATE	__DATE__
 #define COMPILE_TIME	__TIME__
 #ifndef WIN32
