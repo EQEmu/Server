d78b9ff2a1da5c0235b7ba7de3e62d05865bf7ee
Akkadius <akkadius1@gmail.com>
2015-01-24 03:49:08 -0600

    Remove occurrences of LogSQL

--- zone/command.cpp
+++ zone/command.cpp
@@ -7252,7 +7252,6 @@ void command_flagedit(Client *c, const Seperator *sep) {
 			return;
 		}
 
-        c->LogSQL(query.c_str());
         c->Message(15, "Success! Zone %s now requires a flag, named %s", database.GetZoneName(zoneid), flag_name);
         return;
 	}
