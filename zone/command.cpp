8ba8a1e6fa57fdc72a263d9a49768f2992c0e16b
Akkadius <akkadius1@gmail.com>
2015-01-24 03:49:14 -0600

    Remove occurrences of LogSQL

--- zone/command.cpp
+++ zone/command.cpp
@@ -8137,7 +8137,6 @@ void command_advnpcspawn(Client *c, const Seperator *sep)
             return;
         }
 
-        c->LogSQL(query.c_str());
         c->Message(0, "Updating respawn timer successful.");
         s2->SetRespawnTimer(new_rs);
         s2->SetVariance(new_var);
