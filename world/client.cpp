faaa98d3f000c5d4c14a08c9bcd27489b932c1d7
KimLS <KLS@peqtgc.com>
2015-01-30 13:04:51 -0800

    Crash fix for crash we saw on peq

--- world/client.cpp
+++ world/client.cpp
@@ -1069,6 +1069,10 @@ void Client::EnterWorld(bool TryBootup) {
 	if (zoneID == 0)
 		return;
 
+	if(!cle) {
+		return;
+	}
+
 	ZoneServer* zs = nullptr;
 	if(instanceID > 0)
 	{
@@ -1127,7 +1131,6 @@ void Client::EnterWorld(bool TryBootup) {
 	cle->SetChar(charid, char_name);
 	database.UpdateLiveChar(char_name, GetAccountID());
 	Log.Out(Logs::Detail, Logs::World_Server,"%s %s (%d:%d)",seencharsel ? "Entering zone" : "Zoning to",zone_name,zoneID,instanceID);
-//	database.SetAuthentication(account_id, char_name, zone_name, ip);
 
 	if (seencharsel) {
 		if (GetAdmin() < 80 && zoneserver_list.IsZoneLocked(zoneID)) {
