8aa8982b66251061ae5937a9a20bbcb7fec02de4
Akkadius <akkadius1@gmail.com>
2015-02-01 21:43:50 -0600

    Add File 2015_02_01_logsys_packet_logs.sql

---
+++ utils/sql/git/required/2015_02_01_logsys_packet_logs.sql
@@ -0,0 +1,3 @@
+REPLACE INTO `logsys_categories` (`log_category_id`, `log_category_description`) VALUES ('39', 'Packet: Server -> Client');
+REPLACE INTO `logsys_categories` (`log_category_id`, `log_category_description`) VALUES ('5', 'Packet: Client -> Server');
+REPLACE INTO `logsys_categories` (`log_category_id`, `log_category_description`) VALUES ('40', 'Packet: Client -> Server Unhandled'); 
