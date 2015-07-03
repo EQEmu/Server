803fa48c053b6cb6b81c14b2b69eb735f1a63cf7
Michael Cook (mackal) <mcook@mackal.net>
2015-07-03 02:17:14 -0400

    Fix AA class check

--- zone/aa.cpp
+++ zone/aa.cpp
@@ -839,7 +839,7 @@ void Client::SendAlternateAdvancementRank(int aa_id, int level) {
 		return;
 	}
 
-	if(!(ability->classes & (1 << GetClass()))) {
+	if(!(ability->classes & (1 << (GetClass() - 1)))) {
 		return;
 	}
 
@@ -1400,7 +1400,7 @@ bool Mob::CanUseAlternateAdvancementRank(AA::Rank *rank) {
 	if(!ability)
 		return false;
 
-	if(!ability->classes & (1 << GetClass())) {
+	if(!(ability->classes & (1 << (GetClass() - 1)))) {
 		return false;
 	}
 
