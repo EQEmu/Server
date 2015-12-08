6f1ad1fbc19528c9c51fe543f2dfbdc528600d07
Uleat <uleat.charismata@gmail.com>
2015-12-07 19:28:13 -0500

    Major change to how commands are loaded

--- common/version.h
+++ common/version.h
@@ -30,7 +30,7 @@
 	Manifest: https://github.com/EQEmu/Server/blob/master/utils/sql/db_update_manifest.txt
 */
 
-#define CURRENT_BINARY_DATABASE_VERSION 9090
+#define CURRENT_BINARY_DATABASE_VERSION 9091
 #ifdef BOTS
 	#define CURRENT_BINARY_BOTS_DATABASE_VERSION 9000
 #else
