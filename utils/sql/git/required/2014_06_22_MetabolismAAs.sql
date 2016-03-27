d1facd9368ebeb24051376dabfefdbbbd8449c55
KayenEQ <kayen85@gmail.com>
2016-03-27 10:57:29 -0400

    Kayen: delete bad test files accidently merged

--- utils/sql/git/required/2014_06_22_MetabolismAAs.sql
+++
@@ -1,6 +0,0 @@
--- Innate Metabolism
-INSERT INTO `aa_effects` (`aaid`, `slot`, `effectid`, `base1`, `base2`) VALUES ('68', '1', '233', '110', '0');
-INSERT INTO `aa_effects` (`aaid`, `slot`, `effectid`, `base1`, `base2`) VALUES ('69', '1', '233', '125', '0');
-INSERT INTO `aa_effects` (`aaid`, `slot`, `effectid`, `base1`, `base2`) VALUES ('70', '1', '233', '150', '0');
-
-
