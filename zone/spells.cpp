3d64878e60f6bafaf966e59d2d13100badf12327
Michael Cook (mackal) <mcook@mackal.net>
2016-08-15 15:23:37 -0400

    Skip OP_BeginCast for discs

--- zone/spells.cpp
+++ zone/spells.cpp
@@ -457,14 +457,16 @@ bool Mob::DoCastSpell(uint16 spell_id, uint16 target_id, CastingSlot slot,
 	// now tell the people in the area -- we ALWAYS want to send this, even instant cast spells.
 	// The only time this is skipped is for NPC innate procs and weapon procs. Procs from buffs
 	// oddly still send this. Since those cases don't reach here, we don't need to check them
-	auto outapp = new EQApplicationPacket(OP_BeginCast,sizeof(BeginCast_Struct));
-	BeginCast_Struct* begincast = (BeginCast_Struct*)outapp->pBuffer;
-	begincast->caster_id = GetID();
-	begincast->spell_id = spell_id;
-	begincast->cast_time = orgcasttime; // client calculates reduced time by itself
-	outapp->priority = 3;
-	entity_list.QueueCloseClients(this, outapp, false, 200, 0, true); //IsClient() ? FILTER_PCSPELLS : FILTER_NPCSPELLS);
-	safe_delete(outapp);
+	if (slot != CastingSlot::Discipline) {
+		auto outapp = new EQApplicationPacket(OP_BeginCast,sizeof(BeginCast_Struct));
+		BeginCast_Struct* begincast = (BeginCast_Struct*)outapp->pBuffer;
+		begincast->caster_id = GetID();
+		begincast->spell_id = spell_id;
+		begincast->cast_time = orgcasttime; // client calculates reduced time by itself
+		outapp->priority = 3;
+		entity_list.QueueCloseClients(this, outapp, false, 200, 0, true); //IsClient() ? FILTER_PCSPELLS : FILTER_NPCSPELLS);
+		safe_delete(outapp);
+	}
 
 	// cast time is 0, just finish it right now and be done with it
 	if(cast_time == 0) {
