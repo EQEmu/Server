4de9b2c53e56f59941d2cceec2153d14e06ee714
Natedog2012 <jwalters_06@yahoo.com>
2016-08-26 06:34:51 -0700

    Add TiltX and TiltY manipulation to objects (Perl) Translate OP_GroundSpawn for Titanium
    #perl plugin http://wiki.eqemulator.org/i?Module=Pastebin&Paste=u9IbA6Ql

--- common/version.h
+++ common/version.h
@@ -30,7 +30,7 @@
 	Manifest: https://github.com/EQEmu/Server/blob/master/utils/sql/db_update_manifest.txt
 */
 
-#define CURRENT_BINARY_DATABASE_VERSION 9097
+#define CURRENT_BINARY_DATABASE_VERSION 9098
 #ifdef BOTS
 	#define CURRENT_BINARY_BOTS_DATABASE_VERSION 9008
 #else
