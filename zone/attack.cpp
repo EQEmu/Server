a6b5a38eaa1d8eba58b037cfbf0fb127e5fe0101
Paul Coene <pcoene1@rochester.rr.com>
2016-11-19 14:11:56 -0500

    Fix so mobs can have defensive procs as per npc_spells.

--- zone/attack.cpp
+++ zone/attack.cpp
@@ -1217,8 +1217,14 @@ bool Client::Attack(Mob* other, int Hand, bool bRiposte, bool IsStrikethrough, b
 
 	CommonBreakInvisibleFromCombat();
 
+	// Defensive procs from opponent happen here.
+	// We, the attacker check for them and make them happen.
+	// Added 2nd check for spell based defensive procs on opponent.
 	if(GetTarget())
+		{
 		TriggerDefensiveProcs(other, Hand, true, damage);
+		TriggerDefensiveProcs(other, Hand, false);
+		}
 
 	if (damage > 0)
 		return true;
@@ -1783,8 +1789,14 @@ bool NPC::Attack(Mob* other, int Hand, bool bRiposte, bool IsStrikethrough, bool
 			TrySkillProc(other, skillinuse, 0, true, Hand);
 	}
 
+	// Defensive procs from opponent happen here.
+	// We, the attacker check for them and make them happen.
+	// Added 2nd check for spell based defensive procs on opponent.
 	if(GetHP() > 0 && !other->HasDied())
+		{
 		TriggerDefensiveProcs(other, Hand, true, damage);
+		TriggerDefensiveProcs(other, Hand, false);
+		}
 
 	if (damage > 0)
 		return true;
