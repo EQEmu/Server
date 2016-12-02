3e504916197a3a9cc68f94da4b941ef36fe398d6
Akkadius <akkadius1@gmail.com>
2016-12-01 19:46:43 -0600

    Update eqemu_server.pl to use new AppVeyor build link [skip ci]

--- utils/scripts/eqemu_server.pl
+++ utils/scripts/eqemu_server.pl
@@ -1214,7 +1214,7 @@ sub copy_file {
 
 sub fetch_latest_windows_appveyor {
 	print "[Update] Fetching Latest Windows Binaries (unstable) from Appveyor... \n";
-	get_remote_file("https://ci.appveyor.com/api/projects/KimLS/server/artifacts/build_x86.zip", "updates_staged/master_windows_build.zip", 1);
+	get_remote_file("https://ci.appveyor.com/api/projects/KimLS/server/artifacts/build_x86_pdb.zip", "updates_staged/master_windows_build.zip", 1);
 	print "[Update] Fetched Latest Windows Binaries (unstable) from Appveyor... \n";
 	print "[Update] Extracting... --- \n";
 	unzip('updates_staged/master_windows_build.zip', 'updates_staged/binaries/');
