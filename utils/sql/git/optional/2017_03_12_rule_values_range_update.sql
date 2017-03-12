41b1634199cf994d0cd07318265816a1fd974bcb
Akkadius <akkadius1@gmail.com>
2017-03-12 17:19:42 -0500

    Update proper file name utils/sql/git/optional/2017_03_12_rule_values_range_update.sql [skip ci]

--- utils/sql/git/optional/2017_03_12_rule_values_range_update.sql
+++ utils/sql/git/optional/2017_03_12_rule_values_range_update.sql
@@ -5,3 +5,6 @@ INSERT INTO `rule_values` VALUES (1, 'Range:Anims', '135', 'The packet range in 
 INSERT INTO `rule_values` VALUES (1, 'Range:DamageMessages', '50', 'The packet range in which damage messages are sent (non-crit)');
 INSERT INTO `rule_values` VALUES (1, 'Range:SpellMessages', '75', 'The packet range in which spell damage messages are sent');
 INSERT INTO `rule_values` VALUES (1, 'Range:Say', '135', 'The range that is required before /say or hail messages will work to an NPC');
+INSERT INTO `rule_values` VALUES (1, 'Range:SpellParticles', '135', 'The packet range in which spell particles are sent');
+INSERT INTO `rule_values` VALUES (1, 'Range:MobPositionUpdates', '600', 'The packet range in which mob position updates are sent');
+INSERT INTO `rule_values` VALUES (1, 'Range:CriticalDamage', '80', 'The packet range in which critical hit messages are sent');
