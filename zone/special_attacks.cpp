1e1de3a48af1aef020e7f09a2d0f3072ea986a59
Michael Cook (mackal) <mcook@mackal.net>
2017-03-20 19:49:35 -0400

    Fix error

--- zone/special_attacks.cpp
+++ zone/special_attacks.cpp
@@ -1900,7 +1900,6 @@ void Mob::Taunt(NPC *who, bool always_succeed, int chance_bonus, bool FromSpell,
 		if (chance_bonus) {
 			Success = zone->random.Roll(chance_bonus);
 		} else {
-			int32 newhate = 0;
 			float tauntchance = 50.0f;
 
 			if (always_succeed)
@@ -1938,7 +1937,7 @@ void Mob::Taunt(NPC *who, bool always_succeed, int chance_bonus, bool FromSpell,
 
 		if (Success) {
 			if (hate_top && hate_top != this) {
-				newhate = (who->GetNPCHate(hate_top) - who->GetNPCHate(this)) + 1 + bonus_hate;
+				int newhate = (who->GetNPCHate(hate_top) - who->GetNPCHate(this)) + 1 + bonus_hate;
 				who->CastToNPC()->AddToHateList(this, newhate);
 				Success = true;
 			} else {
