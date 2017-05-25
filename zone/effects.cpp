b793f3d9cacdd1bb8ed203dc82918d9e5d27187b
Michael Cook (mackal) <mcook@mackal.net>
2017-05-24 22:12:23 -0400

    Whoops, lets make use of the calced focus effect

--- zone/effects.cpp
+++ zone/effects.cpp
@@ -361,7 +361,7 @@ int32 Client::GetActSpellCost(uint16 spell_id, int32 cost)
 	if (spec)
 		PercentManaReduction = 1 + spec / 20; // there seems to be some non-obvious rounding here, let's truncate for now.
 
-	int16 focus_redux = GetFocusEffect(focusManaCost, spell_id);
+	PercentManaReduction += GetFocusEffect(focusManaCost, spell_id);
 
 	cost -= (cost * (PercentManaReduction / 100));
 
