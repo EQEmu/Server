5fac13075b7acbd685821c1c12494a1d0df321b9
KimLS <kls@peqtgc.com>
2017-06-24 18:29:18 -0700

    Up resend delay max a few seconds

--- common/net/daybreak_connection.h
+++ common/net/daybreak_connection.h
@@ -220,7 +220,7 @@ namespace EQ
 				resend_delay_ms = 150;
 				resend_delay_factor = 1.5;
 				resend_delay_min = 150;
-				resend_delay_max = 1000;
+				resend_delay_max = 4000;
 				connect_delay_ms = 500;
 				stale_connection_ms = 90000;
 				connect_stale_ms = 5000;
