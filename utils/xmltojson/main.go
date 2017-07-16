d0ef6a52931c4091cf6efd0b2b804e7524c72ae9
Xackery <xackery@gmail.com>
2017-07-15 17:17:36 -0700

    added revisions to xmltojson based on source repo changes.

--- utils/xmltojson/main.go
+++ utils/xmltojson/main.go
@@ -8,16 +8,15 @@ import (
 	"os"
 	"strings"
 
-	xj "github.com/xackery/goxml2json"
+	xj "github.com/basgys/goxml2json"
 )
 
 func main() {
 	var err error
 	var data []byte
 	var sData string
-	var buf *bytes.Buffer
+	buf := &bytes.Buffer{}
 	var buf2 bytes.Buffer
-	xj.SetAttributePrefix("")
 
 	if data, err = ioutil.ReadFile("eqemu_config.xml"); err != nil {
 		fmt.Println("Failed to open eqemu_config.xml:", err.Error())
@@ -26,10 +25,18 @@ func main() {
 
 	//detect malformed xml in eqemuconfig
 	sData = strings.Replace(string(data), "<?xml version=\"1.0\">", "<?xml version=\"1.0\"?>", 1)
+	r := strings.NewReader(sData)
+	dec := xj.NewDecoder(r)
+	root := &xj.Node{}
+	if err = dec.DecodeWithCustomPrefixes(root, "", ""); err != nil {
+		fmt.Println("Failed to decode eqemu_config.xml:", err.Error())
+		os.Exit(1)
+	}
 
-	//convert xml to json
-	if buf, err = xj.Convert(strings.NewReader(sData)); err != nil {
-		fmt.Println("Failed to process eqemu_config.xml:", err.Error())
+	enc := xj.NewEncoder(buf)
+	err = enc.EncodeWithCustomPrefixes(root, "", "")
+	if err != nil {
+		fmt.Println("Failed to encode eqemu_config.xml:", err.Error())
 		os.Exit(1)
 	}
 
