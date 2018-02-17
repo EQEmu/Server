97bb50a5c4a9738f914be4b173a8af35b0bad758
Uleat <uleat.charismata@gmail.com>
2018-02-17 17:35:54 -0500

    Fix for query failure on some systems

--- utils/sql/git/required/2018_02_13_Heading.sql
+++ utils/sql/git/required/2018_02_13_Heading.sql
@@ -1,3 +1,3 @@
 UPDATE spawn2 SET heading = heading * 8.0 / 4.0;
 UPDATE grid_entries SET heading = heading * 8.0 / 4.0 WHERE heading <> -1;
-INSERT INTO variables (varname, value) VALUES ('fixed_heading', 1); -- hack
+INSERT INTO variables (varname, value, information) VALUES ('fixed_heading', 1, 'manifest heading fix hack'); -- hack
