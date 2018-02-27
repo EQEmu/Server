6f49a379149d046755e0aae50f69c62dc79f6c58
Uleat <uleat.charismata@gmail.com>
2018-02-26 23:04:28 -0500

    Fix for linux build fail

--- zone/worldserver.cpp
+++ zone/worldserver.cpp
@@ -22,6 +22,7 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
 #include <stdio.h>
 #include <iomanip>
 #include <stdarg.h>
+#include <limits.h>
 
 #ifdef _WINDOWS
 #include <process.h>
