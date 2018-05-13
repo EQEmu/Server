80c76433967fa63f5fa083d61273ad09e82b551a
KimLS <KLS@peqtgc.com>
2018-05-12 22:30:07 -0700

    Fix for spaces instead of tabs

--- zone/pathing.cpp
+++ zone/pathing.cpp
@@ -188,24 +188,24 @@ glm::vec3 Mob::HandleStuckPath(const glm::vec3 &To, const glm::vec3 &From)
 	bool partial = false;
 	bool stuck = false;
 	auto r = zone->pathing->FindRoute(To, From, partial, stuck);
-    Route.clear();
-    
-    if (r.size() < 1) {
-        Teleport(To);
-        return To;
-    }
+	Route.clear();
+	
+	if (r.size() < 1) {
+	Teleport(To);
+	return To;
+	}
 
-    auto iter = r.rbegin();
-    auto final_node = (*iter);
-    Teleport(final_node.pos);
+	auto iter = r.rbegin();
+	auto final_node = (*iter);
+	Teleport(final_node.pos);
 
-    if (r.size() < 2) {
-        return final_node.pos;
-    }
-    else {
-        iter++;
-        return (*iter).pos;
-    }
+	if (r.size() < 2) {
+		return final_node.pos;
+	}
+	else {
+		iter++;
+		return (*iter).pos;
+	}
 }
 
 void CullPoints(std::vector<FindPerson_Point> &points) {
