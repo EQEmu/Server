c35e010461bee727597bf4573f859f3d20ea9d62
Michael Cook (mackal) <mcook@mackal.net>
2018-06-13 17:23:07 -0400

    Tweak SQL

--- utils/sql/git/required/tasks_revamp.sql
+++ utils/sql/git/required/tasks_revamp.sql
@@ -1,2 +1,3 @@
 ALTER TABLE `tasks` ADD `type` TINYINT NOT NULL DEFAULT '0' AFTER `id`;
 ALTER TABLE `tasks` ADD `duration_code` TINYINT NOT NULL DEFAULT '0' AFTER `duration`;
+UPDATE `tasks` SET `type` = '2'; -- we were treating them all as quests
