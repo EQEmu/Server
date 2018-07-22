a366f153f796049a79f76126f5112972420ad019
Trust <fryguy503@gmail.com>
2018-07-22 15:29:39 -0400

    untargetable npcs like horses shouldn't get hit by AoEs

--- zone/effects.cpp
+++ zone/effects.cpp
@@ -742,7 +742,7 @@ void EntityList::AESpell(Mob *caster, Mob *center, uint16 spell_id, bool affect_
 			continue;
 		if (spells[spell_id].targettype == ST_AreaNPCOnly && !curmob->IsNPC())
 			continue;
-		if (!curmob->IsTargetable())
+		if (!curmob->IsTargetable() && !curmob->IsAura())
 			continue;
 		// check PC/NPC only flag 1 = PCs, 2 = NPCs
 		if (spells[spell_id].pcnpc_only_flag == 1 && !curmob->IsClient() && !curmob->IsMerc())
