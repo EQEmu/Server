fc69824a3f2b6c185813acdaf0dd559069afecea
JJ <jeremiah.oligario@gmail.com>
2018-11-25 16:53:00 -0500

    Oops. https://github.com/EQEmu/Server/commit/be17d21149e840a3df7447d7d76a91e6247b9376

--- utils/sql/git/required/2018_11_25_name_filter_update.sql
+++ utils/sql/git/required/2018_11_25_name_filter_update.sql
@@ -1,1 +1,5 @@
-ALTER TABLE `zone` ADD `npc_max_aggro_dist` INT NOT NULL DEFAULT '600';
+ALTER TABLE `name_filter`
+ADD COLUMN `id` INT(11) NULL AUTO_INCREMENT FIRST,
+DROP PRIMARY KEY,
+ADD PRIMARY KEY (`id`) USING BTREE,
+ADD INDEX `name_search_index`(`name`);
