293a18301df7fdbce25087269769406ad650dc44
Noudess <noudess@gmail.com>
2019-03-06 16:10:23 -0500

    More format

--- utils/scripts/eqemu_server.pl
+++ utils/scripts/eqemu_server.pl
@@ -2216,7 +2216,7 @@ sub run_database_check {
             }
 
             if ($val == 9138) {
-				fix_quest_factions();
+                fix_quest_factions();
 			}
         }
         $db_run_stage = 2;
