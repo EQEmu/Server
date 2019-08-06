b117be29fb94828e2c0d0ddb81ff6afbab1c0cfd
Akkadius <akkadius1@gmail.com>
2019-08-06 01:59:54 -0500

    Movement code readability

---
+++ cmake-build-debug/submodules/libuv/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /Users/cmiles/code/eqemu-docker-cloud/code/submodules/libuv
+# Build directory: /Users/cmiles/code/eqemu-docker-cloud/code/cmake-build-debug/submodules/libuv
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
