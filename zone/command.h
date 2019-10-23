101b77b359438f3845c01a8e218a42f0e4b5588b
kentai <kentai.xiaobai@gmail.com>
2019-10-24 02:38:48 +1100

    Revert "#faction -- Displays Target NPC's Primary faction"
    This reverts commit 7b2e3b94ca0dbb780ba3cf53ce1154d5c23b5e3b.

--- zone/command.h
+++ zone/command.h
@@ -97,7 +97,6 @@ void command_emoteview(Client* c, const Seperator *sep);
 void command_enablerecipe(Client *c, const Seperator *sep);
 void command_equipitem(Client *c, const Seperator *sep);
 void command_face(Client *c, const Seperator *sep);
-void command_faction(Client *c, const Seperator *sep);
 void command_findaliases(Client *c, const Seperator *sep);
 void command_findnpctype(Client *c, const Seperator *sep);
 void command_findspell(Client *c, const Seperator *sep);
