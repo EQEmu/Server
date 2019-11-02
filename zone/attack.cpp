2a8ab28551e44150356aa22cc30612ce38730be2
Paul Coene <pcoene1@twc.com>
2019-11-02 15:19:15 -0400

    Fix damage done by poison proc changes
    spell procs were not firing due to bad conditional.

--- zone/attack.cpp
+++ zone/attack.cpp
@@ -4075,7 +4075,7 @@ void Mob::TrySpellProc(const EQEmu::ItemInstance *inst, const EQEmu::ItemData *w
 		if (IsPet() && hand != EQEmu::invslot::slotPrimary) //Pets can only proc spell procs from their primay hand (ie; beastlord pets)
 			continue; // If pets ever can proc from off hand, this will need to change
 
-		if (!weapon || (SpellProcs[i].base_spellID == POISON_PROC && weapon->ItemType != EQEmu::item::ItemType1HPiercing))
+		if (SpellProcs[i].base_spellID == POISON_PROC && weapon != nullptr && weapon->ItemType != EQEmu::item::ItemType1HPiercing)
 			continue; // Old school poison will only proc with 1HP equipped.
 
 		// Not ranged
