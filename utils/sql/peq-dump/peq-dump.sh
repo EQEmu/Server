e7fab67d8ab0ab69a9be45a10e0000e341633ecb
Akkadius <akkadius1@gmail.com>
2020-03-09 22:57:27 -0500

    Finalize peq-dump.sh script [skip ci]

--- utils/sql/peq-dump/peq-dump.sh
+++ utils/sql/peq-dump/peq-dump.sh
@@ -1,7 +1,11 @@
 #!/usr/bin/env bash
+# Run from the context of server directory
 
 world_path=""
 
+#############################################
+# world path
+#############################################
 if [ -d "bin" ]
 then
     world_path="bin/"
@@ -11,25 +15,18 @@ world_bin="${world_path}world"
 
 echo "World path is [$world_path] bin is [$world_bin]"
 
-# Run from the context of server directory
-#
-#  --content-tables
-#  --login-tables
-#  --player-tables
-#  --system-tables
-#  --query-serv-tables
-
 #############################################
 # dump
 #############################################
 
 dump_path=/tmp/peq-dump/
-
-mkdir -p ${dump_path}
+echo "Generating dump path [${dump_path}]"
+rm -rf ${dump_path} && mkdir -p ${dump_path}
 
 #############################################
 # generate "drop_" table files
 #############################################
+echo "Generating [drop_*] table exports..."
 bash -c "${world_bin} database:dump --content-tables --drop-table-syntax-only --dump-output-to-console > ${dump_path}drop_tables_content.sql"
 bash -c "${world_bin} database:dump --login-tables --drop-table-syntax-only --dump-output-to-console > ${dump_path}drop_tables_login.sql"
 bash -c "${world_bin} database:dump --player-tables --drop-table-syntax-only --dump-output-to-console > ${dump_path}drop_tables_player.sql"
@@ -40,10 +37,27 @@ bash -c "${world_bin} database:dump --query-serv-tables --drop-table-syntax-only
 #############################################
 # generate "create_" table files
 #############################################
+echo "Generating [create_*] table exports..."
+
+# structure only
 bash -c "${world_bin} database:dump --login-tables --table-structure-only --dump-output-to-console | sed 's/ AUTO_INCREMENT=[0-9]*\b//g' > ${dump_path}create_tables_login.sql"
 bash -c "${world_bin} database:dump --player-tables --table-structure-only --dump-output-to-console | sed 's/ AUTO_INCREMENT=[0-9]*\b//g' > ${dump_path}create_tables_player.sql"
 bash -c "${world_bin} database:dump --state-tables --table-structure-only --dump-output-to-console | sed 's/ AUTO_INCREMENT=[0-9]*\b//g' > ${dump_path}create_tables_state.sql"
 bash -c "${world_bin} database:dump --query-serv-tables --table-structure-only --dump-output-to-console | sed 's/ AUTO_INCREMENT=[0-9]*\b//g' > ${dump_path}create_tables_queryserv.sql"
 
+# with content
 bash -c "${world_bin} database:dump --content-tables --dump-output-to-console > ${dump_path}create_tables_content.sql"
 bash -c "${world_bin} database:dump --system-tables --dump-output-to-console > ${dump_path}create_tables_system.sql"
+
+#############################################
+# zip
+#############################################
+human_date=$(date +"%B-%d-%Y" | tr '[:upper:]' '[:lower:]')
+
+echo "Compressing..."
+bash -c "cd ${dump_path} && zip peq-latest.zip * && mv ${dump_path}peq-latest.zip /tmp/peq-latest.zip"
+
+echo "Cleaning up..."
+rm -rf ${dump_path}
+
+echo "Dump located [/tmp/peq-latest.zip]"
