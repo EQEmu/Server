12baf25d36b8ee7bf6a746e67b8701fd34b10fb1
Noudess <noudess@gmail.com>
2020-04-30 18:51:26 -0400

    Fix sql - was using old field namey

--- utils/sql/git/required/2020_04_30_fishing_2018.sql
+++ utils/sql/git/required/2020_04_30_fishing_2018.sql
@@ -1,4 +1,4 @@
 /* Update all non-magical fishing poles to expendable per 2018 patch */
 
 ALTER TABLE items RENAME COLUMN unk219 TO sub_type;
-UPDATE items SET unk219 = !magic WHERE itemtype = 36;
+UPDATE items SET sub_type = !magic WHERE itemtype = 36;
