14239167b107cb2dd36179f0746f196908044b32
Akkadius <akkadius1@gmail.com>
2020-12-07 21:17:29 -0600

    Fix formatting from https://github.com/EQEmu/Server/pull/1142

--- zone/mob_ai.cpp
+++ zone/mob_ai.cpp
@@ -1632,14 +1632,16 @@ void NPC::AI_DoMovement() {
 			}
 			else { // Mob was in water, make sure new spot is in water also
 				roambox_destination_z = m_Position.z;
-				auto position = glm::vec3( roambox_destination_x,
-										    roambox_destination_y,
-											m_Position.z + 15);
+				auto position = glm::vec3(
+					roambox_destination_x,
+					roambox_destination_y,
+					m_Position.z + 15
+				);
 				if (!zone->watermap->InLiquid(position)) {
-						roambox_destination_x = m_SpawnPoint.x;
-						roambox_destination_y = m_SpawnPoint.y;
-						roambox_destination_z = m_SpawnPoint.z;
-				}	
+					roambox_destination_x = m_SpawnPoint.x;
+					roambox_destination_y = m_SpawnPoint.y;
+					roambox_destination_z = m_SpawnPoint.z;
+				}
 			}
 
 			PathfinderOptions opts;
