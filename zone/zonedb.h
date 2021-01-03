6c8c81f3db340d91f465d04408cd41c2a1f735c4
hg <4683435+hgtw@users.noreply.github.com>
2021-01-02 19:19:31 -0500

    Move player corpses on instance shutdown
    Moves corpses to graveyard when an expired instance shuts down.
    Zones without a graveyard move them to non-instance version instead.
    
    Fixes player corpses being left inside instances that expire
    before graveyards process or in instances without a graveyard

--- zone/zonedb.h
+++ zone/zonedb.h
@@ -376,6 +376,7 @@ public:
 	uint32		GetCharacterCorpseID(uint32 char_id, uint8 corpse);
 	uint32		GetCharacterCorpseItemAt(uint32 corpse_id, uint16 slotid);
 	uint32		GetPlayerCorpseTimeLeft(uint8 corpse, uint8 type);
+	void        SendCharacterCorpseToNonInstance(uint32 corpse_db_id);
 
 	/* Faction   */
 	bool		GetNPCFactionList(uint32 npcfaction_id, int32* faction_id, int32* value, uint8* temp, int32* primary_faction = 0);
