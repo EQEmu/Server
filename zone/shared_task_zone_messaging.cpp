6ab5fa16f11aff69dfda1632b125a1d217d5d64f
Akkadius <akkadius1@gmail.com>
2021-05-18 00:31:43 -0500

    Zone message handler

---
+++ zone/shared_task_zone_messaging.cpp
@@ -0,0 +1,6 @@
+#include "shared_task_zone_messaging.h"
+
+void SharedTaskZoneMessaging::HandleWorldMessage(ServerPacket *pack)
+{
+
+}
---
+++ zone/shared_task_zone_messaging.h
@@ -0,0 +1,11 @@
+#ifndef EQEMU_SHARED_TASK_ZONE_MESSAGING_H
+#define EQEMU_SHARED_TASK_ZONE_MESSAGING_H
+
+class ServerPacket;
+
+class SharedTaskZoneMessaging {
+	static void HandleWorldMessage(ServerPacket* pack);
+};
+
+
+#endif //EQEMU_SHARED_TASK_ZONE_MESSAGING_H
