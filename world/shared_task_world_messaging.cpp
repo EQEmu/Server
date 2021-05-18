3b7c2b4111b8b543a727011be3a1469dfb7b9227
Akkadius <akkadius1@gmail.com>
2021-05-18 00:30:19 -0500

    World message handler

---
+++ world/shared_task_world_messaging.cpp
@@ -0,0 +1,7 @@
+#include "shared_task_world_messaging.h"
+#include "cliententry.h"
+
+void SharedTaskWorldMessaging::HandleZoneMessage(ServerPacket *pack)
+{
+
+}
---
+++ world/shared_task_world_messaging.h
@@ -0,0 +1,11 @@
+#ifndef EQEMU_SHARED_TASK_WORLD_MESSAGING_H
+#define EQEMU_SHARED_TASK_WORLD_MESSAGING_H
+
+class ServerPacket;
+
+class SharedTaskWorldMessaging {
+	static void HandleZoneMessage(ServerPacket* pack);
+};
+
+
+#endif //EQEMU_SHARED_TASK_WORLD_MESSAGING_H
