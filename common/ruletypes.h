0461ac7912c4a04bebad40c1e0a8b043eb4fd791
Michael <fryguy503@gmail.com>
2021-06-11 14:29:09 -0400

    [Rule] Allow Skill ups from items (Default: On) (#1376)

--- common/ruletypes.h
+++ common/ruletypes.h
@@ -164,6 +164,7 @@ RULE_INT(Character, DefaultGuild, 0, "If not 0, new characters placed into the g
 RULE_BOOL(Character, ProcessFearedProximity, false, "Processes proximity checks when feared")
 RULE_BOOL(Character, EnableCharacterEXPMods, false, "Enables character zone-based experience modifiers.")
 RULE_BOOL(Character, PVPEnableGuardFactionAssist, true, "Enables faction based assisting against the aggresor in pvp.")
+RULE_BOOL(Character, SkillUpFromItems, true, "Allow Skill ups from clickable items")
 RULE_CATEGORY_END()
 
 RULE_CATEGORY(Mercs)
