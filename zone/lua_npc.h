c4ab0091e37bdf3d72462da07316eb3c6a2db30c
Kinglykrab <kinglykrab@gmail.com>
2021-09-02 18:29:01 -0400

    [Quest API] Add GetHealScale() and GetSpellScale()  to Perl and Lua.

--- zone/lua_npc.h
+++ zone/lua_npc.h
@@ -144,6 +144,8 @@ public:
 	uint16 CountItem(uint32 item_id);
 	uint32 GetItemIDBySlot(uint16 slot_id);
 	uint16 GetFirstSlotByItemID(uint32 item_id);
+	float GetHealScale();
+	float GetSpellScale();
 };
 
 #endif
