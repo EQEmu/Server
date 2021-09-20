9b06221be015f3a363b6dca53e137f2f48d8120a
Noudess <noudess@gmail.com>
2021-09-20 11:30:33 -0400

    [Bug Fix] Fix bug where IVU could not be cast on char with Invis

--- zone/spell_effects.cpp
+++ zone/spell_effects.cpp
@@ -582,7 +582,6 @@ bool Mob::SpellEffect(Mob* caster, uint16 spell_id, float partial, int level_ove
 				snprintf(effect_desc, _EDLEN, "Invisibility to Animals");
 #endif
 				invisible_animals = true;
-				SetInvisible(0);
 				break;
 			}
 
@@ -593,7 +592,6 @@ bool Mob::SpellEffect(Mob* caster, uint16 spell_id, float partial, int level_ove
 				snprintf(effect_desc, _EDLEN, "Invisibility to Undead");
 #endif
 				invisible_undead = true;
-				SetInvisible(0);
 				break;
 			}
 			case SE_SeeInvis:
