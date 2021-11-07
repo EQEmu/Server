dad005dd06a57eb42703ea4818f6058a914b135d
Kinglykrab <kinglykrab@gmail.com>
2021-11-07 16:42:54 -0500

    Add message back to #worldwide cast, add optional parameter to disable.

--- zone/command.cpp
+++ zone/command.cpp
@@ -763,6 +763,10 @@ void command_worldwide(Client *c, const Seperator *sep)
 		if (sep->arg[2] && Seperator::IsNumber(sep->arg[2])) {
 			uint8 update_type = WWSpellUpdateType_Cast;
 			auto spell_id = std::stoul(sep->arg[2]);
+			bool disable_message = false;
+			if (sep->arg[3] && Seperator::IsNumber(sep->arg[3])) {
+				disable_message = std::stoi(sep->arg[3]) ? true : false;
+			}
 
 			c->Message(
 				Chat::White,
@@ -774,8 +778,17 @@ void command_worldwide(Client *c, const Seperator *sep)
 			);
 
 			quest_manager.WorldWideSpell(update_type, spell_id);
+			if (!disable_message) {
+				quest_manager.WorldWideMessage(
+					Chat::Yellow,
+					fmt::format(
+						"[SYSTEM] A GM has cast [{}] world-wide!",
+						GetSpellName(spell_id)
+					).c_str()
+				);
+			}
 		} else {
-			c->Message(Chat::White, "Usage: #worldwide cast [Spell ID]");
+			c->Message(Chat::White, "Usage: #worldwide cast [Spell ID] [Disable Message]");
 		}
 	} else if (sub_command == "remove") {
 		if (sep->arg[2] && Seperator::IsNumber(sep->arg[2])) {
@@ -864,7 +877,7 @@ void command_worldwide(Client *c, const Seperator *sep)
 
 	if (!sep->arg[1]) {
 		c->Message(Chat::White, "This command is used to perform world-wide tasks.");
-		c->Message(Chat::White, "Usage: #worldwide cast [Spell ID]");
+		c->Message(Chat::White, "Usage: #worldwide cast [Spell ID] [Disable Message]");
 		c->Message(Chat::White, "Usage: #worldwide remove [Spell ID]");
 		c->Message(Chat::White, "Usage: #worldwide message [Message]");
 		c->Message(
