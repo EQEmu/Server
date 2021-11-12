77f36e7ad9961d9c6e7ca2fe03e0f6e439fb54a3
Akkadius <akkadius1@gmail.com>
2021-11-12 14:40:50 -0600

    Remove sanitize from world level

--- world/login_server.cpp
+++ world/login_server.cpp
@@ -603,8 +603,6 @@ void LoginServer::SendInfo()
 		WorldConfig::SetLocalAddress(l->local_ip_address);
 	}
 
-	SanitizeWorldServerName(l->server_long_name);
-
 	LogInfo(
 		"[LoginServer::SendInfo] protocol_version [{}] server_version [{}] long_name [{}] short_name [{}] account_name [{}] remote_ip_address [{}] local_ip [{}]",
 		l->protocol_version,
