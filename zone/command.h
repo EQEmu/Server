437438fde28f03bc7c662acdf15ff84e3b136d3b
Kinglykrab <kinglykrab@gmail.com>
2021-11-11 20:07:47 -0500

    [Commands] Remove #crashtest Command. - Remove unused/deprecated command.

--- zone/command.h
+++ zone/command.h
@@ -74,7 +74,6 @@ void command_connectworldserver(Client *c, const Seperator *sep);
 void command_copycharacter(Client *c, const Seperator *sep);
 void command_corpse(Client *c, const Seperator *sep);
 void command_corpsefix(Client *c, const Seperator *sep);
-void command_crashtest(Client *c, const Seperator *sep);
 void command_cvs(Client *c, const Seperator *sep);
 void command_d1(Client *c, const Seperator *sep);
 void command_damage(Client *c, const Seperator *sep);
