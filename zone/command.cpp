fc9b0de2ba28802de24fefe2cb02d1c38d9b432a
Kinglykrab <kinglykrab@gmail.com>
2021-11-13 23:51:43 -0500

    Cleanup.

--- zone/command.cpp
+++ zone/command.cpp
@@ -6992,7 +6992,7 @@ void command_setxp(Client *c, const Seperator *sep)
 void command_setpvppoints(Client *c, const Seperator *sep)
 {
 	int arguments = sep->argnum;
-	if (arguments == 0 || !sep->IsNumber(1)) {
+	if (!arguments || !sep->IsNumber(1)) {
 		c->Message(Chat::White, "Command Syntax: #setpvppoints [Amount]");
 		return;
 	}
@@ -7002,7 +7002,7 @@ void command_setpvppoints(Client *c, const Seperator *sep)
 		target = c->GetTarget()->CastToClient();
 	}		
 
-	uint32 pvp_points = static_cast<uint32>(std::min(std::stoll(sep->arg[1]), (long long) 2000000000));
+	uint32 pvp_points = static_cast<uint32>(std::min(std::stoull(sep->arg[1]), (unsigned long long) 2000000000));
 	target->SetPVPPoints(pvp_points);
 	target->Save();
 	target->SendPVPStats();
