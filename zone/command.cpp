4b7fec4fed9b6d9177c7ee5d085c4250599218e5
Kinglykrab <kinglykrab@gmail.com>
2021-11-13 23:46:26 -0500

    Cleanup.

--- zone/command.cpp
+++ zone/command.cpp
@@ -9206,25 +9206,25 @@ void command_itemsearch(Client *c, const Seperator *sep)
 void command_setaaxp(Client *c, const Seperator *sep)
 {
 	int arguments = sep->argnum;
-
 	if (arguments <= 1 || !sep->IsNumber(2)) {
 		c->Message(Chat::White, "Usage: #setaaxp [AA|Group|Raid] [AA Experience]");
 		return;
 	}
 
-	Client *target = c;
+	Client *target = c;	
+	if (c->GetTarget() && c->GetTarget()->IsClient()) {
+		target = c->GetTarget()->CastToClient();
+	}
+
 	std::string aa_type = str_tolower(sep->arg[1]);
-	uint32 aa_experience = static_cast<uint32>(std::min(std::stoll(sep->arg[2]), (long long) 2000000000));
+	std::string group_raid_string;
+	uint32 aa_experience = static_cast<uint32>(std::min(std::stoull(sep->arg[2]), (unsigned long long) 2000000000));
 	bool is_aa = aa_type.find("aa") != std::string::npos;
 	bool is_group = aa_type.find("group") != std::string::npos;
 	bool is_raid = aa_type.find("raid") != std::string::npos;
 	if (!is_aa && !is_group && !is_raid) {
 		c->Message(Chat::White, "Usage: #setaaxp [AA|Group|Raid] [AA Experience]");
 		return;
-	}
-	
-	if (c->GetTarget() && c->GetTarget()->IsClient()) {
-		target = c->GetTarget()->CastToClient();
 	}
 
 	if (is_aa) {
@@ -9234,22 +9234,17 @@ void command_setaaxp(Client *c, const Seperator *sep)
 			false
 		);
 	} else if (is_group) {
+		group_raid_string = "Group ";
 		target->SetLeadershipEXP(
 			aa_experience,
 			target->GetRaidEXP()
 		);
 	} else if (is_raid) {
+		group_raid_string = "Raid ";
 		target->SetLeadershipEXP(
 			target->GetGroupEXP(),
 			aa_experience
 		);
-	}
-
-	std::string group_raid_string;
-	if (is_group) {
-		group_raid_string = "Group ";
-	} else if (is_raid) {
-		group_raid_string = "Raid ";
 	}
 
 	std::string aa_exp_message = fmt::format(
@@ -9258,7 +9253,6 @@ void command_setaaxp(Client *c, const Seperator *sep)
 		c == target ? "have" : "has",
 		aa_experience,
 		group_raid_string
-
 	);
 	c->Message(
 		Chat::White,
