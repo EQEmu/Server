948a3c3593f226352c671c54906cc92f47928056
Kinglykrab <kinglykrab@gmail.com>
2021-11-18 22:19:31 -0500

    Typo.

--- zone/gm_commands/nukeitem.cpp
+++ zone/gm_commands/nukeitem.cpp
@@ -4,7 +4,7 @@ void command_nukeitem(Client *c, const Seperator *sep)
 {
 	int arguments = sep->argnum;
 	if (!arguments || !sep->IsNumber(1)) {
-		c->Message(Chat::White, "Usage: #nukeitem [Item ID] - Removes the specified Item ID from you or your your player target's inventory");
+		c->Message(Chat::White, "Usage: #nukeitem [Item ID] - Removes the specified Item ID from you or your player target's inventory");
 		return;
 	}
 
