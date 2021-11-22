0913400945add379c9b49e809768a463344e28e9
Kinglykrab <kinglykrab@gmail.com>
2021-11-21 21:19:37 -0500

    [Commands] Cleanup #zsky Command. - Cleanup message and logic. - Add parameter to allow data to be saved to database.

--- zone/gm_commands/zsky.cpp
+++ zone/gm_commands/zsky.cpp
@@ -2,19 +2,43 @@
 
 void command_zsky(Client *c, const Seperator *sep)
 {
-	// modifys and resends zhdr packet
-	if (sep->arg[1][0] == 0) {
-		c->Message(Chat::White, "Usage: #zsky <sky type>");
+	int arguments = sep->argnum;
+	if (!arguments || !sep->IsNumber(1)) {
+		c->Message(Chat::White, "Usage: #zsky [Sky Type] [Permanent (0 = False, 1 = True)]");
+		return;
 	}
-	else if (atoi(sep->arg[1]) < 0 || atoi(sep->arg[1]) > 255) {
-		c->Message(Chat::White, "ERROR: Sky type can not be less than 0 or greater than 255!");
-	}
-	else {
-		zone->newzone_data.sky = atoi(sep->arg[1]);
-		auto outapp            = new EQApplicationPacket(OP_NewZone, sizeof(NewZone_Struct));
-		memcpy(outapp->pBuffer, &zone->newzone_data, outapp->size);
-		entity_list.QueueClients(c, outapp);
-		safe_delete(outapp);
-	}
-}
 
+	auto sky_type = std::stoul(sep->arg[1]);
+	auto permanent = sep->arg[2] ? atobool(sep->arg[2]) : false;
+	if (sky_type < 0 || sky_type > 255) {
+		c->Message(Chat::White, "Sky Type cannot be less than 0 or greater than 255!");
+		return;
+	}
+
+	if (permanent) {
+		auto query = fmt::format(
+			"UPDATE zone SET sky = {} WHERE zoneidnumber = {} AND version = {}",
+			sky_type,
+			zone->GetZoneID(),
+			zone->GetInstanceVersion()
+		);
+		database.QueryDatabase(query);
+	}
+
+	zone->newzone_data.sky = static_cast<uint8>(sky_type);
+	auto outapp = new EQApplicationPacket(OP_NewZone, sizeof(NewZone_Struct));
+	memcpy(outapp->pBuffer, &zone->newzone_data, outapp->size);
+	entity_list.QueueClients(c, outapp);
+	safe_delete(outapp);
+
+	c->Message(
+		Chat::White,
+		fmt::format(
+			"Sky Changed | Zone: {} ({}) Sky Type: {} Permanent: {}",
+			zone->GetLongName(),
+			zone->GetZoneID(),
+			sky_type,
+			permanent ? "Yes" : "No"
+		).c_str()
+	);
+}
