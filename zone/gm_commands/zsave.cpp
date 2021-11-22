eb21987ff5232035c198324475d6c682615a598f
Kinglykrab <kinglykrab@gmail.com>
2021-11-21 21:13:05 -0500

    [Commands] Cleanup #zsave Command. - Cleanup message and logic.

--- zone/gm_commands/zsave.cpp
+++ zone/gm_commands/zsave.cpp
@@ -2,11 +2,15 @@
 
 void command_zsave(Client *c, const Seperator *sep)
 {
-	if (zone->SaveZoneCFG()) {
-		c->Message(Chat::Red, "Zone header saved successfully.");
-	}
-	else {
-		c->Message(Chat::Red, "ERROR: Zone header data was NOT saved.");
-	}
+	c->Message(
+		Chat::Red,
+		fmt::format(
+			"Zone header {}.",
+			(
+				zone->SaveZoneCFG() ?
+				"saved successfully" :
+				"failed to save"
+			)
+		).c_str()
+	);
 }
-
