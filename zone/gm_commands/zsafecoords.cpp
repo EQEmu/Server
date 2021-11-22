6201ec494cb82a6d2fefbc26381916e2a94bb782
Kinglykrab <kinglykrab@gmail.com>
2021-11-21 21:10:04 -0500

    [Commands] Cleanup #zsafecoords Command. - Cleanup message and logic. - Add parameter to allow data to be saved to database.

--- zone/gm_commands/zsafecoords.cpp
+++ zone/gm_commands/zsafecoords.cpp
@@ -2,25 +2,64 @@
 
 void command_zsafecoords(Client *c, const Seperator *sep)
 {
-	// modifys and resends zhdr packet
-	if (sep->arg[3][0] == 0) {
-		c->Message(Chat::White, "Usage: #zsafecoords <safe x> <safe y> <safe z>");
+	int arguments = sep->argnum;
+	if (
+		!arguments ||
+		!sep->IsNumber(1) ||
+		!sep->IsNumber(2) ||
+		!sep->IsNumber(3)
+	) {
+		c->Message(Chat::White, "Usage: #zsafecoords [X] [Y] [Z] [Heading] [Permanent (0 = False, 1 = True]");
+		return;
 	}
-	else {
-		zone->newzone_data.safe_x = atof(sep->arg[1]);
-		zone->newzone_data.safe_y = atof(sep->arg[2]);
-		zone->newzone_data.safe_z = atof(sep->arg[3]);
-		//float newdatax = atof(sep->arg[1]);
-		//float newdatay = atof(sep->arg[2]);
-		//float newdataz = atof(sep->arg[3]);
-		//memcpy(&zone->zone_header_data[114], &newdatax, sizeof(float));
-		//memcpy(&zone->zone_header_data[118], &newdatay, sizeof(float));
-		//memcpy(&zone->zone_header_data[122], &newdataz, sizeof(float));
-		//zone->SetSafeCoords();
-		auto outapp               = new EQApplicationPacket(OP_NewZone, sizeof(NewZone_Struct));
-		memcpy(outapp->pBuffer, &zone->newzone_data, outapp->size);
-		entity_list.QueueClients(c, outapp);
-		safe_delete(outapp);
+
+	auto x = std::stof(sep->arg[1]);
+	auto y = std::stof(sep->arg[2]);
+	auto z = std::stof(sep->arg[3]);
+	auto heading = sep->arg[3] ? std::stof(sep->arg[3]) : c->GetHeading();
+	auto permanent = sep->arg[4] ? atobool(sep->arg[4]) : false;
+	zone->newzone_data.safe_x = x;
+	zone->newzone_data.safe_y = y;
+	zone->newzone_data.safe_z = z;
+
+	auto outapp = new EQApplicationPacket(OP_NewZone, sizeof(NewZone_Struct));
+	memcpy(outapp->pBuffer, &zone->newzone_data, outapp->size);
+	entity_list.QueueClients(c, outapp);
+	safe_delete(outapp);
+
+	if (permanent) {
+		auto query = fmt::format(
+			"UPDATE zone SET safe_x = {:.2f}, safe_y = {:.2f}, safe_z = {:.2f}, safe_heading = {:.2f} WHERE zoneidnumber = {} AND version = {}",
+			x,
+			y,
+			z,
+			heading,
+			zone->GetZoneID(),
+			zone->GetInstanceVersion()
+		);
+		database.QueryDatabase(query);
 	}
+
+	c->Message(
+		Chat::White,
+		fmt::format(
+			"Safe Coordinates Changed | Zone: {} ({}){} XYZ: {:.2f}, {:.2f}, {:.2f} Heading: {:.2f} Permanent: {} ",
+			zone->GetLongName(),
+			zone->GetZoneID(),
+			(
+				zone->GetInstanceVersion() ?
+				fmt::format(
+					" Version: {}",
+					zone->GetInstanceVersion()
+				) :
+				""
+			),
+			x,
+			y,
+			z,
+			heading,
+			permanent ? "Yes" : "No"
+		).c_str()
+	);
 }
 
