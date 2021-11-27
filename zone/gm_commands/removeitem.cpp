78af488ac6347ff8f6cd7e16994ae77db554bed3
Kinglykrab <kinglykrab@gmail.com>
2021-11-26 23:23:12 -0500

    [Commands] Add #removeitem Command. - Add #removeitem [Item ID] [Amount] command to remove items by amount versus nuking them all, removes all if amount is greater than what you or your target have.

---
+++ zone/gm_commands/removeitem.cpp
@@ -0,0 +1,85 @@
+#include "../client.h"
+
+void command_removeitem(Client *c, const Seperator *sep)
+{
+	int arguments = sep->argnum;
+	if (!arguments || !sep->IsNumber(1)) {
+		c->Message(Chat::White, "Usage: #removeitem [Item ID] [Amount]");
+		return;
+	}
+
+	Client *target = c;
+	if (c->GetTarget() && c->GetTarget()->IsClient()) {
+		target = c->GetTarget()->CastToClient();
+	}
+
+	auto target_string = (
+		c == target ?
+		"yourself" :
+		fmt::format(
+			"{} ({})",
+			target->GetCleanName(),
+			target->GetID()
+		)
+	);
+	
+	auto item_id = std::stoi(sep->arg[1]);
+	if (!database.GetItem(item_id)) {
+		c->Message(
+			Chat::White,
+			fmt::format(
+				"Item ID {} could not be found.",
+				item_id
+			).c_str()
+		);
+		return;
+	}
+	
+	auto item_link = database.CreateItemLink(item_id);
+	auto amount = sep->IsNumber(2) ? std::stoul(sep->arg[2]) : 1;
+	auto item_count = target->CountItem(item_id);
+	if (item_count) {
+		if (item_count >= amount) {
+			target->RemoveItem(item_id, amount);
+
+			c->Message(
+				Chat::White,
+				fmt::format(
+					"Removed {} {} ({}) from {}.",
+					amount,
+					item_link,
+					item_id,
+					target_string
+				).c_str()
+			);
+		} else {
+			target->RemoveItem(item_id, item_count);
+
+			c->Message(
+				Chat::White,
+				fmt::format(
+					"Removed {} {} ({}) from {} because {} did not have {} {} ({}).",
+					item_count,
+					item_link,
+					item_id,
+					target_string,
+					c == target ? "you" : "they",
+					amount,
+					item_link,
+					item_id
+				).c_str()
+			);
+		}
+	} else {
+		c->Message(
+			Chat::White,
+			fmt::format(
+				"Could not find any {} ({}) to delete from {}.",
+				database.CreateItemLink(item_id),
+				item_id,
+				target_string
+			).c_str()
+		);
+	}
+}
+
