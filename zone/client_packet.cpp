75fbde7324402cbfcedaa52cf5d1e85271a74add
Natedog2012 <jwalters_06@yahoo.com>
2022-01-03 15:01:17 -0600

    Pickpocket distance is 15 constant according to Mackal

--- zone/client_packet.cpp
+++ zone/client_packet.cpp
@@ -11081,7 +11081,7 @@ void Client::Handle_OP_PickPocket(const EQApplicationPacket *app)
 	else if (victim->GetOwnerID()) {
 		Message(0, "You cannot steal from pets!");
 	}
-	else if (!CombatRange(victim, 3.0)) { //Give benefit of the doubt due to pathing NPCs
+	else if (Distance(GetPosition(), victim->GetPosition()) > 15) {
 		Message(Chat::Red, "Attempt to pickpocket out of range detected.");
 		database.SetMQDetectionFlag(this->AccountName(), this->GetName(), "OP_PickPocket was sent from outside combat range.", zone->GetShortName());
 	}
