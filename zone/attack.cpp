970075a97a71b70baf1b2c144db8e4631dcf0c2c
mmcgarvey <matt.mcgarvey01@gmail.com>
2022-01-03 16:55:20 -0500

    [Bugfix] XTarget Changes Causing Crashes
    Added nullptr check when processing XTargetAutoHaters for pet owner.

--- zone/attack.cpp
+++ zone/attack.cpp
@@ -2571,7 +2571,7 @@ bool NPC::Death(Mob* killer_mob, int32 damage, uint16 spell, EQ::skills::SkillTy
 
 		entity_list.RemoveFromAutoXTargets(this);
 
-		if (killer->GetUltimateOwner() && killer->GetUltimateOwner()->IsClient()) {
+		if (killer && killer->GetUltimateOwner() && killer->GetUltimateOwner()->IsClient()) {
 			killer->GetUltimateOwner()->CastToClient()->ProcessXTargetAutoHaters();
 		}
 		uint16 emoteid = this->GetEmoteID();
