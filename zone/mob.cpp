bd6deae9ef66b0bf85662269008daa7bc259ba83
KayenEQ <kayen85@gmail.com>
2022-02-13 22:30:25 -0500

    [Combat] /shield command "too far away message
    better chat

--- zone/mob.cpp
+++ zone/mob.cpp
@@ -6459,7 +6459,7 @@ bool Mob::ShieldAbility(uint32 target_id, int shielder_max_distance, int shield_
 	}
 
 	if (shield_target->CalculateDistance(GetX(), GetY(), GetZ()) > static_cast<float>(shielder_max_distance)) {
-		MessageString(Chat::Blue, TARGET_TOO_FAR);
+		MessageString(Chat::TooFarAway, TARGET_TOO_FAR);
 		return false; 
 	}
 
