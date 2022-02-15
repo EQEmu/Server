6303f129afb4b338ef0401686b75c1d0407dbd8a
Kinglykrab <89047260+Kinglykrab@users.noreply.github.com>
2022-02-14 19:02:27 -0500

    [Commands] Cleanup #setskillall Command. (#1992)
    - Cleanup messages and logic.

--- zone/gm_commands/setskillall.cpp
+++ zone/gm_commands/setskillall.cpp
@@ -2,28 +2,50 @@
 
 void command_setskillall(Client *c, const Seperator *sep)
 {
-	if (c->GetTarget() == 0) {
-		c->Message(Chat::White, "Error: #setallskill: No target.");
+	auto target = c;
+	if (c->GetTarget() && c->GetTarget()->IsClient()) {
+		target = c->GetTarget()->CastToClient();
 	}
-	else if (!c->GetTarget()->IsClient()) {
-		c->Message(Chat::White, "Error: #setskill: Target must be a client.");
-	}
-	else if (!sep->IsNumber(1) || atoi(sep->arg[1]) < 0 || atoi(sep->arg[1]) > HIGHEST_CAN_SET_SKILL) {
-		c->Message(Chat::White, "Usage: #setskillall value ");
-		c->Message(Chat::White, "       value = 0 to %d", HIGHEST_CAN_SET_SKILL);
-	}
-	else {
-		if (c->Admin() >= commandSetSkillsOther || c->GetTarget() == c || c->GetTarget() == 0) {
-			LogInfo("Set ALL skill request from [{}], target:[{}]", c->GetName(), c->GetTarget()->GetName());
-			uint16                     level     = atoi(sep->arg[1]);
-			for (EQ::skills::SkillType skill_num = EQ::skills::Skill1HBlunt;
-				skill_num <= EQ::skills::HIGHEST_SKILL;
-				skill_num = (EQ::skills::SkillType) (skill_num + 1)) {
-				c->GetTarget()->CastToClient()->SetSkill(skill_num, level);
+
+	if (!sep->IsNumber(1)) {
+		c->Message(Chat::White, "Usage: #setskillall [Skill Level] - Set all of your or your target's skills to the specified skill level");
+		c->Message(
+			Chat::White,
+			fmt::format(
+				"Note: Skill Level ranges from 0 to {}",
+				HIGHEST_CAN_SET_SKILL
+			).c_str()
+		);
+	} else {
+		if (c->Admin() >= commandSetSkillsOther || c->GetTarget() == c) {
+			LogInfo(
+				"Set ALL skill request from [{}], target:[{}]",
+				c->GetCleanName(),
+				target->GetCleanName()
+			);
+
+			auto skill_level = static_cast<uint16>(std::stoul(sep->arg[1]));
+
+			c->Message(
+				Chat::White,
+				fmt::format(
+					"Setting all skills to {} for {}.",
+					skill_level,
+					c == target ?
+					"yourself" :
+					fmt::format(
+						"{} ({})",
+						target->GetCleanName(),
+						target->GetID()
+					)
+				).c_str()
+			);
+
+			for (EQ::skills::SkillType skill_num = EQ::skills::Skill1HBlunt; skill_num <= EQ::skills::HIGHEST_SKILL; skill_num = (EQ::skills::SkillType) (skill_num + 1)) {
+				target->SetSkill(skill_num, skill_level);
 			}
-		}
-		else {
-			c->Message(Chat::White, "Error: Your status is not high enough to set anothers skills");
+		} else {
+			c->Message(Chat::White, "Your status is not high enough to set another player's skills.");
 		}
 	}
 }
