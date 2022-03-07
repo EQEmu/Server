25a19a13dce6beb886d4ed4b36bbb5e1ac1db764
Chris Miles <akkadius1@gmail.com>
2022-03-06 20:02:33 -0800

    [Database] Add Primary ID Keys to Tables (#2036)

--- common/version.h
+++ common/version.h
@@ -34,7 +34,7 @@
  * Manifest: https://github.com/EQEmu/Server/blob/master/utils/sql/db_update_manifest.txt
  */
 
-#define CURRENT_BINARY_DATABASE_VERSION 9176
+#define CURRENT_BINARY_DATABASE_VERSION 9177
 
 #ifdef BOTS
 	#define CURRENT_BINARY_BOTS_DATABASE_VERSION 9028
