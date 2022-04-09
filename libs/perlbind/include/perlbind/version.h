a2c6252c58d0875246316c5ab06e61148b94d8bb
hg <4683435+hgtw@users.noreply.github.com>
2022-04-09 19:54:38 -0400

    Add perlbind library

---
+++ libs/perlbind/include/perlbind/version.h
@@ -0,0 +1,10 @@
+#pragma once
+
+constexpr int perlbind_version_major = 1;
+constexpr int perlbind_version_minor = 0;
+constexpr int perlbind_version_patch = 0;
+
+constexpr int perlbind_version()
+{
+  return perlbind_version_major * 10000 + perlbind_version_minor * 100 + perlbind_version_patch;
+}
