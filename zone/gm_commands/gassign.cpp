c79629db861cc7d45c631b359a4865a9d09ffecb
Kinglykrab <kinglykrab@gmail.com>
2022-05-03 20:22:11 -0400

    [Commands] Cleanup #gassign Command. - Cleanup messages and logic.

--- zone/gm_commands/gassign.cpp
+++ zone/gm_commands/gassign.cpp
@@ -2,13 +2,22 @@
 
 void command_gassign(Client *c, const Seperator *sep)
 {
-	if (sep->IsNumber(1) && c->GetTarget() && c->GetTarget()->IsNPC() &&
-		c->GetTarget()->CastToNPC()->GetSpawnPointID() > 0) {
-		int spawn2id = c->GetTarget()->CastToNPC()->GetSpawnPointID();
-		database.AssignGrid(c, atoi(sep->arg[1]), spawn2id);
+	if (!sep->IsNumber(1)) {
+		c->Message(Chat::White, "Usage: #gassign [Grid ID]");
+		return;
 	}
-	else {
-		c->Message(Chat::White, "Usage: #gassign [num] - must have an npc target!");
+
+	auto grid_id = std::stoul(sep->arg[1]);
+
+	if (!c->GetTarget() || !c->GetTarget()->IsNPC()) {
+		c->Message(Chat::White, "You must target an NPC to use this command.");
+		return;
+	}
+
+	auto target = c->GetTarget()->CastToNPC();
+	if (target->GetSpawnPointID() > 0) {
+		auto spawn2_id = target->GetSpawnPointID();
+		database.AssignGrid(c, grid_id, target->GetID());
 	}
 }
 
