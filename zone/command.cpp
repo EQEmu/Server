bec0365b69a13b7aeb9fc452c7097ad12ec7bdf9
Kinglykrab <kinglykrab@gmail.com>
2022-05-03 20:55:43 -0400

    [Commands] Cleanup #attack Command. - Cleanup messages and logic.

--- zone/command.cpp
+++ zone/command.cpp
@@ -122,7 +122,7 @@ int command_init(void)
 		command_add("appearance", "[type] [value] - Send an appearance packet for you or your target", AccountStatus::GMLeadAdmin, command_appearance) ||
 		command_add("appearanceeffects", "- [view] [set] [remove] appearance effects.", AccountStatus::GMAdmin, command_appearanceeffects) ||
 		command_add("apply_shared_memory", "[shared_memory_name] - Tells every zone and world to apply a specific shared memory segment by name.", AccountStatus::GMImpossible, command_apply_shared_memory) ||
-		command_add("attack", "[targetname] - Make your NPC target attack targetname", AccountStatus::GMLeadAdmin, command_attack) ||
+		command_add("attack", "[Entity Name] - Make your NPC target attack an entity by name", AccountStatus::GMLeadAdmin, command_attack) ||
 		command_add("augmentitem",  "Force augments an item. Must have the augment item window open.", AccountStatus::GMImpossible, command_augmentitem) ||
 		command_add("ban", "[name] [reason]- Ban by character name", AccountStatus::GMLeadAdmin, command_ban) ||
 		command_add("beard", "- Change the beard of your target", AccountStatus::QuestTroupe, command_beard) ||
