c80a3b96c9c8cb64aecc11a4726e8647005eaa56
Kinglykrab <89047260+Kinglykrab@users.noreply.github.com>
2022-05-04 22:32:39 -0400

    Update gassign.cpp

--- zone/gm_commands/gassign.cpp
+++ zone/gm_commands/gassign.cpp
@@ -2,17 +2,18 @@
 
 void command_gassign(Client *c, const Seperator *sep)
 {
-	if (!sep->IsNumber(1)) {
+	if (!c->GetTarget() || !c->GetTarget()->IsNPC()) {
+		c->Message(Chat::White, "You must target an NPC to use this command.");
+		return;
+	}
+
+	int arguments = sep->argnum;
+	if (!arguments || !sep->IsNumber(1)) {
 		c->Message(Chat::White, "Usage: #gassign [Grid ID]");
 		return;
 	}
 
 	auto grid_id = std::stoul(sep->arg[1]);
-
-	if (!c->GetTarget() || !c->GetTarget()->IsNPC()) {
-		c->Message(Chat::White, "You must target an NPC to use this command.");
-		return;
-	}
 
 	auto target = c->GetTarget()->CastToNPC();
 	if (target->GetSpawnPointID() > 0) {
