1c5b385a253ffd4a5c591799c39da88f2533a09e
Kinglykrab <kinglykrab@gmail.com>
2022-05-06 19:25:47 -0400

    [Commands] Cleanup #ucs Command. - Cleanup messages and logic.

--- zone/gm_commands/ucs.cpp
+++ zone/gm_commands/ucs.cpp
@@ -2,88 +2,87 @@
 
 void command_ucs(Client *c, const Seperator *sep)
 {
-	if (!c) {
+	LogInfo(
+		"Character [{}] attempting UCS reconnect while UCS server is {}available.",
+		c->GetName(),
+		zone->IsUCSServerAvailable() ? "" : "un"
+	);
+
+	if (!zone->IsUCSServerAvailable()) {
+		c->Message(Chat::White, "The UCS is currently unavailable.");
 		return;
 	}
 
-	LogInfo("Character [{}] attempting ucs reconnect while ucs server is [{}] available",
-			c->GetName(), (zone->IsUCSServerAvailable() ? "" : "un"));
+	EQApplicationPacket *outapp = nullptr;
+	std::string buffer;
 
-	if (zone->IsUCSServerAvailable()) {
-		EQApplicationPacket *outapp = nullptr;
-		std::string         buffer;
+	std::string mail_key = database.GetMailKey(c->CharacterID(), true);
+	EQ::versions::UCSVersion connection_type = EQ::versions::ucsUnknown;
 
-		std::string              MailKey        = database.GetMailKey(c->CharacterID(), true);
-		EQ::versions::UCSVersion ConnectionType = EQ::versions::ucsUnknown;
-
-		// chat server packet
-		switch (c->ClientVersion()) {
-			case EQ::versions::ClientVersion::Titanium:
-				ConnectionType = EQ::versions::ucsTitaniumChat;
-				break;
-			case EQ::versions::ClientVersion::SoF:
-				ConnectionType = EQ::versions::ucsSoFCombined;
-				break;
-			case EQ::versions::ClientVersion::SoD:
-				ConnectionType = EQ::versions::ucsSoDCombined;
-				break;
-			case EQ::versions::ClientVersion::UF:
-				ConnectionType = EQ::versions::ucsUFCombined;
-				break;
-			case EQ::versions::ClientVersion::RoF:
-				ConnectionType = EQ::versions::ucsRoFCombined;
-				break;
-			case EQ::versions::ClientVersion::RoF2:
-				ConnectionType = EQ::versions::ucsRoF2Combined;
-				break;
-			default:
-				ConnectionType = EQ::versions::ucsUnknown;
-				break;
-		}
-
-		buffer = StringFormat(
-			"%s,%i,%s.%s,%c%s",
-			Config->ChatHost.c_str(),
-			Config->ChatPort,
-			Config->ShortName.c_str(),
-			c->GetName(),
-			ConnectionType,
-			MailKey.c_str()
-		);
-
-		outapp = new EQApplicationPacket(OP_SetChatServer, (buffer.length() + 1));
-		memcpy(outapp->pBuffer, buffer.c_str(), buffer.length());
-		outapp->pBuffer[buffer.length()] = '\0';
-
-		c->QueuePacket(outapp);
-		safe_delete(outapp);
-
-		// mail server packet
-		switch (c->ClientVersion()) {
-			case EQ::versions::ClientVersion::Titanium:
-				ConnectionType = EQ::versions::ucsTitaniumMail;
-				break;
-			default:
-				// retain value from previous switch
-				break;
-		}
-
-		buffer = StringFormat(
-			"%s,%i,%s.%s,%c%s",
-			Config->MailHost.c_str(),
-			Config->MailPort,
-			Config->ShortName.c_str(),
-			c->GetName(),
-			ConnectionType,
-			MailKey.c_str()
-		);
-
-		outapp = new EQApplicationPacket(OP_SetChatServer2, (buffer.length() + 1));
-		memcpy(outapp->pBuffer, buffer.c_str(), buffer.length());
-		outapp->pBuffer[buffer.length()] = '\0';
-
-		c->QueuePacket(outapp);
-		safe_delete(outapp);
+	switch (c->ClientVersion()) {
+		case EQ::versions::ClientVersion::Titanium:
+			connection_type = EQ::versions::ucsTitaniumChat;
+			break;
+		case EQ::versions::ClientVersion::SoF:
+			connection_type = EQ::versions::ucsSoFCombined;
+			break;
+		case EQ::versions::ClientVersion::SoD:
+			connection_type = EQ::versions::ucsSoDCombined;
+			break;
+		case EQ::versions::ClientVersion::UF:
+			connection_type = EQ::versions::ucsUFCombined;
+			break;
+		case EQ::versions::ClientVersion::RoF:
+			connection_type = EQ::versions::ucsRoFCombined;
+			break;
+		case EQ::versions::ClientVersion::RoF2:
+			connection_type = EQ::versions::ucsRoF2Combined;
+			break;
+		default:
+			connection_type = EQ::versions::ucsUnknown;
+			break;
 	}
+
+	buffer = fmt::format(
+		"{},{},{}.{},{}{}",
+		Config->ChatHost,
+		Config->ChatPort,
+		Config->ShortName,
+		c->GetName(),
+		connection_type,
+		mail_key
+	);
+
+	outapp = new EQApplicationPacket(OP_SetChatServer, (buffer.length() + 1));
+	memcpy(outapp->pBuffer, buffer.c_str(), buffer.length());
+	outapp->pBuffer[buffer.length()] = '\0';
+
+	c->QueuePacket(outapp);
+	safe_delete(outapp);
+
+	switch (c->ClientVersion()) {
+		case EQ::versions::ClientVersion::Titanium:
+			connection_type = EQ::versions::ucsTitaniumMail;
+			break;
+		default:
+			break;
+	}
+
+	buffer = StringFormat(
+		"{},{},{}.{},{}{}",
+		Config->MailHost,
+		Config->MailPort,
+		Config->ShortName,
+		c->GetName(),
+		connection_type,
+		mail_key
+	);
+
+	outapp = new EQApplicationPacket(OP_SetChatServer2, (buffer.length() + 1));
+	memcpy(outapp->pBuffer, buffer.c_str(), buffer.length());
+	outapp->pBuffer[buffer.length()] = '\0';
+
+	c->QueuePacket(outapp);
+	safe_delete(outapp);
 }
 
