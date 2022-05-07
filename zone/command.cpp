7549fbbeeab1ba79f6c198288c20b2e1a0e581c1
Kinglykrab <89047260+Kinglykrab@users.noreply.github.com>
2022-05-06 22:31:17 -0400

    [Commands] Cleanup #reloadlevelmods Command. (#2122)
    * [Commands] Cleanup #reloadlevelmods Command.
    - Cleanup messages and logic.
    - Make the reloading of modifiers global instead of zone-specific.
    
    * Remove unnecessary message.
    
    * Update worldserver.cpp
    
    * Update worldserver.cpp
    
    * Update worldserver.cpp

--- zone/command.cpp
+++ zone/command.cpp
@@ -287,7 +287,7 @@ int command_init(void)
 		command_add("reloadallrules", "Executes a reload of all rules globally.", AccountStatus::QuestTroupe, command_reloadallrules) ||
 		command_add("reloadcontentflags", "Executes a reload of all expansion and content flags", AccountStatus::QuestTroupe, command_reloadcontentflags) ||
 		command_add("reloademote", "Reloads NPC Emotes", AccountStatus::QuestTroupe, command_reloademote) ||
-		command_add("reloadlevelmods", nullptr, AccountStatus::Max, command_reloadlevelmods) ||
+		command_add("reloadlevelmods", "Reloads level based experience modifiers globally", AccountStatus::Max, command_reloadlevelmods) ||
 		command_add("reloadmerchants", "Reloads merchant lists globally", AccountStatus::Max, command_reloadmerchants) ||
 		command_add("reloadperlexportsettings", "Reloads Perl event export settings globally", AccountStatus::Max, command_reloadperlexportsettings) ||
 		command_add("reloadqst", " - Clear quest cache (any argument causes it to also stop all timers)", AccountStatus::GMLeadAdmin, command_reloadqst) ||
