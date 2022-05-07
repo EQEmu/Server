02828a73b8dae94ea7210ea131dbc7156b3a98a8
Kinglykrab <89047260+Kinglykrab@users.noreply.github.com>
2022-05-06 22:06:12 -0400

    [Commands] Cleanup #npcspecialattk Command. (#2108)
    * [Commands] Cleanup #npcspecialattk Command.
    - Cleanup messages and logic.
    
    * Remove command.

--- zone/command.cpp
+++ zone/command.cpp
@@ -255,7 +255,6 @@ int command_init(void)
 		command_add("npcsay", "[Message] - Make your NPC target say a message.", AccountStatus::GMLeadAdmin, command_npcsay) ||
 		command_add("npcshout", "[Message] - Make your NPC target shout a message.", AccountStatus::GMLeadAdmin, command_npcshout) ||
 		command_add("npcspawn", "[create/add/update/remove/delete] - Manipulate spawn DB", AccountStatus::GMAreas, command_npcspawn) ||
-		command_add("npcspecialattk", "[flagchar] [perm] - Set NPC special attack flags. Flags are E(nrage) F(lurry) R(ampage) S(ummon).", AccountStatus::QuestTroupe, command_npcspecialattk) ||
 		command_add("npcstats", "- Show stats about target NPC", AccountStatus::QuestTroupe, command_npcstats) ||
 		command_add("npctype_cache",  "[id] or all - Clears the npc type cache for either the id or all npcs.", AccountStatus::GMImpossible, command_npctype_cache) ||
 		command_add("npctypespawn", "[NPC ID] [Faction ID] - Spawn an NPC by ID from the database with an option of setting its Faction ID", AccountStatus::Steward, command_npctypespawn) ||
@@ -1299,7 +1298,6 @@ void command_bot(Client *c, const Seperator *sep)
 #include "gm_commands/npcsay.cpp"
 #include "gm_commands/npcshout.cpp"
 #include "gm_commands/npcspawn.cpp"
-#include "gm_commands/npcspecialattk.cpp"
 #include "gm_commands/npcstats.cpp"
 #include "gm_commands/npctype_cache.cpp"
 #include "gm_commands/npctypespawn.cpp"
