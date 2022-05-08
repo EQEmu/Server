375429557163fab86b257d35212d7daf0c84659f
Chris Miles <akkadius1@gmail.com>
2022-05-08 00:15:07 -0500

    Update 2022_05_07_discord_webhooks.sql

--- utils/sql/git/required/2022_05_07_discord_webhooks.sql
+++ utils/sql/git/required/2022_05_07_discord_webhooks.sql
@@ -12,4 +12,4 @@ COLLATE=utf8mb4_general_ci;
 ALTER TABLE logsys_categories
     ADD log_to_discord smallint(11) default 0 AFTER log_to_gmsay;
 ALTER TABLE logsys_categories
-    ADD discord_webhook_id int(11) AFTER log_to_discord;
+    ADD discord_webhook_id int(11) default 0 AFTER log_to_discord;
