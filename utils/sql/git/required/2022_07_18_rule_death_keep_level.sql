2892fad48c5ca3770509cb15bf6ef88a941dd060
Trent Miller <trentdm@gmail.com>
2022-07-18 10:08:52 -0600

    add sql rule

---
+++ utils/sql/git/required/2022_07_18_rule_death_keep_level.sql
@@ -0,0 +1,2 @@
+INSERT INTO `rule_values` (`ruleset_id`, `rule_name`, `rule_value`, `notes`)
+VALUES ('1', 'Character:DeathKeepLevel', 'false', 'Players can not drop below 0% experience from death.');
