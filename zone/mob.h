16d25e2250a03612115c16022e61e499604992fb
dmcintosh-BCT <dan@blackcreektechnologies.com>
2022-07-22 22:46:35 -0400

    Revert "Change GetSkillDmgAmt from Int16 to Int32 (SkillDamageAmount2 which uses function is Int32)"
    This reverts commit cf5dbc9ce81d6e9362ba658a999faab36abf237a.

--- zone/mob.h
+++ zone/mob.h
@@ -883,7 +883,7 @@ public:
 	int16 GetMeleeDmgPositionMod(Mob* defender);
 	int16 GetSkillReuseTime(uint16 skill);
 	int GetCriticalChanceBonus(uint16 skill);
-	int32 GetSkillDmgAmt(uint16 skill);
+	int16 GetSkillDmgAmt(uint16 skill);
 	int16 GetPositionalDmgAmt(Mob* defender);
 	inline bool CanBlockSpell() const { return(spellbonuses.FocusEffects[focusBlockNextSpell]); }
 	bool DoHPToManaCovert(int32 mana_cost = 0);
