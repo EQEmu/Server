6e2d11a2830d0dd0fa297415b66c55bac1465751
hg <4683435+hgtw@users.noreply.github.com>
2022-07-30 14:33:53 -0400

    [Tasks] Support Raw NPC Names in Task Goal List (#2333)

--- zone/attack.cpp
+++ zone/attack.cpp
@@ -2457,7 +2457,7 @@ bool NPC::Death(Mob* killer_mob, int64 damage, uint16 spell, EQ::skills::SkillTy
 				give_exp_client->GetCleanName(),
 				GetNPCTypeID()
 			);
-			task_manager->HandleUpdateTasksOnKill(give_exp_client, GetNPCTypeID(), GetCleanName());
+			task_manager->HandleUpdateTasksOnKill(give_exp_client, GetNPCTypeID(), this);
 		}
 
 		if (kr) {
