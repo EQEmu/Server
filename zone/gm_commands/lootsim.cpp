81d6f9c5557e0c5411e625d2f6787dabb4e014a4
Akkadius <akkadius1@gmail.com>
2022-08-14 00:37:45 -0500

    Add global loot

--- zone/gm_commands/lootsim.cpp
+++ zone/gm_commands/lootsim.cpp
@@ -28,14 +28,19 @@ void command_lootsim(Client *c, const Seperator *sep)
 			npc->SetRecordLootStats(true);
 			for (int i = 0; i < iterations; i++) {
 				npc->AddLootTable(loottable_id);
+
+				for (auto &id: zone->GetGlobalLootTables(npc)) {
+					npc->AddLootTable(id);
+				}
 			}
 
 			entity_list.AddNPC(npc);
 
+			c->SendChatLineBreak();
 			c->Message(
 				Chat::White,
 				fmt::format(
-					"# [Loot Simulator] NPC [{}] ({}) LootTableID [{}] Dropped Items [{}] iterations [{}]",
+					"# [Loot Simulator] NPC [{}] ({}) Loot Table ID [{}] Dropped Items [{}] iterations [{}]",
 					npc->GetCleanName(),
 					npc_id,
 					loottable_id,
@@ -43,7 +48,9 @@ void command_lootsim(Client *c, const Seperator *sep)
 					iterations
 				).c_str()
 			);
+			c->SendChatLineBreak();
 
+			// npc level loot table
 			auto loot_table = database.GetLootTable(loottable_id);
 			if (!loot_table) {
 				c->Message(Chat::Red, "Loot table not found");
@@ -96,26 +103,96 @@ void command_lootsim(Client *c, const Seperator *sep)
 				}
 			}
 
+
+			// global loot
+			auto tables = zone->GetGlobalLootTables(npc);
+			if (!tables.empty()) {
+				c->SendChatLineBreak();
+				c->Message(Chat::White, "# [Loot Simulator] Global Loot");
+			}
+
+			for (auto &id: tables) {
+				c->SendChatLineBreak();
+				c->Message(Chat::White, fmt::format("# Global Loot Table ID [{}]", id).c_str());
+				c->SendChatLineBreak();
+
+				loot_table = database.GetLootTable(id);
+				if (!loot_table) {
+					c->Message(Chat::Red, fmt::format("Global Loot table not found [{}]", id).c_str());
+					continue;
+				}
+
+				for (uint32 i = 0; i < loot_table->NumEntries; i++) {
+					auto le = loot_table->Entries[i];
+
+					c->Message(
+						Chat::White,
+						fmt::format(
+							"# Lootdrop ID [{}] drop_limit [{}] min_drop [{}] mult [{}] probability [{}]",
+							le.lootdrop_id,
+							le.droplimit,
+							le.mindrop,
+							le.multiplier,
+							le.probability
+						).c_str()
+					);
+
+					auto loot_drop = database.GetLootDrop(le.lootdrop_id);
+					if (!loot_drop) {
+						continue;
+					}
+
+					for (uint32 ei = 0; ei < loot_drop->NumEntries; ei++) {
+						auto               e            = loot_drop->Entries[ei];
+						int                rolled_count = npc->GetRolledItemCount(e.item_id);
+						const EQ::ItemData *item        = database.GetItem(e.item_id);
+
+						EQ::SayLinkEngine linker;
+						linker.SetLinkType(EQ::saylink::SayLinkItemData);
+						linker.SetItemData(item);
+
+						auto rolled_percentage = (float) ((float) ((float) rolled_count / (float) iterations) * 100);
+
+						c->Message(
+							Chat::White,
+							fmt::format(
+								"-- [{}] item_id [{}] chance [{}] rolled_count [{}] ({:.2f}%) name [{}]",
+								ei,
+								e.item_id,
+								e.chance,
+								rolled_count,
+								rolled_percentage,
+								linker.GenerateLink()
+							).c_str()
+						);
+					}
+				}
+			}
+
+
+			c->SendChatLineBreak();
 			c->Message(
 				Chat::White,
 				fmt::format(
-					"# [{}] iterations took [{}]",
+					"# Global Loot Benchmark End [{}] iterations took [{}](s)",
 					iterations,
 					benchmark.elapsed()
 				).c_str()
 			);
+			c->SendChatLineBreak();
 
 			LogSys.LoadLogDatabaseSettings();
 		}
-		else {
-			c->Message(
-				Chat::White,
-				fmt::format(
-					"Failed to spawn NPC ID {}.",
-					npc_id
-				).c_str()
-			);
-		}
+	}
+	else {
+		c->Message(
+			Chat::White,
+			fmt::format(
+				"Failed to spawn NPC ID {}.",
+				npc_id
+			).c_str()
+		);
 	}
 }
+
 
