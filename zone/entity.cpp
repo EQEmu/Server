0ef98a54a037cb2ce32c663d2c26b80f70edb5b9
Akkadius <akkadius1@gmail.com>
2022-09-28 16:38:06 -0700

    Debugging

--- zone/entity.cpp
+++ zone/entity.cpp
@@ -4259,14 +4259,7 @@ bool EntityList::LimitCheckName(const char *npc_name)
 	while (it != npc_list.end()) {
 		NPC *npc = it->second;
 		if (npc) {
-			LogInfo("NPC [LimitCheckName] name [{}]", npc_name);
-			LogInfo("NPC [LimitCheckName] ours [{}]", npc->NPCTypedata_ours ? "true" : "false");
-			LogInfo("NPC [LimitCheckName] ndata [{}]", npc->NPCTypedata ? "true" : "false");
-			LogInfo("NPC [LimitCheckName] ndata name [{}]", npc->NPCTypedata->name);
-			LogInfo("NPC [LimitCheckName] raw [{}]", npc->GetRawNPCTypeName());
-		
-
-			if (strcasecmp(npc_name, npc->GetRawNPCTypeName()) == 0) {
+			if (strcasecmp(npc_name, npc->GetName()) == 0) {
 				return false;
 			}
 		}
