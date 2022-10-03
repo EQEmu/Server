c5538c3003dcf172e2899a19bf2187c21ee2cb73
Aeadoin <buvan00@gmail.com>
2022-10-03 16:50:11 -0400

    [Bug Fix] Touch Of Vinitras was ignoring pet DT rule

--- zone/spells.cpp
+++ zone/spells.cpp
@@ -2270,7 +2270,7 @@ bool Mob::SpellFinished(uint16 spell_id, Mob *spell_target, CastingSlot slot, in
 		return false;
 
 	//Death Touch targets the pet owner instead of the pet when said pet is tanking.
-	if ((RuleB(Spells, CazicTouchTargetsPetOwner) && spell_target && spell_target->HasOwner()) && spell_id == SPELL_CAZIC_TOUCH || spell_id == SPELL_TOUCH_OF_VINITRAS) {
+	if ((RuleB(Spells, CazicTouchTargetsPetOwner) && spell_target && spell_target->HasOwner()) && (spell_id == SPELL_CAZIC_TOUCH || spell_id == SPELL_TOUCH_OF_VINITRAS)) {
 		Mob* owner =  spell_target->GetOwner();
 
 		if (owner) {
