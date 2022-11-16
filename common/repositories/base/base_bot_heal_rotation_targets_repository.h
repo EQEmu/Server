6ff52f94c4a6ffd88772b390ab68c9a9edbfd70e
Kinglykrab <89047260+Kinglykrab@users.noreply.github.com>
2022-11-16 08:15:48 -0500

    [Repositories] Add Bot Repositories. (#2529)
    * [Repositories] Add Bot Repositories.
    
    * Remove unnecessary table.
    
    * Add back table.

---
+++ common/repositories/base/base_bot_heal_rotation_targets_repository.h
@@ -0,0 +1,342 @@
+/**
+ * DO NOT MODIFY THIS FILE
+ *
+ * This repository was automatically generated and is NOT to be modified directly.
+ * Any repository modifications are meant to be made to the repository extending the base.
+ * Any modifications to base repositories are to be made by the generator only
+ *
+ * @generator ./utils/scripts/generators/repository-generator.pl
+ * @docs https://eqemu.gitbook.io/server/in-development/developer-area/repositories
+ */
+
+#ifndef EQEMU_BASE_BOT_HEAL_ROTATION_TARGETS_REPOSITORY_H
+#define EQEMU_BASE_BOT_HEAL_ROTATION_TARGETS_REPOSITORY_H
+
+#include "../../database.h"
+#include "../../strings.h"
+#include <ctime>
+
+class BaseBotHealRotationTargetsRepository {
+public:
+	struct BotHealRotationTargets {
+		uint32_t    target_index;
+		uint32_t    heal_rotation_index;
+		std::string target_name;
+	};
+
+	static std::string PrimaryKey()
+	{
+		return std::string("target_index");
+	}
+
+	static std::vector<std::string> Columns()
+	{
+		return {
+			"target_index",
+			"heal_rotation_index",
+			"target_name",
+		};
+	}
+
+	static std::vector<std::string> SelectColumns()
+	{
+		return {
+			"target_index",
+			"heal_rotation_index",
+			"target_name",
+		};
+	}
+
+	static std::string ColumnsRaw()
+	{
+		return std::string(Strings::Implode(", ", Columns()));
+	}
+
+	static std::string SelectColumnsRaw()
+	{
+		return std::string(Strings::Implode(", ", SelectColumns()));
+	}
+
+	static std::string TableName()
+	{
+		return std::string("bot_heal_rotation_targets");
+	}
+
+	static std::string BaseSelect()
+	{
+		return fmt::format(
+			"SELECT {} FROM {}",
+			SelectColumnsRaw(),
+			TableName()
+		);
+	}
+
+	static std::string BaseInsert()
+	{
+		return fmt::format(
+			"INSERT INTO {} ({}) ",
+			TableName(),
+			ColumnsRaw()
+		);
+	}
+
+	static BotHealRotationTargets NewEntity()
+	{
+		BotHealRotationTargets e{};
+
+		e.target_index        = 0;
+		e.heal_rotation_index = 0;
+		e.target_name         = "";
+
+		return e;
+	}
+
+	static BotHealRotationTargets GetBotHealRotationTargets(
+		const std::vector<BotHealRotationTargets> &bot_heal_rotation_targetss,
+		int bot_heal_rotation_targets_id
+	)
+	{
+		for (auto &bot_heal_rotation_targets : bot_heal_rotation_targetss) {
+			if (bot_heal_rotation_targets.target_index == bot_heal_rotation_targets_id) {
+				return bot_heal_rotation_targets;
+			}
+		}
+
+		return NewEntity();
+	}
+
+	static BotHealRotationTargets FindOne(
+		Database& db,
+		int bot_heal_rotation_targets_id
+	)
+	{
+		auto results = db.QueryDatabase(
+			fmt::format(
+				"{} WHERE id = {} LIMIT 1",
+				BaseSelect(),
+				bot_heal_rotation_targets_id
+			)
+		);
+
+		auto row = results.begin();
+		if (results.RowCount() == 1) {
+			BotHealRotationTargets e{};
+
+			e.target_index        = static_cast<uint32_t>(strtoul(row[0], nullptr, 10));
+			e.heal_rotation_index = static_cast<uint32_t>(strtoul(row[1], nullptr, 10));
+			e.target_name         = row[2] ? row[2] : "";
+
+			return e;
+		}
+
+		return NewEntity();
+	}
+
+	static int DeleteOne(
+		Database& db,
+		int bot_heal_rotation_targets_id
+	)
+	{
+		auto results = db.QueryDatabase(
+			fmt::format(
+				"DELETE FROM {} WHERE {} = {}",
+				TableName(),
+				PrimaryKey(),
+				bot_heal_rotation_targets_id
+			)
+		);
+
+		return (results.Success() ? results.RowsAffected() : 0);
+	}
+
+	static int UpdateOne(
+		Database& db,
+		const BotHealRotationTargets &e
+	)
+	{
+		std::vector<std::string> v;
+
+		auto columns = Columns();
+
+		v.push_back(columns[1] + " = " + std::to_string(e.heal_rotation_index));
+		v.push_back(columns[2] + " = '" + Strings::Escape(e.target_name) + "'");
+
+		auto results = db.QueryDatabase(
+			fmt::format(
+				"UPDATE {} SET {} WHERE {} = {}",
+				TableName(),
+				Strings::Implode(", ", v),
+				PrimaryKey(),
+				e.target_index
+			)
+		);
+
+		return (results.Success() ? results.RowsAffected() : 0);
+	}
+
+	static BotHealRotationTargets InsertOne(
+		Database& db,
+		BotHealRotationTargets e
+	)
+	{
+		std::vector<std::string> v;
+
+		v.push_back(std::to_string(e.target_index));
+		v.push_back(std::to_string(e.heal_rotation_index));
+		v.push_back("'" + Strings::Escape(e.target_name) + "'");
+
+		auto results = db.QueryDatabase(
+			fmt::format(
+				"{} VALUES ({})",
+				BaseInsert(),
+				Strings::Implode(",", v)
+			)
+		);
+
+		if (results.Success()) {
+			e.target_index = results.LastInsertedID();
+			return e;
+		}
+
+		e = NewEntity();
+
+		return e;
+	}
+
+	static int InsertMany(
+		Database& db,
+		const std::vector<BotHealRotationTargets> &entries
+	)
+	{
+		std::vector<std::string> insert_chunks;
+
+		for (auto &e: entries) {
+			std::vector<std::string> v;
+
+			v.push_back(std::to_string(e.target_index));
+			v.push_back(std::to_string(e.heal_rotation_index));
+			v.push_back("'" + Strings::Escape(e.target_name) + "'");
+
+			insert_chunks.push_back("(" + Strings::Implode(",", v) + ")");
+		}
+
+		std::vector<std::string> v;
+
+		auto results = db.QueryDatabase(
+			fmt::format(
+				"{} VALUES {}",
+				BaseInsert(),
+				Strings::Implode(",", insert_chunks)
+			)
+		);
+
+		return (results.Success() ? results.RowsAffected() : 0);
+	}
+
+	static std::vector<BotHealRotationTargets> All(Database& db)
+	{
+		std::vector<BotHealRotationTargets> all_entries;
+
+		auto results = db.QueryDatabase(
+			fmt::format(
+				"{}",
+				BaseSelect()
+			)
+		);
+
+		all_entries.reserve(results.RowCount());
+
+		for (auto row = results.begin(); row != results.end(); ++row) {
+			BotHealRotationTargets e{};
+
+			e.target_index        = static_cast<uint32_t>(strtoul(row[0], nullptr, 10));
+			e.heal_rotation_index = static_cast<uint32_t>(strtoul(row[1], nullptr, 10));
+			e.target_name         = row[2] ? row[2] : "";
+
+			all_entries.push_back(e);
+		}
+
+		return all_entries;
+	}
+
+	static std::vector<BotHealRotationTargets> GetWhere(Database& db, const std::string &where_filter)
+	{
+		std::vector<BotHealRotationTargets> all_entries;
+
+		auto results = db.QueryDatabase(
+			fmt::format(
+				"{} WHERE {}",
+				BaseSelect(),
+				where_filter
+			)
+		);
+
+		all_entries.reserve(results.RowCount());
+
+		for (auto row = results.begin(); row != results.end(); ++row) {
+			BotHealRotationTargets e{};
+
+			e.target_index        = static_cast<uint32_t>(strtoul(row[0], nullptr, 10));
+			e.heal_rotation_index = static_cast<uint32_t>(strtoul(row[1], nullptr, 10));
+			e.target_name         = row[2] ? row[2] : "";
+
+			all_entries.push_back(e);
+		}
+
+		return all_entries;
+	}
+
+	static int DeleteWhere(Database& db, const std::string &where_filter)
+	{
+		auto results = db.QueryDatabase(
+			fmt::format(
+				"DELETE FROM {} WHERE {}",
+				TableName(),
+				where_filter
+			)
+		);
+
+		return (results.Success() ? results.RowsAffected() : 0);
+	}
+
+	static int Truncate(Database& db)
+	{
+		auto results = db.QueryDatabase(
+			fmt::format(
+				"TRUNCATE TABLE {}",
+				TableName()
+			)
+		);
+
+		return (results.Success() ? results.RowsAffected() : 0);
+	}
+
+	static int64 GetMaxId(Database& db)
+	{
+		auto results = db.QueryDatabase(
+			fmt::format(
+				"SELECT COALESCE(MAX({}), 0) FROM {}",
+				PrimaryKey(),
+				TableName()
+			)
+		);
+
+		return (results.Success() && results.begin()[0] ? strtoll(results.begin()[0], nullptr, 10) : 0);
+	}
+
+	static int64 Count(Database& db, const std::string &where_filter = "")
+	{
+		auto results = db.QueryDatabase(
+			fmt::format(
+				"SELECT COUNT(*) FROM {} {}",
+				TableName(),
+				(where_filter.empty() ? "" : "WHERE " + where_filter)
+			)
+		);
+
+		return (results.Success() && results.begin()[0] ? strtoll(results.begin()[0], nullptr, 10) : 0);
+	}
+
+};
+
+#endif //EQEMU_BASE_BOT_HEAL_ROTATION_TARGETS_REPOSITORY_H
