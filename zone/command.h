b5035d7e03289d62772f2e254c71bb63f8b4e12b
Kinglykrab <89047260+Kinglykrab@users.noreply.github.com>
2022-11-16 08:15:03 -0500

    [Commands] Remove #profiledump and #profilereset Commands. (#2546)
    - These commands don't seem to be used anymore.

--- zone/command.h
+++ zone/command.h
@@ -197,11 +197,6 @@ void command_petitioninfo(Client *c, const Seperator *sep);
 void command_picklock(Client *c, const Seperator *sep);
 void command_profanity(Client *c, const Seperator *sep);
 
-#ifdef EQPROFILE
-void command_profiledump(Client *c, const Seperator *sep);
-void command_profilereset(Client *c, const Seperator *sep);
-#endif
-
 void command_proximity(Client *c, const Seperator *sep);
 void command_push(Client *c, const Seperator *sep);
 void command_pvp(Client *c, const Seperator *sep);
