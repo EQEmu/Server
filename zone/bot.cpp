cf57bd3e4f16d37e40ef0ef8dd97483b9f2288f9
Aeadoin <109764533+Aeadoin@users.noreply.github.com>
2022-11-26 15:59:25 -0500

    Fix formatting

--- zone/bot.cpp
+++ zone/bot.cpp
@@ -4729,8 +4729,7 @@ void Bot::PerformTradeWithClient(int16 begin_slot_id, int16 end_slot_id, Client*
 			}
 		}
 		
-		bool bots_ignore_race = RuleB(Bot, AllowBotEquipAnyRaceGear);
-		if (!trade_instance->IsClassEquipable(GetClass()) || (GetLevel() < trade_instance->GetItem()->ReqLevel) || (!trade_instance->IsRaceEquipable(GetRace()) && !bots_ignore_race)) { // deity checks will be handled within IsEquipable()
+		if (!trade_instance->IsClassEquipable(GetClass()) || GetLevel() < trade_instance->GetItem()->ReqLevel || (!trade_instance->IsRaceEquipable(GetRace()) && !RuleB(Bot, AllowBotEquipAnyRaceGear))) { // deity checks will be handled within IsEquipable()
 			if (trade_event_exists) {
 				event_trade.push_back(ClientTrade(trade_instance, trade_index));
 				continue;
