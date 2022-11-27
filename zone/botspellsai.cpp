4ea29fa192e857669c7ae72aab3f80710847bd2b
Aeadoin <109764533+Aeadoin@users.noreply.github.com>
2022-11-27 10:41:18 -0500

    [Bot] Update Bot Logic to ignore ST_TargetsTarget when buffing

--- zone/botspellsai.cpp
+++ zone/botspellsai.cpp
@@ -371,12 +371,15 @@ bool Bot::AICastSpell(Mob* tar, uint8 iChance, uint32 iSpellTypes) {
 						continue;
 					}
 					// Validate target
-
-					if (!((spells[selectedBotSpell.SpellId].target_type == ST_Target || spells[selectedBotSpell.SpellId].target_type == ST_Pet || tar == this ||
-						spells[selectedBotSpell.SpellId].target_type == ST_Group || spells[selectedBotSpell.SpellId].target_type == ST_GroupTeleport ||
-						(botClass == BARD && spells[selectedBotSpell.SpellId].target_type == ST_AEBard))
-						&& !tar->IsImmuneToSpell(selectedBotSpell.SpellId, this)
-						&& (tar->CanBuffStack(selectedBotSpell.SpellId, botLevel, true) >= 0))) {
+					// TODO: Add ST_TargetsTarget support for Buffing.
+					if (!((spells[selectedBotSpell.SpellId].target_type == ST_Target || 
+						spells[selectedBotSpell.SpellId].target_type == ST_Pet || 
+						(tar == this && !spells[selectedBotSpell.SpellId].target_type == ST_TargetsTarget) ||
+						spells[selectedBotSpell.SpellId].target_type == ST_Group || 
+						spells[selectedBotSpell.SpellId].target_type == ST_GroupTeleport ||
+						(botClass == BARD && spells[selectedBotSpell.SpellId].target_type == ST_AEBard)) &&
+						!tar->IsImmuneToSpell(selectedBotSpell.SpellId, this) &&
+						(tar->CanBuffStack(selectedBotSpell.SpellId, botLevel, true) >= 0))) {
 							continue;
 					}
 
