38725553322389178080bb1e2467ed15c344ad75
Michael <fryguy503@gmail.com>
2022-12-07 18:50:02 -0500

    [Rules] Rule Gate Pet Zoning (#2625)
    Allows preventing pet zoning from happening for more classic servers.

--- common/ruletypes.h
+++ common/ruletypes.h
@@ -193,6 +193,7 @@ RULE_INT(Character, EnchanterTrackingDistanceMultiplier, 0, "If you want enchant
 RULE_INT(Character, BeastlordTrackingDistanceMultiplier, 0, "If you want beastlords to be able to track, increase this above 0.  0 disables tracking packets.")
 RULE_INT(Character, BerserkerTrackingDistanceMultiplier, 0, "If you want berserkers to be able to track, increase this above 0.  0 disables tracking packets.")
 RULE_BOOL(Character, OnInviteReceiveAlreadyinGroupMessage, true, "If you want clients to receive a message when trying to invite a player into a group that is currently in another group.")
+RULE_BOOL(Character, PetZoneWithOwner, true, "Should Pets Zone with Owner")
 RULE_CATEGORY_END()
 
 RULE_CATEGORY(Mercs)
