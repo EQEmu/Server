06d7cce018cc6b38832c3f32247234afe3b63f7c
Akkadius <akkadius1@gmail.com>
2022-12-14 22:50:52 -0600

    Revert some code changes

--- zone/client_packet.cpp
+++ zone/client_packet.cpp
@@ -12753,7 +12753,6 @@ void Client::Handle_OP_RezzAnswer(const EQApplicationPacket *app)
 		r->action ? "ACCEPT" : "DECLINE"
 	);
 
-
 	OPRezzAnswer(r->action, r->spellid, r->zone_id, r->instance_id, r->x, r->y, r->z);
 
 	if (r->action == ResurrectionActions::Accept) {
