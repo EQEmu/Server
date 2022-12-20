b299aede9f9daf3d91fd7ffb188d3d980de38b03
Akkadius <akkadius1@gmail.com>
2022-12-19 21:28:34 -0600

    Format header

--- common/events/player_event_discord_formatter.h
+++ common/events/player_event_discord_formatter.h
@@ -74,20 +74,20 @@ public:
 	static std::string FormatWithNodata(const PlayerEvent::PlayerEventContainer &c);
 
 	static std::string FormatAAGainedEvent(
-	const PlayerEvent::PlayerEventContainer &c,
-	const PlayerEvent::AAGainedEvent &e
+		const PlayerEvent::PlayerEventContainer &c,
+		const PlayerEvent::AAGainedEvent &e
 	);
 	static std::string FormatAAPurchasedEvent(
-	const PlayerEvent::PlayerEventContainer &c,
-	const PlayerEvent::AAPurchasedEvent &e
+		const PlayerEvent::PlayerEventContainer &c,
+		const PlayerEvent::AAPurchasedEvent &e
 	);
 	static std::string FormatForageSuccessEvent(
-	const PlayerEvent::PlayerEventContainer &c,
-	const PlayerEvent::ForageSuccessEvent &e
+		const PlayerEvent::PlayerEventContainer &c,
+		const PlayerEvent::ForageSuccessEvent &e
 	);
 	static std::string FormatDestroyItemEvent(
-	const PlayerEvent::PlayerEventContainer &c,
-	const PlayerEvent::DestroyItemEvent &e
+		const PlayerEvent::PlayerEventContainer &c,
+		const PlayerEvent::DestroyItemEvent &e
 	);
 	static std::string FormatMerchantPurchaseEvent(
 		const PlayerEvent::PlayerEventContainer &c,
@@ -98,13 +98,9 @@ public:
 		const PlayerEvent::MerchantSellEvent &e
 	);
 	static std::string FormatZoningEvent(
-	const PlayerEvent::PlayerEventContainer &c,
-	const PlayerEvent::ZoningEvent &e
+		const PlayerEvent::PlayerEventContainer &c,
+		const PlayerEvent::ZoningEvent &e
 	);
-
-
-
-
 };
 
 
