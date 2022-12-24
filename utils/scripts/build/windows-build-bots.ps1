cf56f3ff11adc73c1aac7e8fa2e640130a199e20
Akkadius <akkadius1@gmail.com>
2022-12-24 04:45:50 -0600

    Remove 7z from build scripts

--- utils/scripts/build/windows-build-bots.ps1
+++ utils/scripts/build/windows-build-bots.ps1
@@ -12,4 +12,3 @@ Set-Location -Path "$cwd\build_bots_x64"
 cmake -Wno-dev -G "Visual Studio 17 2022" -A x64 -DEQEMU_BUILD_TESTS=ON -DEQEMU_BUILD_LOGIN=ON -DEQEMU_BUILD_ZLIB=ON -DEQEMU_ENABLE_BOTS=ON "$cwd"
 cmake --build . --config RelWithDebInfo --clean-first
 Set-Location -Path "$cwd"
-7z a build_bots_x64.zip $cwd\build_bots_x64\bin\RelWithDebInfo\*.exe $cwd\build_bots_x64\bin\RelWithDebInfo\*.dll $cwd\build_bots_x64\bin\RelWithDebInfo\*.pdb $cwd\build_bots_x64\libs\zlibng\RelWithDebInfo\*.dll $cwd\build_bots_x64\libs\zlibng\RelWithDebInfo\*.pdb
