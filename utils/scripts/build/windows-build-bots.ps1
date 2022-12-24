3408d136e2af56b12783540311c305644b7c9ffe
Akkadius <akkadius1@gmail.com>
2022-12-24 04:34:38 -0600

    Split

--- utils/scripts/build/windows-build-bots.ps1
+++ utils/scripts/build/windows-build-bots.ps1
@@ -2,27 +2,10 @@ $cwd = Get-Location
 
 Set-Location -Path "$cwd"
 
-git submodule init
-git submodule update
-
-Set-Location -Path $cwd
-
-if(![System.IO.Directory]::Exists("$cwd\build_x64")) {
-	Write-Information -MessageData "Creating build x64 folder" -InformationAction Continue
-	New-Item -Path "$cwd\build_x64" -ItemType Directory
-}
-
 if(![System.IO.Directory]::Exists("$cwd\build_bots_x64")) {
 	Write-Information -MessageData "Creating build (bots) x64 folder" -InformationAction Continue
 	New-Item -Path "$cwd\build_bots_x64" -ItemType Directory
 }
-
-Write-Information -MessageData "Creating no bots build x64" -InformationAction Continue
-Set-Location -Path "$cwd\build_x64"
-cmake -Wno-dev -G "Visual Studio 17 2022" -A x64 -DEQEMU_BUILD_TESTS=ON -DEQEMU_BUILD_LOGIN=ON -DEQEMU_BUILD_ZLIB=ON -DEQEMU_ENABLE_BOTS=OFF "$cwd"
-cmake --build . --config RelWithDebInfo --clean-first
-Set-Location -Path "$cwd"
-7z a build_x64.zip $cwd\build_x64\bin\RelWithDebInfo\*.exe $cwd\build_x64\bin\RelWithDebInfo\*.dll $cwd\build_x64\bin\RelWithDebInfo\*.pdb $cwd\build_x64\libs\zlibng\RelWithDebInfo\*.dll $cwd\build_x64\libs\zlibng\RelWithDebInfo\*.pdb
 
 Write-Information -MessageData "Creating bots build x64" -InformationAction Continue
 Set-Location -Path "$cwd\build_bots_x64"
