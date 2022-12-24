337019fb4ddd9bd08ebcb3df76ac0a7246364c96
Akkadius <akkadius1@gmail.com>
2022-12-24 03:48:00 -0600

    Update windows-build.ps1

--- utils/scripts/build/windows-build.ps1
+++ utils/scripts/build/windows-build.ps1
@@ -19,14 +19,14 @@ if(![System.IO.Directory]::Exists("$cwd\build_bots_x64")) {
 
 Write-Information -MessageData "Creating no bots build x64" -InformationAction Continue
 Set-Location -Path "$cwd\build_x64"
-cmake -Wno-dev -G "Visual Studio 17 2022" -A x64 -DEQEMU_BUILD_TESTS=ON -DEQEMU_BUILD_LOGIN=ON -DEQEMU_BUILD_ZLIB=ON -DEQEMU_ENABLE_BOTS=OFF "$cwd\\Server"
+cmake -Wno-dev -G "Visual Studio 17 2022" -A x64 -DEQEMU_BUILD_TESTS=ON -DEQEMU_BUILD_LOGIN=ON -DEQEMU_BUILD_ZLIB=ON -DEQEMU_ENABLE_BOTS=OFF "$cwd"
 cmake --build . --config RelWithDebInfo --clean-first
 Set-Location -Path "$cwd"
 7z a build_x64.zip $cwd\build_x64\bin\RelWithDebInfo\*.exe $cwd\build_x64\bin\RelWithDebInfo\*.dll $cwd\build_x64\bin\RelWithDebInfo\*.pdb $cwd\build_x64\libs\zlibng\RelWithDebInfo\*.dll $cwd\build_x64\libs\zlibng\RelWithDebInfo\*.pdb
 
 Write-Information -MessageData "Creating bots build x64" -InformationAction Continue
 Set-Location -Path "$cwd\build_bots_x64"
-cmake -Wno-dev -G "Visual Studio 17 2022" -A x64 -DEQEMU_BUILD_TESTS=ON -DEQEMU_BUILD_LOGIN=ON -DEQEMU_BUILD_ZLIB=ON -DEQEMU_ENABLE_BOTS=ON "$cwd\\Server"
+cmake -Wno-dev -G "Visual Studio 17 2022" -A x64 -DEQEMU_BUILD_TESTS=ON -DEQEMU_BUILD_LOGIN=ON -DEQEMU_BUILD_ZLIB=ON -DEQEMU_ENABLE_BOTS=ON "$cwd"
 cmake --build . --config RelWithDebInfo --clean-first
 Set-Location -Path "$cwd"
 7z a build_bots_x64.zip $cwd\build_bots_x64\bin\RelWithDebInfo\*.exe $cwd\build_bots_x64\bin\RelWithDebInfo\*.dll $cwd\build_bots_x64\bin\RelWithDebInfo\*.pdb $cwd\build_bots_x64\libs\zlibng\RelWithDebInfo\*.dll $cwd\build_bots_x64\libs\zlibng\RelWithDebInfo\*.pdb
