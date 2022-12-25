c445f7bed648e42ba8302e8c1cd12cc46a649bfb
Akkadius <akkadius1@gmail.com>
2022-12-25 01:17:23 -0600

    Test

--- utils/scripts/build/package-artifacts.ps1
+++ utils/scripts/build/package-artifacts.ps1
@@ -2,6 +2,6 @@ $cwd = Get-Location
 
 Set-Location -Path "$cwd"
 
-7z a build_x64.zip $cwd\build_x64\bin\RelWithDebInfo\*.exe $cwd\build_x64\bin\RelWithDebInfo\*.dll $cwd\build_x64\bin\RelWithDebInfo\*.pdb $cwd\build_x64\libs\zlibng\RelWithDebInfo\*.dll $cwd\build_x64\libs\zlibng\RelWithDebInfo\*.pdb
+7z a win-build-x64.zip $cwd\win-build-x64\bin\RelWithDebInfo\*.exe $cwd\win-build-x64\bin\RelWithDebInfo\*.dll $cwd\win-build-x64\bin\RelWithDebInfo\*.pdb $cwd\win-build-x64\libs\zlibng\RelWithDebInfo\*.dll $cwd\win-build-x64\libs\zlibng\RelWithDebInfo\*.pdb
 
-7z a build_bots_x64.zip $cwd\build_bots_x64\bin\RelWithDebInfo\*.exe $cwd\build_bots_x64\bin\RelWithDebInfo\*.dll $cwd\build_bots_x64\bin\RelWithDebInfo\*.pdb $cwd\build_bots_x64\libs\zlibng\RelWithDebInfo\*.dll $cwd\build_bots_x64\libs\zlibng\RelWithDebInfo\*.pdb
+7z a win-build-bots-x64.zip $cwd\win-build-bots-x64\bin\RelWithDebInfo\*.exe $cwd\win-build-bots-x64\bin\RelWithDebInfo\*.dll $cwd\win-build-bots-x64\bin\RelWithDebInfo\*.pdb $cwd\win-build-bots-x64\libs\zlibng\RelWithDebInfo\*.dll $cwd\win-build-bots-x64\libs\zlibng\RelWithDebInfo\*.pdb
