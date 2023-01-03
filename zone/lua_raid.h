b2f1eed859266c7ed1f3c207f2ed9b1c8e24c4e2
Valorith <rgagnier06@gmail.com>
2023-01-03 18:14:28 -0500

    Add LUA compatibility

--- zone/lua_raid.h
+++ zone/lua_raid.h
@@ -40,6 +40,7 @@ public:
 	void BalanceHP(int penalty, uint32 group_id);
 	bool IsLeader(const char *c);
 	bool IsLeader(Lua_Client c);
+	Lua_Client GetLeader();
 	bool IsGroupLeader(const char *name);
 	bool IsGroupLeader(Lua_Client c);
 	int GetHighestLevel();
