ff47a76aab7b05362f7a461b0b6af63b23287860
Noudess <noudess@gmail.com>
2023-01-05 11:49:29 -0500

    Restore virtual func & fix pets

--- zone/pets.cpp
+++ zone/pets.cpp
@@ -735,14 +735,6 @@ bool ZoneDatabase::GetBasePetItems(int32 equipmentset, uint32 *items) {
 	return true;
 }
 
-bool Pet::CheckSpellLevelRestriction(uint16 spell_id)
-{
-	auto owner = GetOwner();
-	if (owner)
-		return owner->CheckSpellLevelRestriction(spell_id);
-	return true;
-}
-
 BeastlordPetData::PetStruct ZoneDatabase::GetBeastlordPetData(uint16 race_id) {
 	BeastlordPetData::PetStruct beastlord_pet_data;
 	std::string query = fmt::format(
