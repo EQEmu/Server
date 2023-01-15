b0d9fc66e3a7610bde261af6bdf5d26fad35064a
Valorith <rgagnier06@gmail.com>
2023-01-15 13:06:13 -0500

    Update chatchannel.cpp

--- ucs/chatchannel.cpp
+++ ucs/chatchannel.cpp
@@ -16,7 +16,7 @@
 	Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
 
 */
-#pragma once
+
 #include "../common/eqemu_logsys.h"
 #include "../common/strings.h"
 #include "chatchannel.h"
