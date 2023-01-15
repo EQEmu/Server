5f66df16e80440b1f45439afceceb629a41f0e7f
Kinglykrab <kinglykrab@gmail.com>
2023-01-14 19:37:31 -0500

    Update bot.cpp

--- zone/bot.cpp
+++ zone/bot.cpp
@@ -4242,6 +4242,7 @@ void Bot::LoadAndSpawnAllZonedBots(Client* bot_owner) {
 						}
 
 						if (spawned_bots_count >= bot_spawn_limit) {
+							g->UpdatePlayer(bot_owner);
 							database.SetGroupID(b->GetCleanName(), 0, b->GetBotID());
 							continue;
 						}
