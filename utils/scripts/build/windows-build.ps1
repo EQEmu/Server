268e03206ac7d47284c753b04ee1240fd88f0432
Akkadius <akkadius1@gmail.com>
2023-01-18 23:00:46 -0600

    Release without bots

--- utils/scripts/build/windows-build.ps1
+++ utils/scripts/build/windows-build.ps1
@@ -7,8 +7,8 @@ if(![System.IO.Directory]::Exists("$cwd\win-build-x64")) {
 	New-Item -Path "$cwd\win-build-x64" -ItemType Directory
 }
 
-Write-Information -MessageData "Creating no bots build x64" -InformationAction Continue
+Write-Information -MessageData "Creating build x64" -InformationAction Continue
 Set-Location -Path "$cwd\win-build-x64"
-cmake -Wno-dev -G "Visual Studio 17 2022" -A x64 -DEQEMU_BUILD_TESTS=ON -DEQEMU_BUILD_LOGIN=ON -DEQEMU_BUILD_ZLIB=ON -DEQEMU_ENABLE_BOTS=OFF "$cwd"
+cmake -Wno-dev -G "Visual Studio 17 2022" -A x64 -DEQEMU_BUILD_TESTS=ON -DEQEMU_BUILD_LOGIN=ON -DEQEMU_BUILD_ZLIB=ON "$cwd"
 cmake --build . --config RelWithDebInfo --clean-first
 Set-Location -Path "$cwd"
