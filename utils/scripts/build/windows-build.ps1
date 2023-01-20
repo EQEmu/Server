f5de63b1d793a3feaf86d473de660570cb0a734c
Akkadius <akkadius1@gmail.com>
2023-01-20 16:22:41 -0600

    Shuffle

--- utils/scripts/build/windows-build.ps1
+++ utils/scripts/build/windows-build.ps1
@@ -1,14 +1,34 @@
-$cwd = Get-Location
+Try
+{
+    $cwd = Get-Location
 
-Set-Location -Path "$cwd"
+    Set-Location -Path "$cwd"
 
-if(![System.IO.Directory]::Exists("$cwd\win-build-x64")) {
-	Write-Information -MessageData "Creating build x64 folder" -InformationAction Continue
-	New-Item -Path "$cwd\win-build-x64" -ItemType Directory
+    git submodule init
+    git submodule update
+
+    if (![System.IO.Directory]::Exists("$cwd\win-build-x64"))
+    {
+        Write-Information -MessageData "Creating build x64 folder" -InformationAction Continue
+        New-Item -Path "$cwd\win-build-x64" -ItemType Directory
+    }
+
+    Write-Information -MessageData "Creating build x64" -InformationAction Continue
+    Set-Location -Path "$cwd\win-build-x64"
+    cmake -Wno-dev -G "Visual Studio 17 2022" -A x64 -DEQEMU_BUILD_TESTS=ON -DEQEMU_BUILD_LOGIN=ON -DEQEMU_BUILD_ZLIB=ON "$cwd"
+    cmake --build . --config RelWithDebInfo --clean-first
+    Set-Location -Path "$cwd"
+
+    cmd.exe .\utils\scripts\build\should-release\should-release.exe
+
+    dir *.zip
+    rclone config create remote ftp env_auth true
+    rclone copy eqemu-server-windows-x64.zip remote:
+    rclone ls remote:
+}
+Catch
+{
+    Write-Host ("Caught signal to end")
 }
 
-Write-Information -MessageData "Creating build x64" -InformationAction Continue
-Set-Location -Path "$cwd\win-build-x64"
-cmake -Wno-dev -G "Visual Studio 17 2022" -A x64 -DEQEMU_BUILD_TESTS=ON -DEQEMU_BUILD_LOGIN=ON -DEQEMU_BUILD_ZLIB=ON "$cwd"
-cmake --build . --config RelWithDebInfo --clean-first
-Set-Location -Path "$cwd"
+
