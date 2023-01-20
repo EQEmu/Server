4b20db6ab70cf067b1baf75f6ea02776bcce4fd8
Akkadius <akkadius1@gmail.com>
2023-01-20 16:11:14 -0600

    Update build-release.bat

--- utils/scripts/build/build-release.bat
+++ utils/scripts/build/build-release.bat
@@ -1,9 +1,11 @@
-.\utils\scripts\build\should-release\should-release.exe || echo Not releasing, exit gracefully && exit /b 0
-
 git submodule init
 git submodule update
-utils/scripts/build/windows-build.ps1
-utils/scripts/build/package-artifacts.ps1
+.\utils\scripts\build\windows-build.ps1
+
+.\utils\scripts\build\should-release\should-release.exe || echo Not releasing, exit gracefully && exit /b 0
+
+.\utils\scripts\build\package-artifacts.ps1
+
 dir *.zip
 rclone config create remote ftp env_auth true
 rclone copy eqemu-server-windows-x64.zip remote:
