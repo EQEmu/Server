c7dcf52b82469d824142b02b536ea66f0bda89e9
Akkadius <akkadius1@gmail.com>
2023-01-20 17:06:20 -0600

    Consolidate

---
+++ utils/scripts/build/linux-build.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+set -x
+
+sudo chown eqemu:eqemu /drone/src/ * -R
+sudo chown eqemu:eqemu /home/eqemu/.ccache/ * -R
+
+git submodule init && git submodule update
+
+mkdir -p build && cd build && cmake -DEQEMU_BUILD_TESTS=ON -DEQEMU_BUILD_LOGIN=ON -DEQEMU_BUILD_LUA=ON CMAKE_CXX_FLAGS_RELWITHDEBINFO:STRING="-Os" -DCMAKE_CXX_COMPILER_LAUNCHER=ccache -G 'Unix Makefiles' .. && make -j$((`nproc`-4))
+
+curl https://raw.githubusercontent.com/Akkadius/eqemu-install-v2/master/eqemu_config.json --output eqemu_config.json
+./bin/tests
+
+./utils/scripts/build/should-release/should-release || exit
+
+rm ./build/bin/*.a
+zip -j eqemu-server-linux-x64.zip ./build/bin/*
+
+# shellcheck disable=SC2010
+ls -lsh | grep zip
+rclone config create remote ftp env_auth true > /dev/null
+rclone copy eqemu-server-linux-x64.zip remote:
+rclone ls remote:
