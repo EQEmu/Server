c0ff63e2d85cc2324feb8030b4c9139da7f6b8a7
Akkadius <akkadius1@gmail.com>
2023-01-20 16:32:11 -0600

    Take #45354

--- utils/scripts/build/package-artifacts.ps1
+++
@@ -1,5 +0,0 @@
-$cwd = Get-Location
-
-Set-Location -Path "$cwd"
-
-7z a eqemu-server-windows-x64.zip $cwd\win-build-x64\bin\RelWithDebInfo\*.exe $cwd\win-build-x64\bin\RelWithDebInfo\*.dll $cwd\win-build-x64\bin\RelWithDebInfo\*.pdb $cwd\win-build-x64\libs\zlibng\RelWithDebInfo\*.dll $cwd\win-build-x64\libs\zlibng\RelWithDebInfo\*.pdb
