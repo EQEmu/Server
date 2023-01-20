158dd3ac9789af1ab641dfc3244780b604372f4d
Akkadius <akkadius1@gmail.com>
2023-01-20 16:32:11 -0600

    Take #45354

--- utils/scripts/build/windows-build.ps1
+++ utils/scripts/build/windows-build.ps1
@@ -19,6 +19,8 @@ Try
     cmake --build . --config RelWithDebInfo --clean-first
     Set-Location -Path "$cwd"
 
+    7z a eqemu-server-windows-x64.zip $cwd\win-build-x64\bin\RelWithDebInfo\*.exe $cwd\win-build-x64\bin\RelWithDebInfo\*.dll $cwd\win-build-x64\bin\RelWithDebInfo\*.pdb $cwd\win-build-x64\libs\zlibng\RelWithDebInfo\*.dll $cwd\win-build-x64\libs\zlibng\RelWithDebInfo\*.pdb
+
     cmd.exe .\utils\scripts\build\should-release\should-release.exe
 
     dir *.zip
