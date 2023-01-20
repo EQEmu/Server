f29421a7d6c4b70e28b6a7ae712f110bff365129
Akkadius <akkadius1@gmail.com>
2023-01-20 16:04:08 -0600

    Test pipeline

--- utils/scripts/build/build-release.bat
+++ utils/scripts/build/build-release.bat
@@ -1,1 +1,10 @@
 .\utils\scripts\build\should-release\should-release.exe || echo Not releasing, exit gracefully && exit /b 0
+
+git submodule init
+git submodule update
+utils/scripts/build/windows-build.ps1
+utils/scripts/build/package-artifacts.ps1
+dir *.zip
+rclone config create remote ftp env_auth true
+rclone copy eqemu-server-windows-x64.zip remote:
+rclone ls remote:
