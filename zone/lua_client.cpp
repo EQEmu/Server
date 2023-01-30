7c8328430d9bbf9eac0edcc15b28623fb7216646
Natedog2012 <jwalters_06@yahoo.com>
2023-01-29 21:46:28 -0600

    [Quest API] Add GetItemCooldown to return the time remaining on items in seconds

--- zone/lua_client.cpp
+++ zone/lua_client.cpp
@@ -3026,6 +3026,12 @@ void Lua_Client::SetItemCooldown(uint32 item_id, uint32 in_time)
 	self->SetItemCooldown(item_id, false, in_time);
 }
 
+int Lua_Client::GetItemCooldown(uint32 item_id)
+{
+	Lua_Safe_Call_Int();
+	return self->GetItemCooldown(item_id);
+}
+
 luabind::scope lua_register_client() {
 	return luabind::class_<Lua_Client, Lua_Mob>("Client")
 	.def(luabind::constructor<>())
@@ -3216,6 +3222,7 @@ luabind::scope lua_register_client() {
 	.def("GetInventory", (Lua_Inventory(Lua_Client::*)(void))&Lua_Client::GetInventory)
 	.def("GetInvulnerableEnvironmentDamage", (bool(Lua_Client::*)(void))&Lua_Client::GetInvulnerableEnvironmentDamage)
 	.def("GetItemIDAt", (int(Lua_Client::*)(int))&Lua_Client::GetItemIDAt)
+	.def("GetItemCooldown", (int(Lua_Client::*)(uint32))&Lua_Client::GetItemCooldown)
 	.def("GetLDoNLosses", (int(Lua_Client::*)(void))&Lua_Client::GetLDoNLosses)
 	.def("GetLDoNLossesTheme", (int(Lua_Client::*)(int))&Lua_Client::GetLDoNLossesTheme)
 	.def("GetLDoNPointsTheme", (int(Lua_Client::*)(int))&Lua_Client::GetLDoNPointsTheme)
