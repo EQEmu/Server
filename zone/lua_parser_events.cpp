d6ed45da58777918348786a1b7006f512e3aae60
Valorith <rgagnier06@gmail.com>
2023-02-10 19:54:29 -0500

    Add XP Events

--- zone/lua_parser_events.cpp
+++ zone/lua_parser_events.cpp
@@ -1163,6 +1163,30 @@ void handle_player_aa_gain(
 	lua_setfield(L, -2, "aa_gained");
 }
 
+void handle_player_aaxp_gain(
+        QuestInterface *parse,
+        lua_State* L,
+        Client* client,
+        std::string data,
+        uint32 extra_data,
+        std::vector<std::any> *extra_pointers
+) {
+    lua_pushinteger(L, Strings::ToInt(data));
+    lua_setfield(L, -2, "aaxp_value");
+}
+
+void handle_player_xp_gain(
+        QuestInterface *parse,
+        lua_State* L,
+        Client* client,
+        std::string data,
+        uint32 extra_data,
+        std::vector<std::any> *extra_pointers
+) {
+    lua_pushinteger(L, Strings::ToInt(data));
+    lua_setfield(L, -2, "xp_value");
+}
+
 void handle_player_level_up(
 	QuestInterface *parse,
 	lua_State* L,
