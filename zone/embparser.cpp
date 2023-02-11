6371cda01e624b711f8084d4beb7941e40a8c057
Valorith <rgagnier06@gmail.com>
2023-02-10 20:46:10 -0500

    Tweak

--- zone/embparser.cpp
+++ zone/embparser.cpp
@@ -1994,7 +1994,7 @@ void PerlembParser::ExportEventVariables(
 		}
 
 		case EVENT_AA_GAIN: {
-			ExportVar(package_name.c_str(), "aa_gained", extradata);
+			ExportVar(package_name.c_str(), "aa_gained", data);
 			break;
 		}
 
