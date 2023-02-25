a27b6164346f1336f0bf5318e0c1a2f59b90d3d0
Kinglykrab <kinglykrab@gmail.com>
2023-02-25 17:41:54 -0500

    [Quest API] Add GetDefaultRaceSize() to Perl/Lua
    # Perl
    - Add `$mob->GetDefaultRaceSize()`.
    
    # Lua
    - Add `mob:GetDefaultRaceSize()`.
    
    # Notes
    - Allows operators to get the default race size of a race if they want to use it in a script.

--- zone/lua_mob.h
+++ zone/lua_mob.h
@@ -513,6 +513,7 @@ public:
 	void CopyHateList(Lua_Mob to);
 	bool IsAttackAllowed(Lua_Mob target);
 	bool IsAttackAllowed(Lua_Mob target, bool is_spell_attack);
+	float GetDefaultRaceSize();
 };
 
 #endif
