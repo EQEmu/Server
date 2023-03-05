c8c016a62ca76d042727fee6eda284efa009c472
Kinglykrab <kinglykrab@gmail.com>
2023-03-05 18:41:53 -0500

    Update picklock.cpp

--- zone/gm_commands/picklock.cpp
+++ zone/gm_commands/picklock.cpp
@@ -2,29 +2,28 @@
 
 void command_picklock(Client *c, const Seperator *sep)
 {
-	auto t = c->GetTarget();
-	if (!t || !t->IsNPC()) {
+	if (!c->GetTarget() || !c->GetTarget()->IsNPC()) {
 		c->Message(Chat::White, "You must target an NPC to use this command.");
 		return;
 	}
 
-	if (t->IsNPC()) {
-		if (c->HasSkill(EQ::skills::SkillPickLock)) {
-			if (DistanceSquaredNoZ(c->GetPosition(), t->GetPosition()) > RuleI(Adventure, LDoNTrapDistanceUse)) {
-				c->Message(
-					Chat::White,
-					fmt::format(
-						"{} is too far away.",
-						c->GetTargetDescription(t)
-					).c_str()
-				);
-				return;
-			}
+	auto t = c->GetTarget()->CastToNPC();
 
-			c->HandleLDoNPickLock(t->CastToNPC(), c->GetSkill(EQ::skills::SkillPickLock), LDoNTypeMechanical);
-		} else {
-			c->Message(Chat::White, "You do not have the Pick Lock skill.");
+	if (c->HasSkill(EQ::skills::SkillPickLock)) {
+		if (DistanceSquaredNoZ(c->GetPosition(), t->GetPosition()) > RuleI(Adventure, LDoNTrapDistanceUse)) {
+			c->Message(
+				Chat::White,
+				fmt::format(
+					"{} is too far away.",
+					c->GetTargetDescription(t)
+				).c_str()
+			);
+			return;
 		}
+
+		c->HandleLDoNPickLock(t, c->GetSkill(EQ::skills::SkillPickLock), LDoNTypeMechanical);
+	} else {
+		c->Message(Chat::White, "You do not have the Pick Lock skill.");
 	}
 }
 
