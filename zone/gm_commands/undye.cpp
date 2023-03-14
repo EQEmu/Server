c55570750ca56834e35a62634af4b3b326a40f73
Noudess <noudess@gmail.com>
2023-03-14 10:05:22 -0400

    [Commands] Allow clients with access level to #undye to undye themselves

--- zone/gm_commands/undye.cpp
+++ zone/gm_commands/undye.cpp
@@ -3,16 +3,22 @@
 void command_undye(Client *c, const Seperator *sep)
 {
 	auto target = c;
-	if (c->GetTarget() && c->GetTarget()->IsClient() && c->GetGM()) {
-		target = c->GetTarget()->CastToClient();
-	}
 
-	target->Undye();
-	c->Message(
-		Chat::White,
-		fmt::format(
-			"Undyed armor for {}.",
-			c->GetTargetDescription(target)
-		).c_str()
-	);
+	bool allowed=(c->GetGM() || c->GetTarget() == c);
+
+	if (allowed) {
+		target = c->GetTarget()->CastToClient();
+
+		target->Undye();
+		c->Message(
+			Chat::White,
+			fmt::format(
+				"Undyed armor for {}.",
+				c->GetTargetDescription(target)
+			).c_str()
+		);
+	}
+	else {
+		c->Message(Chat::Red, "Only clients tagged as GM allowed to undye others");
+	}
 }
