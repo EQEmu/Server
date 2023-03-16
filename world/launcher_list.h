63c0b46c2ea5a5f9b196ea45b1048a7ec998ec09
Kinglykrab <kinglykrab@gmail.com>
2023-03-16 18:20:40 -0400

    [Cleanup] Remove CreateLauncher(), FindByZone(), and GetLauncherNameList() from launcher_list.cpp and launcher_list.cpp
    # Notes
    - These are unused.

--- world/launcher_list.h
+++ world/launcher_list.h
@@ -37,17 +37,13 @@ public:
 
 	void LoadList();
 	EQLConfig *GetConfig(const char *name);
-	void CreateLauncher(const char *name, uint8 dynamic_count);
 	void Remove(const char *name);
 
 	void Add(std::shared_ptr<EQ::Net::ServertalkServerConnection> conn);
 	void Remove(std::shared_ptr<EQ::Net::ServertalkServerConnection> conn);
 	LauncherLink *Get(const char *name);
-	LauncherLink *FindByZone(const char *short_name);
 
 	int GetLauncherCount();
-	void GetLauncherNameList(std::vector<std::string> &list);
-
 protected:
 	std::map<std::string, EQLConfig *> m_configs;	//we own these objects
 	std::map<std::string, LauncherLink *> m_launchers;	//we own these objects
