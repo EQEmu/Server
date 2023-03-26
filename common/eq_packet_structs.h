1394b6a4d2f0eb29ab9043b34fd50caa3c2da824
Aeadoin <109764533+Aeadoin@users.noreply.github.com>
2023-03-25 20:00:31 -0400

    [Hotfix] Fix for Items looted from corpses. (#3147)

--- common/eq_packet_structs.h
+++ common/eq_packet_structs.h
@@ -5524,9 +5524,9 @@ struct ServerLootItem_Struct {
 	uint32 aug_6;		  // uint32	aug_5;
 	bool attuned;
 	std::string custom_data;
-	uint32 ornamenticon;
-	uint32 ornamentidfile;
-	uint32 ornament_hero_model;
+	uint32 ornamenticon {};
+	uint32 ornamentidfile {};
+	uint32 ornament_hero_model {};
 	uint16 trivial_min_level;
 	uint16 trivial_max_level;
 	uint16 npc_min_level;
