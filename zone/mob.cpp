118b8d3ced872af1f10c9482eab3dc7158a4cb29
Trent Miller <trentdm@gmail.com>
2023-04-02 15:57:38 -0600

    Remove redundant range check for HateSummon

--- zone/mob.cpp
+++ zone/mob.cpp
@@ -3708,10 +3708,6 @@ bool Mob::HateSummon() {
 	if(target)
 	{
 		if(summon_level == 1) {
-			if (!RuleB(Combat, SummonMeleeRange) && CombatRange(target)) {
-				return false;
-			}
-
 			entity_list.MessageClose(this, true, 500, Chat::Say, "%s says 'You will not evade me, %s!' ", GetCleanName(), target->GetCleanName() );
 
 			float summoner_zoff = GetZOffset();
