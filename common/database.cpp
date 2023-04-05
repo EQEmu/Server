ea9b09cf1fed22b09d1adaa24d85a4980ac669fb
Alex King <89047260+Kinglykrab@users.noreply.github.com>
2023-04-05 11:24:27 -0400

    [Cleanup] Remove unused variable in Database::CopyCharacter() (#3197)
    # Notes
    - This variable was created but never used.
    - https://pvs-studio.com/en/docs/warnings/v808/

--- common/database.cpp
+++ common/database.cpp
@@ -2289,7 +2289,6 @@ bool Database::CopyCharacter(
 			new_rows.emplace_back(new_values);
 		}
 
-		std::string              insert_values;
 		std::vector<std::string> insert_rows;
 
 		for (auto &r: new_rows) {
