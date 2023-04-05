c81ab007648604ae102ef42fef09074c196d835b
Alex King <89047260+Kinglykrab@users.noreply.github.com>
2023-04-05 11:14:55 -0400

    [Cleanup] Set bonuses to use spell ID instead of boolean (#3230)
    # Notes
    - Spell bonuses `Illusion` is the spell ID, not a boolean.

--- zone/bonuses.cpp
+++ zone/bonuses.cpp
@@ -1651,7 +1651,7 @@ void Mob::ApplyAABonuses(const AA::Rank &rank, StatBonuses *newbon)
 		}
 
 		case SE_Illusion:
-			newbon->Illusion = true;
+			newbon->Illusion = rank.spell;
 			break;
 
 		case SE_IllusionPersistence:
