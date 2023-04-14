285cc3af299fcda804a30dc23b1fb78743367236
Alex King <89047260+Kinglykrab@users.noreply.github.com>
2023-04-14 19:40:21 -0400

    [Cleanup] Remove item_timers from questmgr.cpp/questmgr.h (#3286)
    * [Cleanup] Remove item_timers from questmgr.cpp/questmgr.h
    
    # Notes
    - This is unused.
    
    * Update questmgr.h

--- zone/questmgr.cpp
+++ zone/questmgr.cpp
@@ -74,7 +74,6 @@ QuestManager quest_manager;
 
 QuestManager::QuestManager() {
 	HaveProximitySays = false;
-	item_timers = 0;
 }
 
 QuestManager::~QuestManager() {
--- zone/questmgr.h
+++ zone/questmgr.h
@@ -391,8 +391,6 @@ private:
 	std::list<QuestTimer>	QTimerList;
 	std::list<SignalTimer>	STimerList;
 	std::list<PausedTimer>	PTimerList;
-	size_t item_timers;
-
 };
 
 extern QuestManager quest_manager;
