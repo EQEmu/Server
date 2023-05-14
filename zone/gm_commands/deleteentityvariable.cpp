1297a3e845ff92b7fb5eec2d94a12ed5f6731972
Kinglykrab <kinglykrab@gmail.com>
2023-05-14 10:17:32 -0400

    [Commands] Add entity variable commands
    # Commands
    - Add `#clearentityvariables` to clear all entity variables from yourself or your target.
    - Add `#deleteentityvariable [Variable Name]` to delete an entity variable from yourself or your target.
    - Add `#setentityvariable [Variable Name] [Variable Value]` to set an entity variable for yourself or your target.
    - Add `#viewentityvariables [Search Criteria]` to view your or your target's entity variables.
    
    # Notes
    - `#setentityvariable` can use multi-word names/values by using double quotes like `#setentityvariable "Test Variable" "Test Value"`.
    - `#viewentityvariable` does not require a search criteria, not using one shows all entity variables on yourself or your target.

---
+++ zone/gm_commands/deleteentityvariable.cpp
@@ -0,0 +1,44 @@
+#include "../client.h"
+
+void command_deleteentityvariable(Client *c, const Seperator *sep)
+{
+	const auto arguments = sep->argnum;
+
+	if (!arguments) {
+		c->Message(Chat::White, "Usage: #deleteentityvariable [Variable Name]");
+		c->Message(Chat::White, "Example: #deleteentityvariable Test Variable");
+		return;
+	}
+
+	Mob *t = c;
+	if (c->GetTarget()) {
+		t = c->GetTarget();
+	}
+
+	const std::string variable_name = sep->argplus[1];
+
+	if (!t->EntityVariableExists(variable_name)) {
+		c->Message(
+			Chat::White,
+			fmt::format(
+				"{} {} not have an entity variable named '{}'.",
+				c->GetTargetDescription(t, TargetDescriptionType::UCYou),
+				c == t ? "do" : "does",
+				variable_name
+			).c_str()
+		);
+
+		return;
+	}
+
+	t->DeleteEntityVariable(variable_name);
+
+	c->Message(
+		Chat::White,
+		fmt::format(
+			"Deleted an entity variable named '{}' from {}.",
+			variable_name,
+			c->GetTargetDescription(t, TargetDescriptionType::LCSelf)
+		).c_str()
+	);
+}
