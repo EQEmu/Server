f795f7aa0de3b31a68bab5a410f1192e5beaede5
Adam Liverman <livermana@gmail.com>
2023-05-14 18:32:11 -0400

    fix for mob scaling issue with min dmg set to zero while max dmg is not.

--- zone/npc.cpp
+++ zone/npc.cpp
@@ -2613,6 +2613,11 @@ void NPC::ModifyNPCStat(const std::string& stat, const std::string& value)
 	else if (stat_lower == "min_hit") {
 		min_dmg     = Strings::ToInt(value);
 		// TODO: fix DB
+		if (min_dmg > max_dmg) {
+			const auto temporary_damage = max_dmg;
+			max_dmg = min_dmg;
+			min_dmg = temporary_damage;
+		}
 		base_damage = round((max_dmg - min_dmg) / 1.9);
 		min_damage  = min_dmg - round(base_damage / 10.0);
 		return;
@@ -2620,6 +2625,11 @@ void NPC::ModifyNPCStat(const std::string& stat, const std::string& value)
 	else if (stat_lower == "max_hit") {
 		max_dmg     = Strings::ToInt(value);
 		// TODO: fix DB
+		if (max_dmg < min_dmg) {
+			const auto temporary_damage = min_dmg;
+			min_dmg = max_dmg;
+			max_dmg = temporary_damage;
+		}
 		base_damage = round((max_dmg - min_dmg) / 1.9);
 		min_damage  = min_dmg - round(base_damage / 10.0);
 		return;
