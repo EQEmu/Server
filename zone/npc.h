02d573cac2ceed7db3473016ce87136aa3b093ec
Kinglykrab <kinglykrab@gmail.com>
2023-06-24 10:05:04 -0400

    Update npc.h

--- zone/npc.h
+++ zone/npc.h
@@ -650,7 +650,7 @@ protected:
 
 	uint16	skills[EQ::skills::HIGHEST_SKILL + 1];
 
-	uint32 equipment[EQ::invslot::EQUIPMENT_COUNT];	//this is an array of item IDs
+	uint32	equipment[EQ::invslot::EQUIPMENT_COUNT];	//this is an array of item IDs
 
 	uint32	herosforgemodel;			//this is the Hero Forge Armor Model (i.e 63 or 84 or 203)
 	uint16	d_melee_texture1;
