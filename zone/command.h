2a679f10024b4fbbb9d754a01ea01357b2f203e6
Alex King <89047260+Kinglykrab@users.noreply.github.com>
2023-06-24 14:09:51 -0400

    [Cleanup] Remove command_showpetspell in zone/command.h (#3430)
    # Notes
    - This is unused.

--- zone/command.h
+++ zone/command.h
@@ -249,7 +249,6 @@ void command_setxp(Client *c, const Seperator *sep);
 void command_showbuffs(Client *c, const Seperator *sep);
 void command_shownumhits(Client *c, const Seperator *sep);
 void command_shownpcgloballoot(Client *c, const Seperator *sep);
-void command_showpetspell(Client *c, const Seperator *sep);
 void command_showskills(Client *c, const Seperator *sep);
 void command_showspellslist(Client *c, const Seperator *sep);
 void command_showstats(Client *c, const Seperator *sep);
