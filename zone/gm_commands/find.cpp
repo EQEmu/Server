f092f2aeb93852029c21a5eae25a6d30f1f24378
Akkadius <akkadius1@gmail.com>
2023-06-26 23:36:02 -0500

    Update find.cpp

--- zone/gm_commands/find.cpp
+++ zone/gm_commands/find.cpp
@@ -17,36 +17,45 @@
 
 void command_find(Client *c, const Seperator *sep)
 {
-	struct FindCommand {
-		std::string command{};
-		std::string usage{};
-		void (*function)(Client *c, const Seperator *sep) = nullptr;
-		std::vector<std::string> aliases{};
+	// Cmd represents a command
+	// variables are short to save space horizontally
+	// when adding a new sub-command, add it to the vector below
+	struct Cmd {
+		std::string cmd{}; // command
+		std::string u{}; // usage
+		void (*fn)(Client *c, const Seperator *sep) = nullptr; // function
+		std::vector<std::string> a{}; // aliases
 	};
 
-	std::vector<FindCommand> commands = {
-		FindCommand{.command = "aa", .usage = "aa [Search Criteria]", .function = FindAA, .aliases = {"#findaa"}},
-		FindCommand{.command = "character", .usage = "character [Search Criteria]", .function = FindCharacter, .aliases = {"#findcharacter"}},
-		FindCommand{.command = "class", .usage = "class [Search Criteria]", .function = FindClass, .aliases = {"#findclass"}},
-		FindCommand{.command = "currency", .usage = "currency [Search Criteria]", .function = FindCurrency, .aliases = {"#findcurrency"}},
-		FindCommand{.command = "deity", .usage = "deity [Search Criteria]", .function = FindDeity, .aliases = {"#finddeity"}},
-		FindCommand{.command = "faction", .usage = "faction [Search Criteria]", .function = FindFaction, .aliases = {"#findfaction"}},
-		FindCommand{.command = "item", .usage = "item [Search Criteria]", .function = FindItem, .aliases = {"#fi", "#finditem"}},
-		FindCommand{.command = "language", .usage = "language [Search Criteria]", .function = FindLanguage, .aliases = {"#findlanguage"}},
-		FindCommand{.command = "npctype", .usage = "npctype [Search Criteria]", .function = FindNPCType, .aliases = {"#fn", "#findnpc", "#findnpctype"}},
-		FindCommand{.command = "race", .usage = "race [Search Criteria]", .function = FindRace, .aliases = {"#findrace"}},
-		FindCommand{.command = "recipe", .usage = "recipe [Search Criteria]", .function = FindRecipe, .aliases = {"#findrecipe"}},
-		FindCommand{.command = "skill", .usage = "skill [Search Criteria]", .function = FindSkill, .aliases = {"#findskill"}},
-		FindCommand{.command = "spell", .usage = "spell [Search Criteria]", .function = FindSpell, .aliases = {"#fs", "#findspell"}},
-		FindCommand{.command = "task", .usage = "task [Search Criteria]", .function = FindTask, .aliases = {"#findtask"}},
-		FindCommand{.command = "zone", .usage = "zone [Search Criteria]", .function = FindZone, .aliases = {"#fz", "#findzone"}},
+	std::vector<Cmd> commands = {
+		Cmd{.cmd = "aa", .u = "aa [Search Criteria]", .fn = FindAA, .a = {"#findaa"}},
+		Cmd{.cmd = "character", .u = "character [Search Criteria]", .fn = FindCharacter, .a = {"#findcharacter"}},
+		Cmd{.cmd = "class", .u = "class [Search Criteria]", .fn = FindClass, .a = {"#findclass"}},
+		Cmd{.cmd = "currency", .u = "currency [Search Criteria]", .fn = FindCurrency, .a = {"#findcurrency"}},
+		Cmd{.cmd = "deity", .u = "deity [Search Criteria]", .fn = FindDeity, .a = {"#finddeity"}},
+		Cmd{.cmd = "faction", .u = "faction [Search Criteria]", .fn = FindFaction, .a = {"#findfaction"}},
+		Cmd{.cmd = "item", .u = "item [Search Criteria]", .fn = FindItem, .a = {"#fi", "#finditem"}},
+		Cmd{.cmd = "language", .u = "language [Search Criteria]", .fn = FindLanguage, .a = {"#findlanguage"}},
+		Cmd{
+			.cmd = "npctype", .u = "npctype [Search Criteria]", .fn = FindNPCType, .a = {
+				"#fn",
+				"#findnpc",
+				"#findnpctype"
+			}
+		},
+		Cmd{.cmd = "race", .u = "race [Search Criteria]", .fn = FindRace, .a = {"#findrace"}},
+		Cmd{.cmd = "recipe", .u = "recipe [Search Criteria]", .fn = FindRecipe, .a = {"#findrecipe"}},
+		Cmd{.cmd = "skill", .u = "skill [Search Criteria]", .fn = FindSkill, .a = {"#findskill"}},
+		Cmd{.cmd = "spell", .u = "spell [Search Criteria]", .fn = FindSpell, .a = {"#fs", "#findspell"}},
+		Cmd{.cmd = "task", .u = "task [Search Criteria]", .fn = FindTask, .a = {"#findtask"}},
+		Cmd{.cmd = "zone", .u = "zone [Search Criteria]", .fn = FindZone, .a = {"#fz", "#findzone"}},
 	};
 
 	// Check for arguments
 	const auto arguments = sep->argnum;
 	if (!arguments) {
 		for (const auto &cmd: commands) {
-			c->Message(Chat::White, fmt::format("Usage: #find {}", cmd.usage).c_str());
+			c->Message(Chat::White, fmt::format("Usage: #find {}", cmd.u).c_str());
 		}
 		return;
 	}
@@ -54,7 +63,7 @@ void command_find(Client *c, const Seperator *sep)
 	// look for alias or command
 	for (const auto &cmd: commands) {
 		// Check for alias first
-		for (const auto &alias: cmd.aliases) {
+		for (const auto &alias: cmd.a) {
 			if (!alias.empty() && alias == Strings::ToLower(sep->arg[0])) {
 				// build string from sep args
 				std::vector<std::string> args = {};
@@ -65,7 +74,7 @@ void command_find(Client *c, const Seperator *sep)
 				}
 
 				// build the rewrite string
-				std::string rewrite = fmt::format("#find {} {}", cmd.command, Strings::Join(args, " "));
+				std::string rewrite = fmt::format("#find {} {}", cmd.cmd, Strings::Join(args, " "));
 
 				// rewrite to #find <sub-command <args>
 				c->SendGMCommand(rewrite);
@@ -74,8 +83,8 @@ void command_find(Client *c, const Seperator *sep)
 		}
 
 		// Check for command
-		if (cmd.command == Strings::ToLower(sep->arg[1])) {
-			cmd.function(c, sep);
+		if (cmd.cmd == Strings::ToLower(sep->arg[1])) {
+			cmd.fn(c, sep);
 			return;
 		}
 	}
@@ -83,6 +92,6 @@ void command_find(Client *c, const Seperator *sep)
 	// Command not found
 	c->Message(Chat::White, "Command not found. Usage: #find [command]");
 	for (const auto &cmd: commands) {
-		c->Message(Chat::White, fmt::format("Usage: #find {}", cmd.usage).c_str());
+		c->Message(Chat::White, fmt::format("Usage: #find {}", cmd.u).c_str());
 	}
 }
