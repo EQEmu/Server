e12368f002435bd717818d16e535225b929ce6a9
Alex King <89047260+Kinglykrab@users.noreply.github.com>
2023-07-02 10:26:37 -0400

    [Quest API] Add Hate Entry Methods to Perl (#3459)
    # Perl
    - Add `$hate_entry->GetFrenzy()`.
    - Add `$hate_entry->SetDamage(value)`.
    - Add `$hate_entry->SetEnt(mob)`.
    - Add `$hate_entry->SetFrenzy(is_frenzy)`.
    - Add `$hate_entry->SetHate(value)`.
    
    # Lua
    - Convert `hate_entry:GetFrenzy()` to `bool` instead of `int`, as `is_entity_frenzy` is a `bool`.

--- zone/perl_hateentry.cpp
+++ zone/perl_hateentry.cpp
@@ -7,9 +7,19 @@
 #include "embperl.h"
 #include "hate_list.h"
 
+int64_t Perl_HateEntry_GetDamage(struct_HateList* self) // @categories Script Utility, Hate and Aggro
+{
+	return self->hatelist_damage;
+}
+
 Mob* Perl_HateEntry_GetEnt(struct_HateList* self) // @categories Script Utility, Hate and Aggro
 {
 	return self->entity_on_hatelist;
+}
+
+bool Perl_HateEntry_GetFrenzy(struct_HateList* self) // @categories Script Utility, Hate and Aggro
+{
+	return self->is_entity_frenzy;
 }
 
 int64_t Perl_HateEntry_GetHate(struct_HateList* self) // @categories Script Utility, Hate and Aggro
@@ -17,9 +27,24 @@ int64_t Perl_HateEntry_GetHate(struct_HateList* self) // @categories Script Util
 	return self->stored_hate_amount;
 }
 
-int64_t Perl_HateEntry_GetDamage(struct_HateList* self) // @categories Script Utility, Hate and Aggro
+void Perl_HateEntry_SetDamage(struct_HateList* self, int64 value) // @categories Script Utility, Hate and Aggro
 {
-	return self->hatelist_damage;
+	self->hatelist_damage = value;
+}
+
+void Perl_HateEntry_SetEnt(struct_HateList* self, Mob* mob) // @categories Script Utility, Hate and Aggro
+{
+	self->entity_on_hatelist = mob;
+}
+
+void Perl_HateEntry_SetFrenzy(struct_HateList* self, bool is_frenzy) // @categories Script Utility, Hate and Aggro
+{
+	self->is_entity_frenzy = is_frenzy;
+}
+
+void Perl_HateEntry_SetHate(struct_HateList* self, int64 value) // @categories Script Utility, Hate and Aggro
+{
+	self->stored_hate_amount = value;
 }
 
 void perl_register_hateentry()
@@ -29,7 +54,12 @@ void perl_register_hateentry()
 	auto package = perl.new_class<struct_HateList>("HateEntry");
 	package.add("GetDamage", &Perl_HateEntry_GetDamage);
 	package.add("GetEnt", &Perl_HateEntry_GetEnt);
+	package.add("GetFrenzy", &Perl_HateEntry_GetFrenzy);
 	package.add("GetHate", &Perl_HateEntry_GetHate);
+	package.add("SetDamage", &Perl_HateEntry_SetDamage);
+	package.add("SetEnt", &Perl_HateEntry_SetEnt);
+	package.add("SetFrenzy", &Perl_HateEntry_SetFrenzy);
+	package.add("SetHate", &Perl_HateEntry_SetHate);
 }
 
 #endif //EMBPERL_XS_CLASSES
