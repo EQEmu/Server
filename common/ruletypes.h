70ce81fb0aad226065e23a4a7880f630c1a4915c
Jonathan Sider <jsider@gmail.com>
2023-07-15 19:46:26 -0700

    [Bug Fix] Fix rule check and add rule for pickpocket command (#3492)
    * Fix bugs in skills
    
    -Add rule for allowing pickpocket
    -Fix method that is supposed to check rule
    -Changed Z axis range for pickpocket (was failing on giants)
    
    * Add zoffset to account for taller models

--- common/ruletypes.h
+++ common/ruletypes.h
@@ -637,6 +637,7 @@ RULE_BOOL(Bots, OldRaceRezEffects, false, "Older clients had ID 757 for races wi
 RULE_BOOL(Bots, ResurrectionSickness, true, "Use Resurrection Sickness based on Resurrection spell cast, set to false to disable Resurrection Sickness.")
 RULE_INT(Bots, OldResurrectionSicknessSpell, 757, "757 is Default Old Resurrection Sickness Spell")
 RULE_INT(Bots, ResurrectionSicknessSpell, 756, "756 is Default Resurrection Sickness Spell")
+RULE_BOOL(Bots, AllowPickpocketCommand, true, "Allows the use of the bot command 'pickpocket'")
 RULE_CATEGORY_END()
 
 RULE_CATEGORY(Chat)
