1642eec06f3ced572d7fb58431572d53c19c03cd
Trust <fryguy503@gmail.com>
2023-08-07 21:56:45 -0400

    [Bug] Escape item name in trader audit.
    Quick fix to escape the item names in Trader Audit.

--- zone/trading.cpp
+++ zone/trading.cpp
@@ -1462,7 +1462,7 @@ static void BazaarAuditTrail(const char *seller, const char *buyer, const char *
 	std::string query = StringFormat("INSERT INTO `trader_audit` "
                                     "(`time`, `seller`, `buyer`, `itemname`, `quantity`, `totalcost`, `trantype`) "
                                     "VALUES (NOW(), '%s', '%s', '%s', %i, %i, %i)",
-                                    seller, buyer, itemName, quantity, totalCost, tranType);
+                                    seller, buyer, Strings::Escape(itemName).c_str(), quantity, totalCost, tranType);
 	database.QueryDatabase(query);
 }
 
