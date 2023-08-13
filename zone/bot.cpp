2fbc19dacdef81a11b80b359c66a1bbd097384df
Aeadoin <109764533+Aeadoin@users.noreply.github.com>
2023-08-13 15:03:08 -0400

    formatting (for real this time)

--- zone/bot.cpp
+++ zone/bot.cpp
@@ -119,7 +119,15 @@ Bot::Bot(NPCType *npcTypeData, Client* botOwner) : NPC(npcTypeData, nullptr, glm
 }
 
 // This constructor is used when the bot is loaded out of the database
-Bot::Bot(uint32 botID, uint32 botOwnerCharacterID, uint32 botSpellsID, double totalPlayTime, uint32 lastZoneId, NPCType *npcTypeData, int32 expansion_bitmask)
+Bot::Bot(
+		uint32 botID,
+		uint32 botOwnerCharacterID,
+		uint32 botSpellsID,
+		double totalPlayTime,
+		uint32 lastZoneId,
+		NPCType *npcTypeData,
+		int32 expansion_bitmask
+	)
 	: NPC(npcTypeData, nullptr, glm::vec4(), Ground, false), rest_timer(1), ping_timer(1)
 {
 	GiveNPCTypeData(npcTypeData);
