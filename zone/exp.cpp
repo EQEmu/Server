298ef3a65d05e7cfe42f872cf17c21fdf1d3dc0c
Valorith <rgagnier06@gmail.com>
2023-08-20 16:37:45 -0400

    Formatting

--- zone/exp.cpp
+++ zone/exp.cpp
@@ -1180,7 +1180,7 @@ void Raid::SplitExp(const uint64 exp, Mob* other) {
 	auto       raid_experience = exp;
 	const auto highest_level   = GetHighestLevel();
 
-	 if (RuleB(Character, EnableRaidEXPModifier)) {
+	if (RuleB(Character, EnableRaidEXPModifier)) {
 		raid_experience = static_cast<uint64>(
 				static_cast<float>(raid_experience) *
 				(1.0f - RuleR(Character, RaidExpMultiplier))
