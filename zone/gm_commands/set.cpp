cdb866a2cb0936456cb060c95d44962a8baa8c74
Kinglykrab <kinglykrab@gmail.com>
2023-08-20 09:08:11 -0400

    [Bug Fix] Fix #motd/#set motd Command
    # Notes
    - This command was non-functional since the consolidation due to an oversight on my part.

--- zone/gm_commands/set.cpp
+++ zone/gm_commands/set.cpp
@@ -33,6 +33,7 @@
 #include "set/loginserver_info.cpp"
 #include "set/mana.cpp"
 #include "set/mana_full.cpp"
+#include "set/motd.cpp"
 #include "set/name.cpp"
 #include "set/ooc_mute.cpp"
 #include "set/password.cpp"
@@ -98,6 +99,7 @@ void command_set(Client *c, const Seperator *sep)
 		Cmd{.cmd = "loginserver_info", .u = "loginserver_info [Email] [Password]", .fn = SetLoginserverInfo, .a = {"#setlsinfo"}},
 		Cmd{.cmd = "mana", .u = "mana [Amount]", .fn = SetMana, .a = {"#setmana"}},
 		Cmd{.cmd = "mana_full", .u = "mana_full", .fn = SetManaFull, .a = {"#mana"}},
+		Cmd{.cmd = "motd", .u = "motd", .fn = SetMOTD, .a = {"#motd"}},
 		Cmd{.cmd = "name", .u = "name", .fn = SetName, .a = {"#name"}},
 		Cmd{.cmd = "ooc_mute", .u = "ooc_mute", .fn = SetOOCMute, .a = {"#oocmute"}},
 		Cmd{.cmd = "password", .u = "password [Account Name] [Password] (account table password)", .fn = SetPassword, .a = {"#setpass"}},
