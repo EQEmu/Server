707041849a2b00c4ba4bbd65a28ad514c2a57acb
Noudess <noudess@gmail.com>
2023-08-31 09:59:57 -0400

    [Messages] Remove duplicate message on tracking begin

--- zone/client.cpp
+++ zone/client.cpp
@@ -11090,8 +11090,6 @@ void Client::SetTrackingID(uint32 entity_id)
 	}
 
 	TrackingID = entity_id;
-
-	MessageString(Chat::Skills, TRACKING_BEGIN, m->GetCleanName());
 }
 
 int Client::GetRecipeMadeCount(uint32 recipe_id)
