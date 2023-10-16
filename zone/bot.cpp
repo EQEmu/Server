EQEmu/Server

commit d8926cd5f37ba2092b1dd1eaa5925045426ddd2e

[Crash] Fix dangling pointer crash observed in SendHPPacketsFrom (#3611)
* [Crash] Fix dangling pointer crash observed in SendHPPacketsFrom

* Update bot.cpp

zone/bot.cpp
@@ -465,6 +465,14 @@ Bot::~Bot() {
 	}
 
 	entity_list.RemoveBot(GetID());
+
+	if (GetGroup()) {
+		GetGroup()->DelMember(this);
+	}
+
+	if (GetRaid()) {
+		GetRaid()->RemoveMember(GetName());
+	}
 }
 
 void Bot::SetBotID(uint32 botID) {

commit 09b34fbd0364ef00838ffab4c2bd235fae1c1b10

[Bots] Fix bot removal on zone, regression from #3611

zone/bot.cpp
@@ -467,7 +467,7 @@ Bot::~Bot() {
 	entity_list.RemoveBot(GetID());
 
 	if (GetGroup()) {
-		GetGroup()->DelMember(this);
+		GetGroup()->MemberZoned(this);
 	}
 
 	if (GetRaid()) {