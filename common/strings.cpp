ab33f54ac6a243fbcdea4f4884db176ad7a6bd6b
Akkadius <akkadius1@gmail.com>
2023-10-17 17:50:22 -0500

    Update strings.cpp

--- common/strings.cpp
+++ common/strings.cpp
@@ -51,11 +51,10 @@
 
 std::string Strings::Random(size_t length)
 {
-	static auto &chrs = "0123456789"
-						"abcdefghijklmnopqrstuvwxyz"
-						"ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	static auto &chrs = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
-	thread_local static std::mt19937                                          rg{std::random_device{}()};
+	thread_local static std::mt19937 rg{std::random_device{}()};
+
 	thread_local static std::uniform_int_distribution<std::string::size_type> pick(0, sizeof(chrs) - 2);
 
 	std::string s;
