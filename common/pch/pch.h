8af4fb2330d5102db7c8f636346fc9075983f89f
Akkadius <akkadius1@gmail.com>
2023-12-16 17:11:23 -0600

    Experiment with PCH

---
+++ common/pch/pch.h
@@ -0,0 +1,54 @@
+// types
+#include <limits>
+#include <string>
+#include <cctype>
+#include <sstream>
+
+// containers
+#include <iterator>
+#include <set>
+#include <unordered_set>
+#include <map>
+#include <unordered_map>
+#include <list>
+#include <vector>
+
+// utilities
+#include <iostream>
+#include <cassert>
+#include <cmath>
+#include <memory>
+#include <functional>
+#include <algorithm>
+#include <utility>
+#include <tuple>
+
+// fmt
+#include <fmt/format.h>
+
+// lua
+#include "lua.hpp"
+#include <luabind/luabind.hpp>
+#include <luabind/object.hpp>
+
+// perl
+//#include <perlbind/perlbind.h>
+
+// cereal
+#include <cereal/cereal.hpp>
+#include <cereal/archives/binary.hpp>
+#include <cereal/archives/json.hpp>
+#include <cereal/types/chrono.hpp>
+#include <cereal/types/string.hpp>
+#include <cereal/types/vector.hpp>
+
+// glm
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtx/transform.hpp>
+#include <glm/vec2.hpp>
+#include <glm/vec3.hpp>
+#include <glm/vec4.hpp>
+#include <glm/geometric.hpp>
+
+// zlib
+#include <zlib.h>
