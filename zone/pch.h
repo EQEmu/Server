5ff9407e26bcf6390ab70a4c85fc7aef2247cd1e
Akkadius <akkadius1@gmail.com>
2023-12-16 18:13:43 -0600

    Test

---
+++ zone/pch.h
@@ -0,0 +1,55 @@
+// types
+#include <limits>
+#include <string>
+#include <cctype>
+#include <sstream>
+
+// containers
+#include <iterator>
+#include <set>
+#include <unordered_set>
+#include <map>
+#include <unordered_map>
+#include <list>
+#include <vector>
+
+// utilities
+#include <iostream>
+#include <cassert>
+#include <cmath>
+#include <memory>
+#include <functional>
+#include <algorithm>
+#include <utility>
+#include <tuple>
+#include <fstream>
+#include <cstdio>
+
+
+// fmt
+#include <fmt/format.h>
+//# include <fmt/core.h>
+//# define FMT_STRING(s) s
+
+// lua
+#include "lua.hpp"
+#include <luabind/luabind.hpp>
+#include <luabind/object.hpp>
+
+// perl
+//#include <perlbind/perlbind.h>
+
+#include "../common/types.h"
+#include "../common/eqemu_logsys.h"
+#include "../common/eqemu_logsys_log_aliases.h"
+#include "../common/features.h"
+#include "../common/global_define.h"
+
+#include "mob.h"
+#include "npc.h"
+#include "corpse.h"
+#include "doors.h"
+#include "bot.h"
+#include "entity.h"
+#include "client.h"
+#include "zone.h"
