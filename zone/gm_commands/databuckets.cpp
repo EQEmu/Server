e4b437a8ca11b402521a42d52e957a39a56dc232
Kinglykrab <kinglykrab@gmail.com>
2023-12-30 17:54:23 -0500

    Add edit.

--- zone/gm_commands/databuckets.cpp
+++ zone/gm_commands/databuckets.cpp
@@ -7,38 +7,34 @@ void command_databuckets(Client *c, const Seperator *sep)
 {
 	const int arguments = sep->argnum;
 	if (!arguments) {
-		c->Message(Chat::White, "Usage: #databuckets delete [Key] [Character ID] [NPC ID] [Bot ID]");
-		c->Message(Chat::White, "Usage: #databuckets view [Partial Key] [Character ID] [NPC ID] [Bot ID]");
-		c->Message(Chat::White, "Note: Character ID, NPC ID, and Bot ID are optional if not needed, if needed they are required for specificity");
+		SendDataBucketsSubCommands(c);
 		return;
 	}
 
 	const bool is_delete = !strcasecmp(sep->arg[1], "delete");
+	const bool is_edit   = !strcasecmp(sep->arg[1], "edit");
 	const bool is_view   = !strcasecmp(sep->arg[1], "view");
 
 	if (
 		!is_delete &&
+		!is_edit &&
 		!is_view
 	) {
-		c->Message(Chat::White, "Usage: #databuckets delete [Key] [Character ID] [NPC ID] [Bot ID]");
-		c->Message(Chat::White, "Usage: #databuckets view [Partial Key] [Character ID] [NPC ID] [Bot ID]");
-		c->Message(Chat::White, "Note: Character ID, NPC ID, and Bot ID are optional if not needed, if needed they are required for specificity");
+		SendDataBucketsSubCommands(c);
 		return;
 	}
 
 	if (is_delete) {
 		if (arguments < 2) {
-			c->Message(Chat::White, "Usage: #databuckets delete [Key] [Character ID] [NPC ID] [Bot ID]");
-			c->Message(Chat::White, "Usage: #databuckets view [Partial Key] [Character ID] [NPC ID] [Bot ID]");
-			c->Message(Chat::White, "Note: Character ID, NPC ID, and Bot ID are optional if not needed, if needed they are required for specificity");
+			SendDataBucketsSubCommands(c);
 			return;
 		}
 
 		const std::string& key_filter = sep->arg[2];
 
-		uint32 character_id = sep->IsNumber(3) ? Strings::ToUnsignedInt(sep->arg[3]) : 0;
-		uint32 npc_id       = sep->IsNumber(4) ? Strings::ToUnsignedInt(sep->arg[4]) : 0;
-		uint32 bot_id       = sep->IsNumber(5) ? Strings::ToUnsignedInt(sep->arg[5]) : 0;
+		const uint32 character_id = sep->IsNumber(3) ? Strings::ToUnsignedInt(sep->arg[3]) : 0;
+		const uint32 npc_id       = sep->IsNumber(4) ? Strings::ToUnsignedInt(sep->arg[4]) : 0;
+		const uint32 bot_id       = sep->IsNumber(5) ? Strings::ToUnsignedInt(sep->arg[5]) : 0;
 
 		if (
 			!character_id &&
@@ -90,19 +86,59 @@ void command_databuckets(Client *c, const Seperator *sep)
 				).c_str()
 			);
 		}
+	} else if (is_edit) {
+		if (arguments < 6) {
+			SendDataBucketsSubCommands(c);
+			return;
+		}
+
+		const std::string& key_filter = sep->arg[2];
+		const std::string& value      = sep->arg[6];
+
+		const uint32 character_id = sep->IsNumber(3) ? Strings::ToUnsignedInt(sep->arg[3]) : 0;
+		const uint32 npc_id       = sep->IsNumber(4) ? Strings::ToUnsignedInt(sep->arg[4]) : 0;
+		const uint32 bot_id       = sep->IsNumber(5) ? Strings::ToUnsignedInt(sep->arg[5]) : 0;
+		const uint32 expires      = arguments > 6 && sep->IsNumber(7) ? Strings::ToUnsignedInt(sep->arg[7]) : 0;
+
+		DataBucketKey k = {};
+
+		k.key          = key_filter;
+		k.character_id = character_id;
+		k.npc_id       = npc_id;
+		k.bot_id       = bot_id;
+		k.value        = value;
+
+		if (arguments > 6) {
+			k.expires = expires;
+		}
+
+		const std::string& expires_string = expires == 0 ? "Never" : std::to_string(expires);
+
+		DataBucket::SetData(k);
+
+		c->Message(
+			Chat::White,
+			fmt::format(
+				"Bucket Edited | Key: {} Value: {} Expires: {} Character ID: {} NPC ID: {} Bot ID: {}",
+				key_filter,
+				value,
+				expires_string,
+				character_id,
+				npc_id,
+				bot_id
+			).c_str()
+		);
 	} else if (is_view) {
 		if (arguments < 2) {
-			c->Message(Chat::White, "Usage: #databuckets delete [Key] [Character ID] [NPC ID] [Bot ID]");
-			c->Message(Chat::White, "Usage: #databuckets view [Partial Key] [Character ID] [NPC ID] [Bot ID]");
-			c->Message(Chat::White, "Note: Character ID, NPC ID, and Bot ID are optional if not needed, if needed they are required for specificity");
+			SendDataBucketsSubCommands(c);
 			return;
 		}
 
 		const std::string& key_filter = sep->arg[2];
 
-		uint32 character_id = sep->IsNumber(3) ? Strings::ToUnsignedInt(sep->arg[3]) : 0;
-		uint32 npc_id       = sep->IsNumber(4) ? Strings::ToUnsignedInt(sep->arg[4]) : 0;
-		uint32 bot_id       = sep->IsNumber(5) ? Strings::ToUnsignedInt(sep->arg[5]) : 0;
+		const uint32 character_id = sep->IsNumber(3) ? Strings::ToUnsignedInt(sep->arg[3]) : 0;
+		const uint32 npc_id       = sep->IsNumber(4) ? Strings::ToUnsignedInt(sep->arg[4]) : 0;
+		const uint32 bot_id       = sep->IsNumber(5) ? Strings::ToUnsignedInt(sep->arg[5]) : 0;
 
 		std::string where_filter = fmt::format(
 			"`key` LIKE '%{}%'",
@@ -214,4 +250,13 @@ void command_databuckets(Client *c, const Seperator *sep)
 
 		c->Message(Chat::White, response.c_str());
 	}
+}
+
+void SendDataBucketsSubCommands(Client *c)
+{
+	c->Message(Chat::White, "Usage: #databuckets delete [Key] [Character ID] [NPC ID] [Bot ID]");
+	c->Message(Chat::White, "Usage: #databuckets edit [Key] [Character ID] [NPC ID] [Bot ID] [Value] [Expires]");
+	c->Message(Chat::White, "Usage: #databuckets view [Partial Key] [Character ID] [NPC ID] [Bot ID]");
+	c->Message(Chat::White, "Note: Character ID, NPC ID, and Bot ID are optional if not needed, if needed they are required for specificity");
+	c->Message(Chat::White, "Note: Edit requires Character ID, NPC ID, Bot ID, and Value, Expires is optional and does not modify the existing expiration time if not provided");
 }
