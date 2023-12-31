cc3a2e866f1e0c3fca1d2b180e36313da181f44e
Kinglykrab <kinglykrab@gmail.com>
2023-12-30 22:54:34 -0500

    [Commands] Cleanup #shutdown Command
    # Notes
    - Cleanup messages and logic.
    - Add a confirmation so operators don't accidentally shutdown the zone they're in thinking this is `#worldshutdown` instead.

--- zone/gm_commands/shutdown.cpp
+++ zone/gm_commands/shutdown.cpp
@@ -3,6 +3,22 @@
 
 void command_shutdown(Client *c, const Seperator *sep)
 {
-	CatchSignal(2);
-}
+	const int arguments = sep->argnum;
+	if (!arguments) {
+		c->Message(
+			Chat::White,
+			fmt::format(
+				"Using this command will shut down your current zone. Please {} this is what you want to do.",
+				Saylink::Silent("#shutdown confirm", "confirm")
+			).c_str()
+		);
+		return;
+	}
 
+	const bool is_confirm = !strcasecmp(sep->arg[1], "confirm");
+
+	if (is_confirm) {
+		c->Message(Chat::White, "Shutting down your current zone.");
+		CatchSignal(2);
+	}
+}
