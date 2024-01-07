e3588b1ea245117350d1729a26818aefb4053144
Trust <fryguy503@gmail.com>
2024-01-06 23:38:05 -0500

    Remove un-needed logic due to legacy code.

--- zone/client_process.cpp
+++ zone/client_process.cpp
@@ -1696,7 +1696,7 @@ void Client::OPGMTrainSkill(const EQApplicationPacket *app)
 
 		uint16 skilllevel = GetRawSkill(skill);
 
-		if(skilllevel == 0 || (skilllevel == 254 && SkillTrainLevel(skill, GetClass()) <= GetLevel())) {
+		if (skilllevel == 0) {
 			//this is a new skill..
 			uint16 t_level = SkillTrainLevel(skill, GetClass());
 
