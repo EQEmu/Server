a89460e27ca770f3a5aee9e0423fb2b74edb9d5b
Trust <fryguy503@gmail.com>
2024-01-07 14:40:53 -0500

    [Bug Fix] Amplification should not benefit from instrument mods

--- zone/client_mods.cpp
+++ zone/client_mods.cpp
@@ -1455,7 +1455,7 @@ int32 Client::CalcATK()
 
 uint32 Mob::GetInstrumentMod(uint16 spell_id)
 {
-	if (GetClass() != Class::Bard) {
+	if (GetClass() != Class::Bard || spells[spell_id].is_discipline || spell_id == SPELL_AMPLIFICATION) {
 		//Other classes can get a base effects mod using SPA 413
 		if (HasBaseEffectFocus()) {
 			return (10 + (GetFocusEffect(focusFcBaseEffects, spell_id) / 10));//TODO: change action->instrument mod to float to support < 10% focus values
