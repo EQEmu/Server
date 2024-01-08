044796696f521904fe00eb1d22ee0160799266a5
Trust <fryguy503@gmail.com>
2024-01-07 20:43:06 -0500

    Correction to location

--- zone/spells.cpp
+++ zone/spells.cpp
@@ -1395,12 +1395,6 @@ void Mob::CastedSpellFinished(uint16 spell_id, uint32 target_id, CastingSlot slo
 
 	bool IsFromItem = false;
 	EQ::ItemInstance *item = nullptr;
-
-	if (IsClient() && (IsDiscipline(spell_id) || IsDisciplineBuff(spell_id))) {
-		std::string msg = "%s";
-		msg += spells[spell_id].cast_on_other;
-		entity_list.MessageClose(this, true, 200, 0, msg.c_str(), this->GetCleanName());
-	}
 	
 	if(IsClient() && slot != CastingSlot::Item && slot != CastingSlot::PotionBelt && spells[spell_id].recast_time > 1000) { // 10 is item
 		if(!CastToClient()->GetPTimers().Expired(&database, pTimerSpellStart + spell_id, false)) {
@@ -3802,6 +3796,12 @@ bool Mob::SpellOnTarget(
 	uint16 caster_level = level_override > 0 ? level_override : GetCasterLevel(spell_id);
 
 	LogSpells("Casting spell [{}] on [{}] with effective caster level [{}]", spell_id, spelltar->GetName(), caster_level);
+
+	if (IsClient() && (IsDiscipline(spell_id) || spells[spell_id].is_discipline)) {
+		std::string msg = "%s";
+		msg += spells[spell_id].cast_on_other;
+		entity_list.MessageClose(this, false, 200, 0, msg.c_str(), this->GetCleanName());
+	}
 
 	// Actual cast action - this causes the caster animation and the particles
 	// around the target
