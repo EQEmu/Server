32659426bacd3e790a485deb2628d27eb1d0bef4
Alex King <89047260+Kinglykrab@users.noreply.github.com>
2024-01-13 00:38:13 -0500

    [AAs] Convert Loading of AAs to Repositories (#3963)
    * [AAs] Convert Loading of AAs to Repositories
    
    - Convert `LoadAlternateAdvancementAbilities()` to repositories.
    - Regenerate repositories since a column was added to `aa_ability`.
    
    * Update aa.cpp
    
    * Update aa.cpp

--- common/repositories/base/base_aa_rank_effects_repository.h
+++ common/repositories/base/base_aa_rank_effects_repository.h
@@ -16,6 +16,7 @@
 #include "../../strings.h"
 #include <ctime>
 
+
 class BaseAaRankEffectsRepository {
 public:
 	struct AaRankEffects {
--- common/repositories/base/base_aa_ranks_repository.h
+++ common/repositories/base/base_aa_ranks_repository.h
@@ -16,6 +16,7 @@
 #include "../../strings.h"
 #include <ctime>
 
+
 class BaseAaRanksRepository {
 public:
 	struct AaRanks {
