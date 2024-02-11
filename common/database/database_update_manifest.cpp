86a2a86ba8ca4a07255c2f83a8c493857474053c
JJ <3617814+joligario@users.noreply.github.com>
2024-02-11 16:12:07 -0500

    [Database] Fix default value for `time_of_death` in `character_corpses` (#4060)
    * [Database] Fix default value for `time_of_death` in `character_corpses`
    
    * Extra `NULL`
    
    * Update repository
    
    * Repositories with updated structure this time

--- common/database/database_update_manifest.cpp
+++ common/database/database_update_manifest.cpp
@@ -5346,6 +5346,16 @@ CREATE TABLE guild_tributes (
 ) ENGINE=InnoDB;
 )"
 	},
+	ManifestEntry{
+		.version = 9261,
+		.description = "2024_02_11_character_corpses.sql",
+		.check = "SHOW COLUMNS FROM `character_corpses` LIKE 'time_of_death'",
+		.condition = "contains",
+		.match = "0000-00-00 00:00:00",
+		.sql = R"(
+ALTER TABLE `character_corpses` MODIFY COLUMN `time_of_death` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP;
+		)"
+	}
 // -- template; copy/paste this when you need to create a new entry
 //	ManifestEntry{
 //		.version = 9228,
--- common/version.h
+++ common/version.h
@@ -42,7 +42,7 @@
  * Manifest: https://github.com/EQEmu/Server/blob/master/utils/sql/db_update_manifest.txt
  */
 
-#define CURRENT_BINARY_DATABASE_VERSION 9260
+#define CURRENT_BINARY_DATABASE_VERSION 9261
 #define CURRENT_BINARY_BOTS_DATABASE_VERSION 9042
 
 #endif
