f505c2cfd2e3e007fd52b5198955f56763f01778
Alex King <89047260+Kinglykrab@users.noreply.github.com>
2024-02-20 23:23:22 -0500

    [Feature] Add Augments to Starting Items for Bots/Players (#4100)
    * [Feature] Add Augments to Starting Items for Bots/Players
    
    - Adds support for augments to `bot_starting_items` and `starting_items`.
    - Will allow operators to kit bots/players out with augmented gear from the start if they so choose.
    
    * Update database_update_manifest.cpp
    
    * Update client.cpp

--- common/version.h
+++ common/version.h
@@ -42,8 +42,8 @@
  * Manifest: https://github.com/EQEmu/Server/blob/master/utils/sql/db_update_manifest.txt
  */
 
-#define CURRENT_BINARY_DATABASE_VERSION 9263
-#define CURRENT_BINARY_BOTS_DATABASE_VERSION 9042
+#define CURRENT_BINARY_DATABASE_VERSION 9264
+#define CURRENT_BINARY_BOTS_DATABASE_VERSION 9043
 
 #endif
 
