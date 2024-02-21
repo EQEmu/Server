a221e50cc2ed5c7008da01bb2bdbf025820251ea
Fryguy <fryguy503@gmail.com>
2024-02-20 23:21:56 -0500

    [Bug Fix] Add safety check to SummonAllCharacterCorpses. (#4107)
    Should prevent an empty vector from being passed to the repository.

--- zone/zonedb.cpp
+++ zone/zonedb.cpp
@@ -3988,7 +3988,9 @@ bool ZoneDatabase::SummonAllCharacterCorpses(
 		}
 	}
 
-	CharacterCorpsesRepository::ReplaceMany(*this, l);
+	if (!l.empty()) {
+		CharacterCorpsesRepository::ReplaceMany(*this, l);
+	}
 
 	return corpse_count > 0;
 }
