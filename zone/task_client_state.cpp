72c2e77d5f4e13949c5b4d9e2f002d9ddbbfaae7
catapultam-habeo <97849758+catapultam-habeo@users.noreply.github.com>
2024-03-02 00:25:05 -0600

    initial commit

--- zone/task_client_state.cpp
+++ zone/task_client_state.cpp
@@ -2290,8 +2290,20 @@ void ClientTaskState::CreateTaskDynamicZone(Client* client, int task_id, Dynamic
 	}
 
 	// dz should be named the version-based zone name (used in choose zone window and dz window on live)
-	auto zone_info = zone_store.GetZone(dz_request.GetZoneID(), dz_request.GetZoneVersion());
-	dz_request.SetName(zone_info->long_name.empty() ? task->title : zone_info->long_name);
+    auto zone_info = zone_store.GetZone(dz_request.GetZoneID(), dz_request.GetZoneVersion());
+
+	// use version 0 name for this if there is no zone with the specified version
+    if (!zone_info) {
+      zone_info = zone_store.GetZone(dz_request.GetZoneID());
+    }
+
+	// fall back to task title if that doesn't exist somehow
+    if (zone_info) { 
+      dz_request.SetName(zone_info->long_name.empty() ? task->title : zone_info->long_name);
+    } else {
+      dz_request.SetName(task->title);        
+    }
+
 	dz_request.SetMinPlayers(task->min_players);
 	dz_request.SetMaxPlayers(task->max_players);
 
