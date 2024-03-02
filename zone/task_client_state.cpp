9398efcc79391a0dbf75c461661222c29653a794
catapultam-habeo <97849758+catapultam-habeo@users.noreply.github.com>
2024-03-01 21:56:40 -0600

    Fix ad-hoc instances (#25)

--- zone/task_client_state.cpp
+++ zone/task_client_state.cpp
@@ -2291,7 +2291,17 @@ void ClientTaskState::CreateTaskDynamicZone(Client* client, int task_id, Dynamic
 
 	// dz should be named the version-based zone name (used in choose zone window and dz window on live)
 	auto zone_info = zone_store.GetZone(dz_request.GetZoneID(), dz_request.GetZoneVersion());
-	dz_request.SetName(zone_info->long_name.empty() ? task->title : zone_info->long_name);
+
+	if (!zone_info) {
+		zone_info = zone_store.GetZone(dz_request.GetZoneID());
+	}
+
+	if (zone_info) { 
+		dz_request.SetName(zone_info->long_name.empty() ? task->title : zone_info->long_name);
+	} else {
+		dz_request.SetName(task->title);		
+	}
+
 	dz_request.SetMinPlayers(task->min_players);
 	dz_request.SetMaxPlayers(task->max_players);
 
