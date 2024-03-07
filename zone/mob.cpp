37021d4d7a8859b2e4f47242dba6c54110367333
mute <natanx@gmail.com>
2024-03-07 09:19:43 -0800

    Adds IsClient() check around ImpliedTargeting function for ExecWeapon… (#30)

--- zone/mob.cpp
+++ zone/mob.cpp
@@ -5250,10 +5250,11 @@ void Mob::ExecWeaponProc(const EQ::ItemInstance* inst, uint16 spell_id, Mob* on,
 		return;
 	}
 
-	Mob* new_target = entity_list.GetMob(GetSpellImpliedTargetID(spell_id, on->GetID()));
-
-	if (new_target) {
-		on = new_target;
+	if (IsClient()) {
+		Mob* new_target = entity_list.GetMob(GetSpellImpliedTargetID(spell_id, on->GetID()));
+		if (new_target) {
+			on = new_target;
+		}
 	}
 
 	if (IsBot() && on->GetSpecialAbility(IMMUNE_DAMAGE_BOT)) {
