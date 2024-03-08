96370e0298cd49af865400cab114cfcb0d9e7344
MortimerGreenwald <110543605+MortimerGreenwald@users.noreply.github.com>
2024-03-08 12:29:53 -0800

    [Bug Fix] An Update to Xtarget to exclude Bot owned Temp/Swarm Pets (#4172)
    * An Update to Xtarget to exclude Bot owned Temp/Swarm Pets
    
    * Missing a parentheses
    
    * Cleaned up logic.

--- zone/client.cpp
+++ zone/client.cpp
@@ -6775,7 +6775,7 @@ void Client::UpdateClientXTarget(Client *c)
 // IT IS NOT SAFE TO CALL THIS IF IT'S NOT INITIAL AGGRO
 void Client::AddAutoXTarget(Mob *m, bool send)
 {
-	if (m->IsBot() || (m->IsPet() && m->IsPetOwnerBot())) {
+	if (m->IsBot() || ((m->IsPet() || m->IsTempPet()) && m->IsPetOwnerBot())) {
 		return;
 	}
 
