47a0cf69129332e601b7500d8d438cc3bccd3fac
Mitch Freeman <65987027+neckkola@users.noreply.github.com>
2024-04-20 23:06:27 -0300

    Remove parcel merchant content to optional sql instead of manifest.

--- common/database/database_update_manifest.cpp
+++ common/database/database_update_manifest.cpp
@@ -5519,10 +5519,6 @@ ALTER TABLE `lootdrop_entries` ADD `content_flags_disabled` varchar(100) NULL;
 
 				ALTER TABLE `npc_types`
 				ADD COLUMN `is_parcel_merchant` TINYINT(1) UNSIGNED NOT NULL DEFAULT '0' AFTER `keeps_sold_items`;
-
-				UPDATE `npc_types` SET `is_parcel_merchant` = 1, `lastname` = 'Parcels and General Supplies'
-				WHERE id IN (202129, 3036, 394025, 75113, 49073, 41021, 40070, 106115, 55150, 9053, 382156, 1032,
-				155088, 23017, 61065, 29008, 67058, 54067, 19031, 50140);
 		)"
 	}
 // -- template; copy/paste this when you need to create a new entry
