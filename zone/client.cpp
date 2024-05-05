ad3a6752222d8a6d84dc456c561f95b6754890b9
Akkadius <akkadius1@gmail.com>
2024-05-04 21:07:24 -0500

    [Hotfix] GetSkill default return 0

--- zone/client.cpp
+++ zone/client.cpp
@@ -12418,25 +12418,27 @@ std::vector<Mob*> Client::GetRaidOrGroupOrSelf(bool clients_only)
 uint16 Client::GetSkill(EQ::skills::SkillType skill_id) const
 {
 	if (skill_id <= EQ::skills::HIGHEST_SKILL) {
-
 		if (spellbonuses.ReduceSkill[skill_id] > 0) {
-
 			if (itembonuses.skillmod[skill_id] > 0) {
-
 				if (itembonuses.skillmodmax[skill_id] > 0) {
-					return std::min(m_pp.skills[skill_id] + itembonuses.skillmodmax[skill_id], m_pp.skills[skill_id] + ((m_pp.skills[skill_id] * (itembonuses.skillmod[skill_id] - spellbonuses.ReduceSkill[skill_id])) / 100));
+					return std::min(
+						m_pp.skills[skill_id] + itembonuses.skillmodmax[skill_id],
+						m_pp.skills[skill_id] + ((m_pp.skills[skill_id] * (itembonuses.skillmod[skill_id] -
+																		   spellbonuses.ReduceSkill[skill_id])) / 100));
 				}
-				return m_pp.skills[skill_id] + ((m_pp.skills[skill_id] * (itembonuses.skillmod[skill_id] - spellbonuses.ReduceSkill[skill_id])) / 100);
+				return m_pp.skills[skill_id] + ((m_pp.skills[skill_id] * (itembonuses.skillmod[skill_id] -
+																		  spellbonuses.ReduceSkill[skill_id])) / 100);
 			}
 			return m_pp.skills[skill_id] - ((m_pp.skills[skill_id] * spellbonuses.ReduceSkill[skill_id]) / 100);
 		}
 
 		if (itembonuses.skillmod[skill_id] > 0) {
-
-			if (itembonuses.skillmodmax[skill_id] > 0){
-				
+			if (itembonuses.skillmodmax[skill_id] > 0) {
 				if (itembonuses.skillmodmax[skill_id] > 0) {
-					return std::min(m_pp.skills[skill_id] + itembonuses.skillmodmax[skill_id], m_pp.skills[skill_id] * (100 + itembonuses.skillmod[skill_id]) / 100);
+					return std::min(
+						m_pp.skills[skill_id] + itembonuses.skillmodmax[skill_id],
+						m_pp.skills[skill_id] * (100 + itembonuses.skillmod[skill_id]) / 100
+					);
 				}
 				return m_pp.skills[skill_id] * (100 + itembonuses.skillmod[skill_id]) / 100;
 			}
@@ -12444,4 +12446,6 @@ uint16 Client::GetSkill(EQ::skills::SkillType skill_id) const
 		}
 		return 0;
 	}
+
+	return 0;
 }
