9c60eb03219b48253cd32f5b959c5072a46eceab
Alex King <89047260+Kinglykrab@users.noreply.github.com>
2024-05-17 09:33:59 -0400

    Update resetaa.cpp

--- zone/gm_commands/resetaa.cpp
+++ zone/gm_commands/resetaa.cpp
@@ -2,23 +2,20 @@
 
 void command_resetaa(Client *c, const Seperator *sep)
 {
-	if (!c->GetTarget() || !c->GetTarget()->IsClient()) {
-		c->Message(Chat::White, "You must target a player to use this command.");
-		return;
+	Client* t = c;
+	if (c->GetTarget() && c->GetTarget()->IsClient()) {
+		t = c->GetTarget()->CastToClient();
 	}
 
-	const uint16 arguments = sep->argnum;
+	const uint16 arguments     = sep->argnum;
+	const bool   is_aa         = !strcasecmp(sep->arg[1], "aa");
+	const bool   is_leadership = !strcasecmp(sep->arg[1], "leadership");
 
-	const bool is_aa         = !strcasecmp(sep->arg[1], "aa");
-	const bool is_leadership = !strcasecmp(sep->arg[1], "leadership");
-
-	if (!is_aa && !is_leadership) {
+	if (!arguments || (!is_aa && !is_leadership)) {
 		c->Message(Chat::White, "Usage: #resetaa aa - Resets and refunds a player's AAs");
 		c->Message(Chat::White, "Usage: #resetaa leadership - Resets a player's Leadership AAs");
 		return;
 	}
-
-	Client* t = c->GetTarget()->CastToClient();
 
 	if (is_aa) {
 		t->ResetAA();
