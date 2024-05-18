2d4007250e6088031f9ba7750b5678e5abfedfea
Kinglykrab <kinglykrab@gmail.com>
2024-05-18 13:55:01 -0400

    [Bug Fix] Fix issue with #suspend

--- zone/gm_commands/suspend.cpp
+++ zone/gm_commands/suspend.cpp
@@ -6,46 +6,56 @@ extern WorldServer worldserver;
 
 void command_suspend(Client *c, const Seperator *sep)
 {
-	auto arguments = sep->argnum;
+	const uint16 arguments = sep->argnum;
 	if (arguments < 2 || !sep->IsNumber(2)) {
 		c->Message(Chat::White, "Usage: #suspend [Character Name] [Days] [Reason]");
-		c->Message(Chat::White, "Note: Specify 0 days to lift a suspension");
+		c->Message(Chat::White, "Note: Specify 0 days to lift a suspension, reason is not required when removing a suspension");
 		return;
 	}
 
-	const std::string character_name = Strings::ToLower(sep->arg[1]);
-	auto days = Strings::ToUnsignedInt(sep->arg[2]);
+	const std::string& character_name = sep->arg[1];
 
-	const std::string reason = sep->arg[3] ? sep->argplus[3] : "";
+	const auto& e = CharacterDataRepository::FindByName(database, character_name);
 
-	auto l = AccountRepository::GetWhere(
-		database,
-		fmt::format(
-			"LOWER(charname) = '{}'",
-			Strings::Escape(character_name)
-		)
-	);
-
-	if (l.empty()) {
+	if (!e.id) {
 		c->Message(
 			Chat::White,
 			fmt::format(
 				"Character '{}' does not exist.",
-				sep->arg[1]
+				character_name
 			).c_str()
 		);
 		return;
 	}
 
-	l[0].status = -1;
-	l[0].suspendeduntil = std::time(nullptr) + (days * 86400);
-	l[0].suspend_reason = reason;
+	auto a = AccountRepository::FindOne(database, e.account_id);
 
-	if (!AccountRepository::UpdateOne(database, l[0])) {
+	if (!a.id) {
 		c->Message(
 			Chat::White,
 			fmt::format(
-				"Failed to suspend {}.",
+				"Character '{}' is not attached to an account.",
+				character_name
+			).c_str()
+		);
+		return;
+	}
+
+	const uint32 days         = Strings::ToUnsignedInt(sep->arg[2]);
+	const bool   is_suspend = days != 0;
+
+	const std::string reason = sep->arg[3] ? sep->argplus[3] : "";
+
+	a.status         = is_suspend ? -1 : 0;
+	a.suspendeduntil = is_suspend ? std::time(nullptr) + (days * 86400) : 0;
+	a.suspend_reason = is_suspend ? reason : "";
+
+	if (!AccountRepository::UpdateOne(database, a)) {
+		c->Message(
+			Chat::White,
+			fmt::format(
+				"Failed to {}suspend {}.",
+				is_suspend ? "" : "un",
 				character_name
 			).c_str()
 		);
@@ -56,13 +66,13 @@ void command_suspend(Client *c, const Seperator *sep)
 		Chat::White,
 		fmt::format(
 			"Account {} ({}) with the character {} {}.",
-			l[0].name,
-			l[0].id,
+			a.name,
+			a.id,
 			character_name,
 			(
-				days ?
+				is_suspend ?
 				fmt::format(
-					"has been temporarily suspended for {} day{}.",
+					"has been suspended for {} day{}",
 					days,
 					days != 1 ? "s" : ""
 				) :
@@ -71,22 +81,24 @@ void command_suspend(Client *c, const Seperator *sep)
 		).c_str()
 	);
 
-	auto* b = entity_list.GetClientByName(character_name.c_str());
+	if (is_suspend) { // Only kick if we're suspending, otherwise there's no reason to kick someone who is already suspended
+		Client* b = entity_list.GetClientByName(character_name.c_str());
 
-	if (b) {
-		b->WorldKick();
-		return;
+		if (b) {
+			b->WorldKick();
+			return;
+		}
+
+		auto pack = new ServerPacket(ServerOP_KickPlayer, sizeof(ServerKickPlayer_Struct));
+		auto* k = (ServerKickPlayer_Struct*) pack->pBuffer;
+
+		strn0cpy(k->adminname, c->GetName(), sizeof(k->adminname));
+		strn0cpy(k->name, character_name.c_str(), sizeof(k->name));
+		k->adminrank = c->Admin();
+
+		worldserver.SendPacket(pack);
+
+		safe_delete(pack);
 	}
-
-	auto pack = new ServerPacket(ServerOP_KickPlayer, sizeof(ServerKickPlayer_Struct));
-	auto* k = (ServerKickPlayer_Struct *) pack->pBuffer;
-
-	strn0cpy(k->adminname, c->GetName(), sizeof(k->adminname));
-	strn0cpy(k->name, character_name.c_str(), sizeof(k->name));
-	k->adminrank = c->Admin();
-
-	worldserver.SendPacket(pack);
-
-	safe_delete(pack);
 }
 
