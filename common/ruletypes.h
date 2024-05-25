ca25122bfaaceee4c836b3548fa8e929f0836215
Fryguy <fryguy503@gmail.com>
2024-05-25 14:28:20 -0400

    [Rule] Allow servers to adjust the filtering threshold for heals from damage (e.g. Mark of Kings). (#4327)

--- common/ruletypes.h
+++ common/ruletypes.h
@@ -505,6 +505,7 @@ RULE_BOOL(Spells, ManaTapsRequireNPCMana, false, "Enabling will require target t
 RULE_INT(Spells, HarmTouchCritRatio, 200, "Harmtouch crit bonus, on top of BaseCritRatio")
 RULE_BOOL(Spells, UseClassicSpellFocus, false, "Enabling will tell the server to handle random focus damage as classic spell imports lack the limit values.")
 RULE_BOOL(Spells, ManaTapsOnAnyClass, false, "Enabling this will allow you to cast mana taps on any class, this will bypass ManaTapsRequireNPCMana rule.")
+RULE_INT(Spells, HealAmountMessageFilterThreshold, 100, "Lifetaps below this threshold will not have a message sent to the client (Heal will still process) 0 to Disable.")
 RULE_CATEGORY_END()
 
 RULE_CATEGORY(Combat)
