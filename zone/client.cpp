402d61ab2ed19d716cca6bf84597a94c7c4e24e1
Kinglykrab <kinglykrab@gmail.com>
2024-05-26 08:43:46 -0400

    Update client.cpp

--- zone/client.cpp
+++ zone/client.cpp
@@ -3199,9 +3199,7 @@ bool Client::BindWound(Mob *bindmob, bool start, bool fail)
 								percent_base = 70;
 						}
 
-						int percent_bonus = 0;
-						if (percent_base >= 70)
-							percent_bonus = spellbonuses.MaxBindWound + itembonuses.MaxBindWound + aabonuses.MaxBindWound;
+						int percent_bonus = spellbonuses.MaxBindWound + itembonuses.MaxBindWound + aabonuses.MaxBindWound;
 
 						int max_percent = percent_base + percent_bonus;
 						if (max_percent < 0)
