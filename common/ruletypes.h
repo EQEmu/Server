a6d40cc77580a5149c026d4148b8fc065470feb2
Trust <fryguy503@gmail.com>
2024-05-26 20:05:54 -0500

    Adjustments to Crippling Blows/Slay Undead and Confirmed Critical Code

--- common/ruletypes.h
+++ common/ruletypes.h
@@ -615,6 +615,7 @@ RULE_INT(Combat, PCAccuracyAvoidanceMod2Scale, 100, "Scale Factor for PC Accurac
 RULE_BOOL(Combat, AllowRaidTargetBlind, false, "Toggle to allow raid targets to be blinded, default is false (Live-like)")
 RULE_BOOL(Combat, RogueBackstabHasteCorrection, false, "Toggle to enable correction for Haste impacting Backstab DPS too much.  DEFAULT: false")
 RULE_BOOL(Combat, LegacyComputeDefense, false, "Trim AGI Scaling of defense mostly for lower levels to help compensate for the newer agi based defense system. Default: False")
+RULE_REAL(Combat, SlayDamageAdjustment, 0.5, "Slay Damage Adjustment Default: 0.5")
 RULE_CATEGORY_END()
 
 RULE_CATEGORY(NPC)
