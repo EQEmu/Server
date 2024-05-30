bfb04bd0bd51c6da3f0c4e50533008d7e2054e18
Kinglykrab <kinglykrab@gmail.com>
2024-05-30 01:45:39 -0400

    Command

--- zone/gm_commands/find.cpp
+++ zone/gm_commands/find.cpp
@@ -16,6 +16,7 @@
 #include "find/race.cpp"
 #include "find/recipe.cpp"
 #include "find/skill.cpp"
+#include "find/stance.cpp"
 #include "find/spell.cpp"
 #include "find/special_ability.cpp"
 #include "find/task.cpp"
@@ -58,6 +59,7 @@ void command_find(Client *c, const Seperator *sep)
 		Cmd{.cmd = "recipe", .u = "recipe [Search Criteria]", .fn = FindRecipe, .a = {"#findrecipe"}},
 		Cmd{.cmd = "skill", .u = "skill [Search Criteria]", .fn = FindSkill, .a = {"#findskill"}},
 		Cmd{.cmd = "special_ability", .u = "special_ability [Search Criteria]", .fn = FindSpecialAbility, .a = {"#fsa", "#findspecialability"}},
+		Cmd{.cmd = "stance", .u = "stance [Search Criteria]", .fn = FindStance, .a = {"#findstance"}},
 		Cmd{.cmd = "spell", .u = "spell [Search Criteria]", .fn = FindSpell, .a = {"#fs", "#findspell"}},
 		Cmd{.cmd = "task", .u = "task [Search Criteria]", .fn = FindTask, .a = {"#findtask"}},
 		Cmd{.cmd = "zone", .u = "zone [Search Criteria]", .fn = FindZone, .a = {"#fz", "#findzone"}},
