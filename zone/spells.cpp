a2e8ccdbf7cfb97526a3ca9ee39d4e20bee51f6f
catapultam-habeo <alex@catapult.am>
2024-06-21 17:45:25 +0000

    adjust dire charm behavior

--- zone/spells.cpp
+++ zone/spells.cpp
@@ -5162,21 +5162,20 @@ bool Mob::IsImmuneToSpell(uint16 spell_id, Mob *caster)
 
 			// Special handling for dire charm
 			if (spell_id != 2759 || spell_id != 2760 || spell_id != 2761) {
-				if (caster->GetLevelCon(GetLevel()) == ConsiderColor::LightBlue ||
-				    caster->GetLevelCon(GetLevel()) == ConsiderColor::Green ||
-					caster->GetLevelCon(GetLevel()) == ConsiderColor::Gray) {
+				if (caster->GetLevelCon(GetLevel()+1) == ConsiderColor::LightBlue ||
+				    caster->GetLevelCon(GetLevel()+1) == ConsiderColor::Green ||
+					caster->GetLevelCon(GetLevel()+1) == ConsiderColor::Gray) {
 
 					return false;		
 				}
 
 				if (caster->GetLevel() <= 46) {
-					if (caster->GetLevelCon(GetLevel()) != ConsiderColor::Yellow &&
-					    caster->GetLevelCon(GetLevel()) != ConsiderColor::Red) {
+					if (caster->GetLevelCon(GetLevel()+1) != ConsiderColor::Yellow &&
+					    caster->GetLevelCon(GetLevel()+1) != ConsiderColor::Red) {
 
 						return false;
 					}
 				}
-
 				return true;
 			} else {		
 				if(GetLevel() > spells[spell_id].max_value[effect_index] && spells[spell_id].max_value[effect_index] != 0)
