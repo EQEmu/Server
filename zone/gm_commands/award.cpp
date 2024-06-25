6aab97e1a53112899a4ebd21fdbe6e23cf9b1da8
catapultam-habeo <alex@catapult.am>
2024-06-25 04:21:45 +0000

    add Reason to award command

--- zone/gm_commands/award.cpp
+++ zone/gm_commands/award.cpp
@@ -5,36 +5,38 @@
 
 void command_award(Client *c, const Seperator *sep)
 {
-	int arguments = sep->argnum;
-	if (!arguments || !sep->IsNumber(2)) {
-		c->Message(Chat::White, "Usage: #award [Character Name] [Amount]");
-		return;
-	}
+    int arguments = sep->argnum;
+    if (arguments < 4 || !sep->IsNumber(2)) {
+        c->Message(Chat::White, "Usage: #award [Character Name] [Amount] [Reason]");
+        return;
+    }
 
-	const auto& l = CharacterDataRepository::GetWhere(
-		database,
-		fmt::format(
-			"`name` = '{}'",
-			Strings::Escape(sep->arg[1])
-		)
-	);
+    const auto& l = CharacterDataRepository::GetWhere(
+        database,
+        fmt::format(
+            "`name` = '{}'",
+            Strings::Escape(sep->arg[1])
+        )
+    );
+    
+    if (l.empty()) {
+        c->Message(Chat::White, "Unable to find character %s", Strings::Escape(sep->arg[1]));
+        return;
+    }
 
-	if (l.empty()) {
-		c->Message(Chat::White, "Unable to find character %s", Strings::Escape(sep->arg[1]));
-		return;
-	}
+    auto& e = l.front();
+    
+    DataBucketKey k;
+    k.character_id = e.id;
+    k.key = "EoM-Award";
+    k.value = sep->arg[2];
 
-	auto& e = l.front();
-	
-	DataBucketKey k;
-	k.character_id = e.id;
-	k.key = "EoM-Award";
-	k.value = sep->arg[2];
+    DataBucket::SetData(k);    
 
-	DataBucket::SetData(k);	
+    std::string reason = sep->arg[3];
 
-	c->Message(Chat::White, "Awarded %d EoM to %s.", Strings::ToInt(sep->arg[2]), sep->arg[1]);
-	zone->SendDiscordMessage("admin", fmt::to_string(c->GetCleanName()) + " awarded " + sep->arg[2] + " EoM to " + sep->arg[1] + ".");
+    c->Message(Chat::White, "Awarded %d EoM to %s for %s.", Strings::ToInt(sep->arg[2]), sep->arg[1], reason.c_str());
+    zone->SendDiscordMessage("admin", fmt::to_string(c->GetCleanName()) + " awarded " + sep->arg[2] + " EoM to " + sep->arg[1] + " Reason: " + reason);
 
-	quest_manager.WorldWideSignal(WWSignalUpdateType_Character, 666);
+    quest_manager.WorldWideSignal(WWSignalUpdateType_Character, 666);
 }
