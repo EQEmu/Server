e5cea73e0c9180a9ba5594c7bd2b020dd1eda758
Alex King <89047260+Kinglykrab@users.noreply.github.com>
2024-07-23 01:33:09 -0400

    [Bug Fix] Fix Client::RemoveTitle (#4421)
    * [Bug Fix] Fix Client::RemoveTitle
    
    * Remove title/suffix if in use.
    
    * Update titles.cpp
    
    * Non static

--- zone/titles.cpp
+++ zone/titles.cpp
@@ -19,7 +19,7 @@
 #include "../common/eq_packet_structs.h"
 #include "../common/strings.h"
 #include "../common/misc_functions.h"
-#include "../common/repositories/titles_repository.h"
+#include "../common/repositories/player_titlesets_repository.h"
 
 #include "client.h"
 #include "entity.h"
@@ -343,7 +343,21 @@ void Client::RemoveTitle(int title_set)
 		return;
 	}
 
-	TitlesRepository::DeleteWhere(
+	for (const auto& title : title_manager.GetTitles()) {
+		if (title.titleset == title_set) {
+			if (std::string(m_pp.title) == title.prefix) {
+				SetAATitle("");
+			}
+
+			if (std::string(m_pp.suffix) == title.suffix) {
+				SetTitleSuffix("");
+			}
+
+			break;
+		}
+	}
+
+	PlayerTitlesetsRepository::DeleteWhere(
 		database,
 		fmt::format(
 			"`title_set` = {} AND `char_id` = {}",
