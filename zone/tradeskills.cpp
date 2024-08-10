2f791e9a3667af62f3f0becca6d44c1450b5cfe3
catapultam-habeo <alex@catapult.am>
2024-08-10 02:42:07 +0000

    actually allow non-gnomes to tinker

--- zone/tradeskills.cpp
+++ zone/tradeskills.cpp
@@ -470,7 +470,7 @@ void Object::HandleCombine(Client* user, const NewCombine_Struct* in_combine, Ob
 		}
 	}
 	else if (spec.tradeskill == EQ::skills::SkillTinkering) {
-		if (user_pp.race != GNOME || RuleB(Custom, MulticlassingEnabled)) {
+		if (user_pp.race != GNOME && !RuleB(Custom, MulticlassingEnabled)) {
 			user->Message(Chat::Red, "Only gnomes can tinker.");
 			auto outapp = new EQApplicationPacket(OP_TradeSkillCombine, 0);
 			user->QueuePacket(outapp);
