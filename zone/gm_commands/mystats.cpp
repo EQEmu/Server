cb1f16a4a018fcdab0ba4d1658ae2699895f52b6
catapultam-habeo <alex@catapult.am>
2024-10-03 16:56:49 +0000

    mystats supports extra pets

--- zone/gm_commands/mystats.cpp
+++ zone/gm_commands/mystats.cpp
@@ -8,15 +8,14 @@ void command_mystats(Client *c, const Seperator *sep)
 	}
 
 	if (
-		(t->IsPet() && t == c->GetPet()) ||
-		(t->IsBot() && t->CastToBot()->GetOwner() && t->CastToBot()->GetOwner() == c)
+		(t->GetOwner() && t == t->GetOwner() && t->GetOwner()->GetID() == GetID())
 	) {
 		t->ShowStats(c);
 
 		// Ugly hack to look at pet equipment
-		
+
 		for (int i = EQ::invslot::EQUIPMENT_BEGIN; i <= EQ::invslot::EQUIPMENT_END; i++) {
-			
+
 			const EQ::ItemInstance *inst_main = nullptr;
 			const EQ::ItemInstance *inst_sub  = nullptr;
 			const EQ::ItemInstance *inst_aug  = nullptr;
@@ -26,27 +25,27 @@ void command_mystats(Client *c, const Seperator *sep)
 			linker.SetLinkType(EQ::saylink::SayLinkItemInst);
 
 			inst_main = t->GetInvPublic().GetItem(i);
-			
+
 			if (inst_main) {
 				item_data  = inst_main->GetItem();
 				linker.SetItemInst(inst_main);
 			} else {
 				item_data = nullptr;
 			}
-			if (i != EQ::invslot::slotCharm && i != EQ::invslot::slotPowerSource && i != EQ::invslot::slotAmmo) {			
+			if (i != EQ::invslot::slotCharm && i != EQ::invslot::slotPowerSource && i != EQ::invslot::slotAmmo) {
 				std::string padded_string = fmt::format("[{:>10}]", EQ::invslot::GetInvPossessionsSlotName(i));
 				std::replace(padded_string.begin(), padded_string.end(), ' ', '-');
 				if (item_data) {
-					padded_string = fmt::format("{}[{}]", padded_string, linker.GenerateLink());	
+					padded_string = fmt::format("{}[{}]", padded_string, linker.GenerateLink());
 				} else {
-					padded_string = fmt::format("{}[{}]", padded_string, "<Empty>");					
-				}				
+					padded_string = fmt::format("{}[{}]", padded_string, "<Empty>");
+				}
 
 				c->Message(
 					Chat::White, padded_string.c_str()
 				);
 			}
-			
+
 		}
 
 		return;
