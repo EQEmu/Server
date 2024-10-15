04a25bf201621d0be1e12aa646f705fcac598b79
Mitch Freeman <65987027+neckkola@users.noreply.github.com>
2024-10-14 22:39:57 -0300

    First pass of player_event_loot_items

---
+++ common/repositories/base/base_player_event_loot_items_repository.h
@@ -0,0 +1,451 @@
+/**
+ * DO NOT MODIFY THIS FILE
+ *
+ * This repository was automatically generated and is NOT to be modified directly.
+ * Any repository modifications are meant to be made to the repository extending the base.
+ * Any modifications to base repositories are to be made by the generator only
+ *
+ * @generator ./utils/scripts/generators/repository-generator.pl
+ * @docs https://docs.eqemu.io/developer/repositories
+ */
+
+#ifndef EQEMU_BASE_PLAYER_EVENT_LOOT_ITEMS_REPOSITORY_H
+#define EQEMU_BASE_PLAYER_EVENT_LOOT_ITEMS_REPOSITORY_H
+
+#include "../../database.h"
+#include "../../strings.h"
+#include <ctime>
+
+class BasePlayerEventLootItemsRepository {
+public:
+	struct PlayerEventLootItems {
+		uint64_t    id;
+		uint64_t    player_event_logs_details_id;
+		uint32_t    item_id;
+		std::string item_name;
+		int32_t     charges;
+		uint32_t    npc_id;
+		std::string corpse_name;
+	};
+
+	static std::string PrimaryKey()
+	{
+		return std::string("id");
+	}
+
+	static std::vector<std::string> Columns()
+	{
+		return {
+			"id",
+			"player_event_logs_details_id",
+			"item_id",
+			"item_name",
+			"charges",
+			"npc_id",
+			"corpse_name",
+		};
+	}
+
+	static std::vector<std::string> SelectColumns()
+	{
+		return {
+			"id",
+			"player_event_logs_details_id",
+			"item_id",
+			"item_name",
+			"charges",
+			"npc_id",
+			"corpse_name",
+		};
+	}
+
+	static std::string ColumnsRaw()
+	{
+		return std::string(Strings::Implode(", ", Columns()));
+	}
+
+	static std::string SelectColumnsRaw()
+	{
+		return std::string(Strings::Implode(", ", SelectColumns()));
+	}
+
+	static std::string TableName()
+	{
+		return std::string("player_event_loot_items");
+	}
+
+	static std::string BaseSelect()
+	{
+		return fmt::format(
+			"SELECT {} FROM {}",
+			SelectColumnsRaw(),
+			TableName()
+		);
+	}
+
+	static std::string BaseInsert()
+	{
+		return fmt::format(
+			"INSERT INTO {} ({}) ",
+			TableName(),
+			ColumnsRaw()
+		);
+	}
+
+	static PlayerEventLootItems NewEntity()
+	{
+		PlayerEventLootItems e{};
+
+		e.id                           = 0;
+		e.player_event_logs_details_id = 0;
+		e.item_id                      = 0;
+		e.item_name                    = "";
+		e.charges                      = 0;
+		e.npc_id                       = 0;
+		e.corpse_name                  = "";
+
+		return e;
+	}
+
+	static PlayerEventLootItems GetPlayerEventLootItems(
+		const std::vector<PlayerEventLootItems> &player_event_loot_itemss,
+		int player_event_loot_items_id
+	)
+	{
+		for (auto &player_event_loot_items : player_event_loot_itemss) {
+			if (player_event_loot_items.id == player_event_loot_items_id) {
+				return player_event_loot_items;
+			}
+		}
+
+		return NewEntity();
+	}
+
+	static PlayerEventLootItems FindOne(
+		Database& db,
+		int player_event_loot_items_id
+	)
+	{
+		auto results = db.QueryDatabase(
+			fmt::format(
+				"{} WHERE {} = {} LIMIT 1",
+				BaseSelect(),
+				PrimaryKey(),
+				player_event_loot_items_id
+			)
+		);
+
+		auto row = results.begin();
+		if (results.RowCount() == 1) {
+			PlayerEventLootItems e{};
+
+			e.id                           = row[0] ? strtoull(row[0], nullptr, 10) : 0;
+			e.player_event_logs_details_id = row[1] ? strtoull(row[1], nullptr, 10) : 0;
+			e.item_id                      = row[2] ? static_cast<uint32_t>(strtoul(row[2], nullptr, 10)) : 0;
+			e.item_name                    = row[3] ? row[3] : "";
+			e.charges                      = row[4] ? static_cast<int32_t>(atoi(row[4])) : 0;
+			e.npc_id                       = row[5] ? static_cast<uint32_t>(strtoul(row[5], nullptr, 10)) : 0;
+			e.corpse_name                  = row[6] ? row[6] : "";
+
+			return e;
+		}
+
+		return NewEntity();
+	}
+
+	static int DeleteOne(
+		Database& db,
+		int player_event_loot_items_id
+	)
+	{
+		auto results = db.QueryDatabase(
+			fmt::format(
+				"DELETE FROM {} WHERE {} = {}",
+				TableName(),
+				PrimaryKey(),
+				player_event_loot_items_id
+			)
+		);
+
+		return (results.Success() ? results.RowsAffected() : 0);
+	}
+
+	static int UpdateOne(
+		Database& db,
+		const PlayerEventLootItems &e
+	)
+	{
+		std::vector<std::string> v;
+
+		auto columns = Columns();
+
+		v.push_back(columns[1] + " = " + std::to_string(e.player_event_logs_details_id));
+		v.push_back(columns[2] + " = " + std::to_string(e.item_id));
+		v.push_back(columns[3] + " = '" + Strings::Escape(e.item_name) + "'");
+		v.push_back(columns[4] + " = " + std::to_string(e.charges));
+		v.push_back(columns[5] + " = " + std::to_string(e.npc_id));
+		v.push_back(columns[6] + " = '" + Strings::Escape(e.corpse_name) + "'");
+
+		auto results = db.QueryDatabase(
+			fmt::format(
+				"UPDATE {} SET {} WHERE {} = {}",
+				TableName(),
+				Strings::Implode(", ", v),
+				PrimaryKey(),
+				e.id
+			)
+		);
+
+		return (results.Success() ? results.RowsAffected() : 0);
+	}
+
+	static PlayerEventLootItems InsertOne(
+		Database& db,
+		PlayerEventLootItems e
+	)
+	{
+		std::vector<std::string> v;
+
+		v.push_back(std::to_string(e.id));
+		v.push_back(std::to_string(e.player_event_logs_details_id));
+		v.push_back(std::to_string(e.item_id));
+		v.push_back("'" + Strings::Escape(e.item_name) + "'");
+		v.push_back(std::to_string(e.charges));
+		v.push_back(std::to_string(e.npc_id));
+		v.push_back("'" + Strings::Escape(e.corpse_name) + "'");
+
+		auto results = db.QueryDatabase(
+			fmt::format(
+				"{} VALUES ({})",
+				BaseInsert(),
+				Strings::Implode(",", v)
+			)
+		);
+
+		if (results.Success()) {
+			e.id = results.LastInsertedID();
+			return e;
+		}
+
+		e = NewEntity();
+
+		return e;
+	}
+
+	static int InsertMany(
+		Database& db,
+		const std::vector<PlayerEventLootItems> &entries
+	)
+	{
+		std::vector<std::string> insert_chunks;
+
+		for (auto &e: entries) {
+			std::vector<std::string> v;
+
+			v.push_back(std::to_string(e.id));
+			v.push_back(std::to_string(e.player_event_logs_details_id));
+			v.push_back(std::to_string(e.item_id));
+			v.push_back("'" + Strings::Escape(e.item_name) + "'");
+			v.push_back(std::to_string(e.charges));
+			v.push_back(std::to_string(e.npc_id));
+			v.push_back("'" + Strings::Escape(e.corpse_name) + "'");
+
+			insert_chunks.push_back("(" + Strings::Implode(",", v) + ")");
+		}
+
+		std::vector<std::string> v;
+
+		auto results = db.QueryDatabase(
+			fmt::format(
+				"{} VALUES {}",
+				BaseInsert(),
+				Strings::Implode(",", insert_chunks)
+			)
+		);
+
+		return (results.Success() ? results.RowsAffected() : 0);
+	}
+
+	static std::vector<PlayerEventLootItems> All(Database& db)
+	{
+		std::vector<PlayerEventLootItems> all_entries;
+
+		auto results = db.QueryDatabase(
+			fmt::format(
+				"{}",
+				BaseSelect()
+			)
+		);
+
+		all_entries.reserve(results.RowCount());
+
+		for (auto row = results.begin(); row != results.end(); ++row) {
+			PlayerEventLootItems e{};
+
+			e.id                           = row[0] ? strtoull(row[0], nullptr, 10) : 0;
+			e.player_event_logs_details_id = row[1] ? strtoull(row[1], nullptr, 10) : 0;
+			e.item_id                      = row[2] ? static_cast<uint32_t>(strtoul(row[2], nullptr, 10)) : 0;
+			e.item_name                    = row[3] ? row[3] : "";
+			e.charges                      = row[4] ? static_cast<int32_t>(atoi(row[4])) : 0;
+			e.npc_id                       = row[5] ? static_cast<uint32_t>(strtoul(row[5], nullptr, 10)) : 0;
+			e.corpse_name                  = row[6] ? row[6] : "";
+
+			all_entries.push_back(e);
+		}
+
+		return all_entries;
+	}
+
+	static std::vector<PlayerEventLootItems> GetWhere(Database& db, const std::string &where_filter)
+	{
+		std::vector<PlayerEventLootItems> all_entries;
+
+		auto results = db.QueryDatabase(
+			fmt::format(
+				"{} WHERE {}",
+				BaseSelect(),
+				where_filter
+			)
+		);
+
+		all_entries.reserve(results.RowCount());
+
+		for (auto row = results.begin(); row != results.end(); ++row) {
+			PlayerEventLootItems e{};
+
+			e.id                           = row[0] ? strtoull(row[0], nullptr, 10) : 0;
+			e.player_event_logs_details_id = row[1] ? strtoull(row[1], nullptr, 10) : 0;
+			e.item_id                      = row[2] ? static_cast<uint32_t>(strtoul(row[2], nullptr, 10)) : 0;
+			e.item_name                    = row[3] ? row[3] : "";
+			e.charges                      = row[4] ? static_cast<int32_t>(atoi(row[4])) : 0;
+			e.npc_id                       = row[5] ? static_cast<uint32_t>(strtoul(row[5], nullptr, 10)) : 0;
+			e.corpse_name                  = row[6] ? row[6] : "";
+
+			all_entries.push_back(e);
+		}
+
+		return all_entries;
+	}
+
+	static int DeleteWhere(Database& db, const std::string &where_filter)
+	{
+		auto results = db.QueryDatabase(
+			fmt::format(
+				"DELETE FROM {} WHERE {}",
+				TableName(),
+				where_filter
+			)
+		);
+
+		return (results.Success() ? results.RowsAffected() : 0);
+	}
+
+	static int Truncate(Database& db)
+	{
+		auto results = db.QueryDatabase(
+			fmt::format(
+				"TRUNCATE TABLE {}",
+				TableName()
+			)
+		);
+
+		return (results.Success() ? results.RowsAffected() : 0);
+	}
+
+	static int64 GetMaxId(Database& db)
+	{
+		auto results = db.QueryDatabase(
+			fmt::format(
+				"SELECT COALESCE(MAX({}), 0) FROM {}",
+				PrimaryKey(),
+				TableName()
+			)
+		);
+
+		return (results.Success() && results.begin()[0] ? strtoll(results.begin()[0], nullptr, 10) : 0);
+	}
+
+	static int64 Count(Database& db, const std::string &where_filter = "")
+	{
+		auto results = db.QueryDatabase(
+			fmt::format(
+				"SELECT COUNT(*) FROM {} {}",
+				TableName(),
+				(where_filter.empty() ? "" : "WHERE " + where_filter)
+			)
+		);
+
+		return (results.Success() && results.begin()[0] ? strtoll(results.begin()[0], nullptr, 10) : 0);
+	}
+
+	static std::string BaseReplace()
+	{
+		return fmt::format(
+			"REPLACE INTO {} ({}) ",
+			TableName(),
+			ColumnsRaw()
+		);
+	}
+
+	static int ReplaceOne(
+		Database& db,
+		const PlayerEventLootItems &e
+	)
+	{
+		std::vector<std::string> v;
+
+		v.push_back(std::to_string(e.id));
+		v.push_back(std::to_string(e.player_event_logs_details_id));
+		v.push_back(std::to_string(e.item_id));
+		v.push_back("'" + Strings::Escape(e.item_name) + "'");
+		v.push_back(std::to_string(e.charges));
+		v.push_back(std::to_string(e.npc_id));
+		v.push_back("'" + Strings::Escape(e.corpse_name) + "'");
+
+		auto results = db.QueryDatabase(
+			fmt::format(
+				"{} VALUES ({})",
+				BaseReplace(),
+				Strings::Implode(",", v)
+			)
+		);
+
+		return (results.Success() ? results.RowsAffected() : 0);
+	}
+
+	static int ReplaceMany(
+		Database& db,
+		const std::vector<PlayerEventLootItems> &entries
+	)
+	{
+		std::vector<std::string> insert_chunks;
+
+		for (auto &e: entries) {
+			std::vector<std::string> v;
+
+			v.push_back(std::to_string(e.id));
+			v.push_back(std::to_string(e.player_event_logs_details_id));
+			v.push_back(std::to_string(e.item_id));
+			v.push_back("'" + Strings::Escape(e.item_name) + "'");
+			v.push_back(std::to_string(e.charges));
+			v.push_back(std::to_string(e.npc_id));
+			v.push_back("'" + Strings::Escape(e.corpse_name) + "'");
+
+			insert_chunks.push_back("(" + Strings::Implode(",", v) + ")");
+		}
+
+		std::vector<std::string> v;
+
+		auto results = db.QueryDatabase(
+			fmt::format(
+				"{} VALUES {}",
+				BaseReplace(),
+				Strings::Implode(",", insert_chunks)
+			)
+		);
+
+		return (results.Success() ? results.RowsAffected() : 0);
+	}
+};
+
+#endif //EQEMU_BASE_PLAYER_EVENT_LOOT_ITEMS_REPOSITORY_H
