77b3752b88061ad611a3d7823956c37890c63bff
Mitch Freeman <65987027+neckkola@users.noreply.github.com>
2024-10-23 22:46:30 -0300

    Add etl for Playerevent::KilledNPC, KilledNamedNPC and KilledRaidNPC

--- zone/client_packet.cpp
+++ zone/client_packet.cpp
@@ -16649,6 +16649,11 @@ struct RecordKillCheck {
 
 void Client::RecordKilledNPCEvent(NPC *n)
 {
+	if (!n) {
+		LogError("NPC passed was invalid.");
+		return;
+	}
+
 	bool is_named = Strings::Contains(n->GetName(), "#") && !n->IsRaidTarget();
 
 	std::vector<RecordKillCheck> checks = {
