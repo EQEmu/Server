95bf30740ffa5aee52590b204a43b0bd9e9907c9
nytmyr <53322305+nytmyr@users.noreply.github.com>
2024-11-06 07:31:48 -0600

    more target validation for bots to prevent pets from getting hit with AEs and pets trying to attack invalid targets

--- zone/bot.cpp
+++ zone/bot.cpp
@@ -2116,6 +2116,13 @@ void Bot::AI_Process()
 
 // TARGET VALIDATION
 		if (!IsValidTarget(bot_owner, leash_owner, lo_distance, leash_distance, tar, tar_distance)) {
+			if (HasPet()) {
+				if (tar && GetPet()->GetTarget() && GetPet()->GetTarget() == tar) {
+					GetPet()->WipeHateList();
+					GetPet()->SetTarget(nullptr);
+				}
+			}
+
 			return;
 		}
 
