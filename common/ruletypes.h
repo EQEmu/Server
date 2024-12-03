86a069134108d671ad7f4f204cdd712a1dad35d3
Paul Johnson <Paul@pjohnsomac-6366.digi.box>
2024-12-02 22:49:57 -0500

    rule for custom files admin level

--- common/ruletypes.h
+++ common/ruletypes.h
@@ -342,6 +342,7 @@ RULE_BOOL(World, EnablePVPRegions, true, "Enables or disables PVP Regions automa
 RULE_STRING(World, SupportedClients, "", "Comma-delimited list of clients to restrict to. Supported values are Titanium | SoF | SoD | UF | RoF | RoF2. Example: Titanium,RoF2")
 RULE_STRING(World, CustomFilesKey, "", "Enable if the server requires custom files and sends a key to validate. Empty string to disable. Example: eqcustom_v1")
 RULE_STRING(World, CustomFilesUrl, "github.com/knervous/eqnexus/releases", "URL to display at character select if client is missing custom files")
+RULE_INT(World, CustomFilesAdminLevel, 20, "Admin level at which custom file key is not required when CustomFilesKey is specified")
 RULE_CATEGORY_END()
 
 RULE_CATEGORY(Zone)
