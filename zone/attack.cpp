5457d14d85f408bc848ba3c7a70447153bf8644f
nytmyr <53322305+nytmyr@users.noreply.github.com>
2024-12-12 22:07:54 -0600

    add bot checks to tryheadshot / tryassassinate

--- zone/attack.cpp
+++ zone/attack.cpp
@@ -6446,6 +6446,7 @@ void Mob::CommonOutgoingHitSuccess(Mob* defender, DamageHitInfo &hit, ExtraAttac
 		}
 		else {
 			int ass = TryAssassinate(defender, hit.skill);
+
 			if (ass > 0) {
 				hit.damage_done = ass;
 			}
