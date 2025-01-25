41f362ce616cad073005fa1d1157f1f5380dcbbc
nytmyr <53322305+nytmyr@users.noreply.github.com>
2025-01-24 20:26:48 -0600

    Add bot pet check for AIYellForHelp

--- zone/npc.cpp
+++ zone/npc.cpp
@@ -3666,7 +3666,7 @@ void NPC::AIYellForHelp(Mob *sender, Mob *attacker)
 			&& mob != attacker
 			&& mob->GetPrimaryFaction() != 0
 			&& !mob->IsEngaged()
-			&& ((!mob->IsPet()) || (mob->IsPet() && mob->GetOwner() && !mob->GetOwner()->IsClient()))
+			&& ((!mob->IsPet()) || (mob->IsPet() && mob->GetOwner() && !mob->GetOwner()->IsOfClientBot()))
 			) {
 
 			/**
