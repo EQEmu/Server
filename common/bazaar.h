c211f37286b30c152388801c9280339087541960
Chris Miles <akkadius1@gmail.com>
2025-01-29 16:31:12 -0600

    Revert bazaar changes

--- common/bazaar.h
+++ common/bazaar.h
@@ -3,13 +3,11 @@
 
 #include <vector>
 #include "shareddb.h"
-#include "../../common/item_instance.h"
 
 class Bazaar {
 public:
 	static std::vector<BazaarSearchResultsFromDB_Struct>
-	GetSearchResults(Database &content_db, Database &db, BazaarSearchCriteria_Struct search, unsigned int char_zone_id, int char_zone_instance_id);
-
+	GetSearchResults(SharedDatabase &db, BazaarSearchCriteria_Struct search, unsigned int char_zone_id, int char_zone_instance_id);
 };
 
 
