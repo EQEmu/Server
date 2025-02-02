ee04d41dc879879c8dabb464661411f6b12b69af
Akkadius <akkadius1@gmail.com>
2025-02-02 03:56:03 -0600

    Enc cleanup

--- loginserver/encryption.h
+++ loginserver/encryption.h
@@ -26,10 +26,6 @@ namespace CryptoHash {
 	const int sha512_hash_length = 128;
 }
 
-/**
- * @param mode
- * @return
- */
 std::string GetEncryptionByModeId(uint32 mode);
 const char *eqcrypt_block(const char *buffer_in, size_t buffer_in_sz, char *buffer_out, bool enc);
 std::string eqcrypt_hash(const std::string &username, const std::string &password, int mode);
