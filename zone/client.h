a547f8afca69c1468819bd2d352c8a63e77bd54e
Alex King <89047260+Kinglykrab@users.noreply.github.com>
2025-02-01 19:41:27 -0500

    Update client.h

--- zone/client.h
+++ zone/client.h
@@ -1834,7 +1834,7 @@ public:
 
 	inline uint32 GetPotionBeltItemIcon(uint8 slot_id) { return EQ::ValueWithin(slot_id, 0, EQ::profile::POTION_BELT_SIZE) ? m_pp.potionbelt.Items[slot_id].Icon : 0; };
 	inline uint32 GetPotionBeltItemID(uint8 slot_id) { return EQ::ValueWithin(slot_id, 0, EQ::profile::POTION_BELT_SIZE) ? m_pp.potionbelt.Items[slot_id].ID : 0; };
-	inline std::string GetPotionBeltItemName(uint8 slot_id) { return EQ::ValueWithin(slot_id, 0, EQ::profile::POTION_BELT_SIZE) ? m_pp.potionbelt.Items[slot_id].Name : 0; };
+	inline std::string GetPotionBeltItemName(uint8 slot_id) { return EQ::ValueWithin(slot_id, 0, EQ::profile::POTION_BELT_SIZE) ? m_pp.potionbelt.Items[slot_id].Name : std::string(); };
 
 	void ShowDevToolsMenu();
 	CheatManager cheat_manager;
