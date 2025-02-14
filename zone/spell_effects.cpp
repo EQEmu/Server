02ec6b3b697b7484f05b18b3ed8feddea76d823a
Kinglykrab <kinglykrab@gmail.com>
2025-02-13 19:15:12 -0500

    Update spell_effects.cpp

--- zone/spell_effects.cpp
+++ zone/spell_effects.cpp
@@ -4344,11 +4344,10 @@ void Mob::BuffFadeBySlot(int slot, bool iRecalcBonuses)
 					AppearanceStruct{
 						.gender_id = GetBaseGender(),
 						.race_id = GetBaseRace(),
+						.size = GetDefaultRaceSize(GetBaseRace(), GetBaseGender()),
 						.texture = 0
 					}
 				);
-				// The GetSize below works because the above setting race to zero sets size back.
-				SendAppearancePacket(AppearanceType::Size, GetSize());
 
 				for (int x = EQ::textures::textureBegin; x <= EQ::textures::LastTintableTexture; x++) {
 					SendWearChange(x);
