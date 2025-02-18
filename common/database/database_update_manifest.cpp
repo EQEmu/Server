0c9cb6b5788932307144de62848d536d24537c6e
Akkadius <akkadius1@gmail.com>
2025-02-18 03:33:53 -0600

    Add entity variables to the schema

--- common/database/database_update_manifest.cpp
+++ common/database/database_update_manifest.cpp
@@ -6900,6 +6900,7 @@ CREATE TABLE `zone_state_spawns` (
   `enabled` smallint(6) DEFAULT 1,
   `anim` smallint(5) unsigned DEFAULT 0,
   `loot_data` text DEFAULT NULL,
+  `entity_variables` text DEFAULT NULL,
   `created_at` datetime DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4
