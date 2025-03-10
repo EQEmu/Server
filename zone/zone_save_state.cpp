41c4d252e623f3ebfa0912abb27e42dfcd56ec2f
Akkadius <akkadius1@gmail.com>
2025-03-09 21:23:24 -0500

    Update zone_save_state.cpp

--- zone/zone_save_state.cpp
+++ zone/zone_save_state.cpp
@@ -525,6 +525,7 @@ void Zone::SaveZoneState()
 				 n.second->IsBot() ||
 				 n.second->IsMerc() ||
 				 n.second->IsTrap() ||
+				 n.second->GetSwarmOwner() ||
 				 n.second->IsPet();
 		if (ignore_npcs) {
 			continue;
