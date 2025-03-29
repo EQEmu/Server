da0a975b711ce2e1eb62288f7a9258ac66e2a050
Catapultam Habeo <alex@catapult.am>
2025-03-28 23:55:53 -0500

    correction

--- zone/client_packet.cpp
+++ zone/client_packet.cpp
@@ -3299,7 +3299,7 @@ void Client::Handle_OP_AugmentItem(const EQApplicationPacket *app)
 					const auto slots_old = tobe_auged->GetItem()->Slots & RuleB(Custom, PowerSourceItemUpgrade) ? (1 << 21) : 0;  // Power Source
 
 					if (!(slots_new & slots_old)) {
-						if (RuleB(Custom, PowerSourceItemUpgrade)) { LogError("Unable to create item with no usability."); }
+						if (!RuleB(Custom, PowerSourceItemUpgrade)) { LogError("Unable to create item with no usability."); }
 						Message(Chat::Red, "The result of this combine would produce an item unusable by anyone.");
 						break;
 					}
