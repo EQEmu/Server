9be2485330afccf3551ffe90dda17ddd78ab4e3f
Akkadius <akkadius1@gmail.com>
2025-03-31 02:34:35 -0500

    [Hotfix] Backfill expire_at (not sure why this didn't make it in there to begin with)

--- common/database/database_update_manifest.cpp
+++ common/database/database_update_manifest.cpp
@@ -7081,7 +7081,7 @@ CREATE INDEX `idx_expire_at` ON `respawn_times` (`expire_at`);
 ALTER TABLE `instance_list`
 ADD COLUMN `expire_at` bigint(11) UNSIGNED NOT NULL DEFAULT 0 AFTER `duration`;
 
-UPDATE instance_list set expire_at = `start` + `duration`; -- backfill existing data
+UPDATE instance_list set expire_at = `start_time` + `duration`; -- backfill existing data
 
 CREATE INDEX `idx_expire_at` ON `instance_list` (`expire_at`);
 )",
