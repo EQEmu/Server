5fb6d7023de9503ec6c81800f1f17cdc0625f9fe
Kinglykrab <kinglykrab@gmail.com>
2025-04-02 22:52:22 -0400

    Update client_packet.cpp

--- zone/client_packet.cpp
+++ zone/client_packet.cpp
@@ -1350,7 +1350,7 @@ void Client::Handle_Connect_OP_ZoneEntry(const EQApplicationPacket *app)
 	database.LoadCharacterLeadershipAbilities(cid, &m_pp); /* Load Character Leadership AA's */
 	database.LoadCharacterTribute(this); /* Load CharacterTribute */
 	database.LoadCharacterEXPModifier(this); /* Load Character EXP Modifier */
-	database.LoadCharacterTitleSets(this);
+	database.LoadCharacterTitleSets(this); /* Load Character Title Sets */
 
 	// this pattern is strange
 	// this is remnants of the old way of doing things
