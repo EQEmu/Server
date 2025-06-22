229d056e409edcc566f6c86dd83ace975502b11b
Akkadius <akkadius1@gmail.com>
2025-06-22 13:15:42 -0500

    [Code] Optimize PCH Config (Faster Builds)

---
+++ common/pch/app-pch.h
@@ -0,0 +1,7 @@
+#include "../types.h"
+#include "../database.h"
+#include "../strings.h"
+#include "../eqemu_logsys.h"
+#include "../eqemu_logsys_log_aliases.h"
+#include "../features.h"
+#include "../global_define.h"
