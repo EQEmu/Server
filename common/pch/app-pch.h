b3b228c26c295fee77174b5cbc6e5f87e9fcbb29
Chris Miles <akkadius1@gmail.com>
2025-06-25 12:26:38 -0500

    [Code] PCH Cleanup (#4952)
    * Update CMakeLists.txt
    
    * Update CMakeLists.txt
    
    * Update CMakeLists.txt
    
    * Update CMakeLists.txt
    
    * Update CMakeLists.txt
    
    * Update CMakeLists.txt
    
    * Update CMakeLists.txt
    
    * Update CMakeLists.txt
    
    * Update CMakeLists.txt
    
    * Update CMakeLists.txt
    
    * Update CMakeLists.txt
    
    * Update app-pch.h
    
    * Update CMakeLists.txt
    
    * Split out pch to zone
    
    * Update CMakeLists.txt
    
    * Update CMakeLists.txt
    
    * Update CMakeLists.txt
    
    * Update CMakeLists.txt
    
    * Update CMakeLists.txt
    
    * Update windows-build.ps1
    
    * Update windows-build.ps1
    
    * Update CMakeLists.txt
    
    * Update CMakeLists.txt
    
    * Update CMakeLists.txt
    
    * Update CMakeLists.txt
    
    * Update CMakeLists.txt

--- common/pch/app-pch.h
+++ common/pch/app-pch.h
@@ -1,3 +1,5 @@
+#pragma once
+
 #include "../types.h"
 #include "../database.h"
 #include "../strings.h"
---
+++ zone/pch/pch.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include "../merc.h"
+#include "../mob.h"
+#include "../npc.h"
+#include "../corpse.h"
+#include "../doors.h"
+#include "../bot.h"
+#include "../entity.h"
+#include "../client.h"
+#include "../zone.h"
